785f32c2a13e411f5f4f8c4e16249f0fe8753992
Jimar <Jimar>
2017-03-10 12:58:14 +0800

    push

---
+++ database/seeds/tbl_payroll_tax_period.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class tbl_payroll_tax_period extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('tbl_payroll_tax_period')->truncate();
+        $statement = "INSERT INTO `tbl_payroll_tax_period` (`payroll_tax_period_id`,`payroll_tax_period`) vALUES
+        	(1, 'Daily'),
+        	(2, 'Weekly'),
+        	(3, 'Semi-monthly'),
+        	(4, 'Monthly')";
+
+        DB::statement($statement);
+
+    }
+}

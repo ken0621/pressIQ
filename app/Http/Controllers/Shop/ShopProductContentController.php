5968472b888211c9d82e009386a230fc9a1332e1
Namae <asd@asd>
2017-03-18 18:25:11 +0800

    Commit by Tamaki Ako <3

--- app/Http/Controllers/Shop/ShopProductContentController.php
+++ app/Http/Controllers/Shop/ShopProductContentController.php
@@ -19,7 +19,7 @@ class ShopProductContentController extends Shop
         $data["breadcrumbs"] = Ecom_Product::getProductBreadcrumbs($data["product"]["eprod_category_id"], $this->shop_info->shop_id);
         $data["_variant"]    = Ecom_Product::getProductOption($id, ",");
         $data["_related"]    = Ecom_Product::getAllProductByCategory($data["product"]["eprod_category_id"], $this->shop_info->shop_id);
-    
+        
         foreach ($data["product"]["variant"] as $keys => $values) 
         {
             // Convert to timestamp

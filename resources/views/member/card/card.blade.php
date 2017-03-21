f291a992a532ecbc5ae7d52eab49d58ecd664ec3
lukzpogi <lukeglennjordan@gmail.com>
2017-03-21 15:21:12 +0800

    luke 3/21/2017

--- resources/views/member/card/card.blade.php
+++ resources/views/member/card/card.blade.php
@@ -3,7 +3,6 @@
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">  
 @endif
 <div class="{{ Request::input('pdf') == 'true' ? '' : 'row' }} clearfix">
-
 <div class="{{ Request::input('pdf') == 'true' ? '' : 'col-md-6' }} clearfix" style="{{ Request::input('pdf') == 'true' ? 'margin-top: 450px;' : '' }}">
 	<div class="containers" style="overflow: hidden; {{ Request::input('pdf') == 'true' ? '-webkit-transform: scale(5,5); transform: scale(5,5);' : '' }} ;height: 276px; background-color: transparent; background-image: url('{{Request::input('pdf') == 'true' ? URL::to('/') : ''}}/assets/card/images/BG-{{ $color }}.jpg') !important;">
 		<div class="top-container clearfix">
@@ -18,7 +17,7 @@
 			</div>
 			<div class="barcode">
 				<div class="barcodeimg" style="background-color: #fff; padding: 7.5px 0;"><img src="{{Request::input('pdf') == 'true' ? URL::to('/') : ''}}/barcode?text={{ $membership_code }}&size=35"></div>
-				<div class="barcodetxt" style="font-size: 9px;">
+				<div class="barcodetxt" style="font-size: 8px;">
 					<span>Membership Code</span>
 					<span>{{ $membership_code }}</span>
 				</div>
@@ -69,7 +68,7 @@
 				</table>
 			</div>
 			<div style="font-weight: 500; color: @if($color == 'gold') black @else white @endif ; font-size: 11px;" style="margin-top: 5px; ">By signing this card, the Cardholder agrees to be bound be the terms and conditions of the VIP Loyalty Program. Present this card along with the valid ID when purchasing or availing privilleges and benefits in the head office, all BCO and partner merchants nationwide.</div>
-			<div style="color: white; font-weight: 600; font-size: 16px; text-align: center; margin-bottom: 5px; letter-spacing: 1px">Non-Transferable&nbsp;&nbsp;&#149;&nbsp;&nbsp;No Annual Fee&nbsp;&nbsp;&#149;&nbsp;&nbsp;No Expiry</div>
+			<div style="color: white; font-weight: 600; font-size: 16px; text-align: center;">Non-Transferable&nbsp;&nbsp;&#149;&nbsp;&nbsp;No Annual Fee&nbsp;&nbsp;&#149;&nbsp;&nbsp;No Expiry</div>
 			<div class="text-right" style="font-weight: 600; color: @if($color == 'gold') black @else white @endif ; font-size: 12px;">Tampering invalidates the card.</div>
 		</div>
 	</div>

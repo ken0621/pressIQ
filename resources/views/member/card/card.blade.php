0f328117a04e8a0aa3a224410c9c87c209bb70c1
lukzpogi <lukeglennjordan@gmail.com>
2017-03-21 15:45:32 +0800

    luke 3/21/2017

--- resources/views/member/card/card.blade.php
+++ resources/views/member/card/card.blade.php
@@ -67,7 +67,7 @@
 					</tbody>
 				</table>
 			</div>
-			<div style="font-weight: 500; color: @if($color == 'gold') black @else white @endif ; font-size: 11px;" style="margin-top: 5px; ">By signing this card, the Cardholder agrees to be bound be the terms and conditions of the VIP Loyalty Program. Present this card along with the valid ID when purchasing or availing privilleges and benefits in the head office, all BCO and partner merchants nationwide.</div>
+			<div style="font-weight: 500; color: @if($color == 'gold') black @else white @endif ; font-size: 10px;" style="margin-top: 5px; ">By signing this card, the Cardholder agrees to be bound be the terms and conditions of the VIP Loyalty Program. Present this card along with the valid ID when purchasing or availing privilleges and benefits in the head office, all BCO and partner merchants nationwide.</div>
 			<div style="color: white; font-weight: 600; font-size: 15px; text-align: center;">Non-Transferable&nbsp;&nbsp;&#149;&nbsp;&nbsp;No Annual Fee&nbsp;&nbsp;&#149;&nbsp;&nbsp;No Expiry</div>
 			<div class="text-right" style="font-weight: 600; color: @if($color == 'gold') black @else white @endif ; font-size: 12px;">Tampering invalidates the card.</div>
 		</div>

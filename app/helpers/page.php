d88c91e13fd7171af86abc85f1feb4033744d05c
asasasas <sasas@gmail.com>
2017-03-21 10:51:40 +0800

    arcy

--- app/helpers/page.php
+++ app/helpers/page.php
@@ -698,13 +698,13 @@ function page_list()
     $nav[$page]['submenu'][$code]['developer'] = "No Developer Yet";
     
     /* -- UTILITIES => AUDIT TRAIL */
-    $code = "utilities-client-list";
-    $nav[$page]['submenu'][$code]['label'] = "Owner's Information Settings";
-    $nav[$page]['submenu'][$code]['code'] = $code;
-    $nav[$page]['submenu'][$code]['url'] = $path . $page . "/client_list";
-    $nav[$page]['submenu'][$code]['user_settings'] = ['access_page'];
-    $nav[$page]['submenu'][$code]['status'] = "No Progress Yet";
-    $nav[$page]['submenu'][$code]['developer'] = "No Developer Yet";
+    // $code = "utilities-client-list";
+    // $nav[$page]['submenu'][$code]['label'] = "Owner's Information Settings";
+    // $nav[$page]['submenu'][$code]['code'] = $code;
+    // $nav[$page]['submenu'][$code]['url'] = $path . $page . "/client_list";
+    // $nav[$page]['submenu'][$code]['user_settings'] = ['access_page'];
+    // $nav[$page]['submenu'][$code]['status'] = "No Progress Yet";
+    // $nav[$page]['submenu'][$code]['developer'] = "No Developer Yet";
 
     /* Maintenance */
     $page = "maintenance";  

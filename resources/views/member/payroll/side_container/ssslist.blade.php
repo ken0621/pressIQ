fc9f43148e1bb1287a58294eb670ab02bdd8c302
ryzamakun <rgtplus.net@gmail.com>
2017-03-22 10:01:05 +0800

    myMes

--- resources/views/member/payroll/side_container/ssslist.blade.php
+++ resources/views/member/payroll/side_container/ssslist.blade.php
@@ -1,89 +1,95 @@
 <form class="form-horizontal global-submit" role="form" action="/member/payroll/sss_table_list/sss_table_save" method="POST">
 	<h4>SSS Table</h4>
 	<input type="hidden" name="_token" value="{{csrf_token()}}">
-	<table class="table table-bordered table-condensed ">
-		<tr>
-			<td colspan="2">
-				Range of Compensation
-			</td>
-			<td>
-				Monthly Salary Credit
-			</td>
-			<td>
-				ER
-			</td>
-			<td>
-				EE
-			</td>
-			<td>
-				Total
-			</td>
-			<td>
-				EC/ER
-			</td>
-			<td width="5%"></td>
-		</tr>
 
-		<tbody class="tbl-sss">
-		@foreach($_sss as $sss)
-			<tr>
-				<td>
-					<input type="number" step="any" name="payroll_sss_min[]" class="border-none width-100 text-right" value="{{$sss->payroll_sss_min}}">
-				</td>
-				<td>
-					<input type="number" step="any" name="payroll_sss_max[]" class="border-none width-100 text-right" value="{{$sss->payroll_sss_max}}">
-				</td>
-				<td>
-					<input type="number" step="any" name="payroll_sss_monthly_salary[]" class="border-none width-100 text-right" value="{{$sss->payroll_sss_monthly_salary}}">
-				</td>
-				<td>
-					<input type="number" step="any" name="payroll_sss_er[]" class="border-none width-100 text-right" value="{{$sss->payroll_sss_er}}">
-				</td>
-				<td>
-					<input type="number" step="any" name="payroll_sss_ee[]" class="border-none width-100 text-right" value="{{$sss->payroll_sss_ee}}">
-				</td>
-				<td>
-					<input type="number" step="any" name="payroll_sss_total[]" class="border-none width-100 text-right" value="{{$sss->payroll_sss_total}}">
-				</td>
-				<td>
-					<input type="number" step="any" name="payroll_sss_eec[]" class="border-none width-100 text-right" value="{{$sss->payroll_sss_eec}}">
-				</td>
-				<td class="text-center" valign="cener">
-					<a href="#" class="remove-tr"><i class="fa fa-trash-o"></i></a>
-				</td>
-			</tr>
-		@endforeach
-		</tbody>
-		
-		<tbody class="display-none tbl-ref">
-			<tr>
-				<td>
-					<input type="number" step="any" name="payroll_sss_min[]" class="border-none width-100 text-right">
-				</td>
-				<td>
-					<input type="number" step="any" name="payroll_sss_max[]" class="border-none width-100 text-right">
-				</td>
-				<td>
-					<input type="number" step="any" name="payroll_sss_monthly_salary[]" class="border-none width-100 text-right">
-				</td>
-				<td>
-					<input type="number" step="any" name="payroll_sss_er[]" class="border-none width-100 text-right">
-				</td>
-				<td>
-					<input type="number" step="any" name="payroll_sss_ee[]" class="border-none width-100 text-right">
-				</td>
-				<td>
-					<input type="number" step="any" name="payroll_sss_total[]" class="border-none width-100 text-right">
-				</td>
-				<td>
-					<input type="number" step="any" name="payroll_sss_eec[]" class="border-none width-100 text-right">
-				</td>
-				<td class="text-center" valign="cener">
-					<a href="#" class="remove-tr"><i class="fa fa-trash-o"></i></a>
-				</td>
-			</tr>
-		</tbody>
-	</table>
+	<div class="load-data" target="value-id-1">
+		<div id="value-id-1">
+			<table class="table table-bordered table-condensed ">
+				<tr>
+					<td colspan="2">
+						Range of Compensation
+					</td>
+					<td>
+						Monthly Salary Credit
+					</td>
+					<td>
+						ER
+					</td>
+					<td>
+						EE
+					</td>
+					<td>
+						Total
+					</td>
+					<td>
+						EC/ER
+					</td>
+					<td width="5%"></td>
+				</tr>
+
+				<tbody class="tbl-sss">
+				@foreach($_sss as $sss)
+					<tr>
+						<td>
+							<input type="number" step="any" name="payroll_sss_min[]" class="border-none width-100 text-right" value="{{$sss->payroll_sss_min}}">
+						</td>
+						<td>
+							<input type="number" step="any" name="payroll_sss_max[]" class="border-none width-100 text-right" value="{{$sss->payroll_sss_max}}">
+						</td>
+						<td>
+							<input type="number" step="any" name="payroll_sss_monthly_salary[]" class="border-none width-100 text-right" value="{{$sss->payroll_sss_monthly_salary}}">
+						</td>
+						<td>
+							<input type="number" step="any" name="payroll_sss_er[]" class="border-none width-100 text-right" value="{{$sss->payroll_sss_er}}">
+						</td>
+						<td>
+							<input type="number" step="any" name="payroll_sss_ee[]" class="border-none width-100 text-right" value="{{$sss->payroll_sss_ee}}">
+						</td>
+						<td>
+							<input type="number" step="any" name="payroll_sss_total[]" class="border-none width-100 text-right" value="{{$sss->payroll_sss_total}}">
+						</td>
+						<td>
+							<input type="number" step="any" name="payroll_sss_eec[]" class="border-none width-100 text-right" value="{{$sss->payroll_sss_eec}}">
+						</td>
+						<td class="text-center" valign="cener">
+							<a href="#" class="remove-tr"><i class="fa fa-trash-o"></i></a>
+						</td>
+					</tr>
+				@endforeach
+				</tbody>
+				
+				<tbody class="display-none tbl-ref">
+					<tr>
+						<td>
+							<input type="number" step="any" name="payroll_sss_min[]" class="border-none width-100 text-right">
+						</td>
+						<td>
+							<input type="number" step="any" name="payroll_sss_max[]" class="border-none width-100 text-right">
+						</td>
+						<td>
+							<input type="number" step="any" name="payroll_sss_monthly_salary[]" class="border-none width-100 text-right">
+						</td>
+						<td>
+							<input type="number" step="any" name="payroll_sss_er[]" class="border-none width-100 text-right">
+						</td>
+						<td>
+							<input type="number" step="any" name="payroll_sss_ee[]" class="border-none width-100 text-right">
+						</td>
+						<td>
+							<input type="number" step="any" name="payroll_sss_total[]" class="border-none width-100 text-right">
+						</td>
+						<td>
+							<input type="number" step="any" name="payroll_sss_eec[]" class="border-none width-100 text-right">
+						</td>
+						<td class="text-center" valign="cener">
+							<a href="#" class="remove-tr"><i class="fa fa-trash-o"></i></a>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+			<div class="pagination"> {!! $_sss->render() !!} </div>
+		</div>
+	</div>
 	<div class="form-group">
 		<div class="col-md-12">
 			<i><span class="color-red">*</span><span class="">Note: Zero (0) value in the last row indicates greater than other provided value.</span></i>
@@ -124,3 +130,12 @@
 		}
 	}
 </script>
+
+<script type="text/javascript">
+	function loading_done_paginate (data)
+	{
+		console.log(data);
+	}
+</script>
+
+<script type="text/javascript" src="/assets/member/js/paginate_ajax_multiple.js"></script>

17bd56020bcd665d51ad1dde5fa9d10c92524921
asasasas <sasas@gmail.com>
2017-03-23 13:43:25 +0800

    test

--- app/Models/Tbl_inventory_slip.php
+++ app/Models/Tbl_inventory_slip.php
@@ -9,4 +9,13 @@ class Tbl_inventory_slip extends Model
     protected $table = 'tbl_inventory_slip';
 	protected $primaryKey = "inventory_slip_id";
     public $timestamps = false;
+
+    public function scopeShop($query)
+    {
+    	return $query->leftjoin("tbl_shop","tbl_shop.shop_id","=","tbl_inventory_slip.inventory_slip_shop_id");
+    }
+    public function scopeVendor($query)
+    {
+    	return $query->leftjoin("tbl_vendor","tbl_vendor.vendor_id","=","tbl_inventory_slip.inventory_source_id");
+    }
 }

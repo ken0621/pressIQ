2cdb7ab37d4955cbd8a90d9128ade495282a2ab2
lukzpogi <lukeglennjordan@gmail.com>
2017-03-29 10:54:59 +0800

    luke 3/29/2017

--- resources/views/member/mlm_wallet/transfer.blade.php
+++ resources/views/member/mlm_wallet/transfer.blade.php
@@ -39,7 +39,7 @@
 <div class="col-md-12">
   <div class="box">
     <div class="box-header with-border">
-      <h3 class="box-title">Refill Logs</h3>
+      <h3 class="box-title">Transfer Logs</h3>
     </div>
     <!-- /.box-header -->
     <div class="box-body">

b8803e3b2201c58b0a66d0bfb97475f54d143886
lukzpogi <lukeglennjordan@gmail.com>
2017-04-10 17:57:13 +0800

    l

--- database/migrations/2017_04_10_133251_update_tbl_mlm_slot_wallet_log.php
+++ database/migrations/2017_04_10_133251_update_tbl_mlm_slot_wallet_log.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateTblMlmSlotWalletLog2 extends Migration
+class UpdateTblMlmSlotWalletLog extends Migration
 {
     /**
      * Run the migrations.

ac66f1300efe78e85e2c04b5ef99760b1b9082b7
jimarzape@gmail.com <jimarzape@gmail.com>
2017-04-11 09:40:52 +0800

    migration fix

--- database/migrations/2017_04_05_113601_update_tbl_payroll_allowance_record_v2.php
+++ database/migrations/2017_04_05_113601_update_tbl_payroll_allowance_record_v2.php
@@ -18,7 +18,7 @@ class UpdateTblPayrollAllowanceRecordV2 extends Migration
             $table->integer('payroll_employee_id')->unsigned()->change();
             $table->foreign("payroll_employee_id")->references("payroll_employee_id")->on("tbl_payroll_employee_basic")->onDelete('cascade');
             $table->dropColumn('payroll_employee_allowance_id');
-            $table->integer('payroll_allowance_id')->unsigned();
+            $table->integer('payroll_allowance_id')->unsigned()->change();
             $table->foreign("payroll_allowance_id")->references("payroll_allowance_id")->on("tbl_payroll_allowance")->onDelete('cascade');
         });
     }

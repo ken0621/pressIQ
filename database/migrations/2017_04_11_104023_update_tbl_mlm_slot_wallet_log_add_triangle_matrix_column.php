373b8ce83636d515728d9d47bf270f72297da254
lukzpogi <lukeglennjordan@gmail.com>
2017-04-11 11:28:48 +0800

    luke

--- database/migrations/2017_04_11_104023_update_tbl_mlm_slot_wallet_log_add_triangle_matrix_column.php
+++ database/migrations/2017_04_11_104023_update_tbl_mlm_slot_wallet_log_add_triangle_matrix_column.php
@@ -14,7 +14,11 @@ class UpdateTblMlmSlotWalletLogAddTriangleMatrixColumn extends Migration
     {
         Schema::table('tbl_mlm_slot_wallet_log', function (Blueprint $table) {
             //
-            $table->integer('wallet_log_matrix_triangle')->default(0);
+            // $table->integer('wallet_log_matrix_triangle')->default(0);
+            if(!schema::hasColumn('tbl_mlm_slot_wallet_log','wallet_log_matrix_triangle'))
+            {
+                $table->integer('wallet_log_matrix_triangle')->default(0);
+            }
         });
     }
 

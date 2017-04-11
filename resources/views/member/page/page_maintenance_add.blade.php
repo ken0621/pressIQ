a6986f74663ef27a2a2d353613c0983b53cbc436
Your Name <you@example.com>
2017-04-11 15:55:32 +0800

    Switch muna

--- resources/views/member/page/page_maintenance_add.blade.php
+++ resources/views/member/page/page_maintenance_add.blade.php
@@ -17,15 +17,30 @@
 		<div style="margin-top: 15px;">
 			@foreach($field as $fields)
 			<div class="form-group">
-				<label>{{ ucwords(str_replace(' ', '_', $fields->name)) }}</label>
+				<label class="{{ $fields->type == "hidden" ? "hide" : "" }}">{{ ucwords(str_replace(' ', '_', $fields->name)) }}</label>
 				@if($fields->type == "textarea")
-				<textarea class="form-control mce" name="{{ $fields->name }}"></textarea>
+					<textarea class="form-control mce" name="{{ $fields->name }}"></textarea>
 				@elseif($fields->type == "image")
-				<input type="hidden" name="{{ $fields->name }}" class="maintenance-image-input" key="{{ $key }}-{{ $fields->type }}">
-				<div class="maintenance-image-holder" key="{{ $key }}-{{ $fields->type }}"></div>
-				<div><button class="image-gallery image-gallery-single btn btn-primary" key="{{ $key }}-{{ $fields->type }}"> Upload Image</button></div>
+					<input type="hidden" name="{{ $fields->name }}" class="maintenance-image-input" key="{{ $key }}-{{ $fields->type }}">
+					<div class="maintenance-image-holder" key="{{ $key }}-{{ $fields->type }}"></div>
+					<div><button class="image-gallery image-gallery-single btn btn-primary" key="{{ $key }}-{{ $fields->type }}"> Upload Image</button></div>
+				@elseif($fields->type == "map")
+					<div id="map"></div>
+				    <script>
+				      function initMap() {
+				        // Create a map object and specify the DOM element for display.
+				        var map = new google.maps.Map(document.getElementById('map'), {
+				          center: {lat: -34.397, lng: 150.644},
+				          scrollwheel: false,
+				          zoom: 8
+				        });
+				      }
+
+				    </script>
+				    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDntbTzqtnwMA7hdMBAeX37YwTjRJi6cDY&callback=initMap"
+				    async defer></script>
 				@else
-				<input class="form-control" type="{{ $fields->type }}" name="{{ $fields->name }}">
+					<input class="form-control" type="{{ $fields->type }}" name="{{ $fields->name }}">
 				@endif
 			</div>
 			@endforeach

4bfa076652c9c725dd0ddbb927f8b224c4ce0266
lukzpogi <lukeglennjordan@gmail.com>
2017-04-11 11:24:54 +0800

    luke

--- database/migrations/2017_04_11_104023_update_tbl_mlm_slot_wallet_log_add_triangle_matrix_column.php
+++ database/migrations/2017_04_11_104023_update_tbl_mlm_slot_wallet_log_add_triangle_matrix_column.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterTblMlmSlotWalletLog extends Migration
+class UpdateTblMlmSlotWalletLogAddTriangleMatrixColumn extends Migration
 {
     /**
      * Run the migrations.
@@ -14,11 +14,7 @@ class AlterTblMlmSlotWalletLog extends Migration
     {
         Schema::table('tbl_mlm_slot_wallet_log', function (Blueprint $table) {
             //
-            if(!schema::hasColumn('tbl_mlm_slot_wallet_log', 'wallet_log_enable_cash'))
-            {
-                $table->integer('wallet_log_enable_cash')->default(0);
-                $table->integer('wallet_log_product_repurchase')->default(0);
-            }
+            $table->integer('wallet_log_matrix_triangle')->default(0);
         });
     }
 

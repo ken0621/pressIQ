0daa43ee7a53d2c6d893482fd0df323ec98c13aa
Kimi no Namae wa? <tamaki@ako.desu>
2017-04-27 01:34:42 +0800

    Switch

--- public/themes/sovereign/views/home.blade.php
+++ public/themes/sovereign/views/home.blade.php
@@ -37,13 +37,100 @@
 				</div>
 			</div>
 			<div id="mission" class="tab-pane fade">
-				
+				<div class="row clearfix">
+					<div class="col-md-8">
+						<h1>Company Mission</h1>
+						<p>To serve the community and its people by providing opportunity for good health and abundant life. And inspire people to help others make a positive change in their lives.</p>
+						<div class="read-more">
+							<a href="/about">Read More</a>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<img class="img-reponsive" src="/themes/{{ $shop_theme }}/img/mission-side.jpg">
+					</div>
+				</div>
 			</div>
 			<div id="vision" class="tab-pane fade">
-				
+				<div class="row clearfix">
+					<div class="col-md-8">
+						<h1>Company Vision</h1>
+						<p>To be the trademark in the field of health nutrition, business education and service to the community thru its outstanding products made available for entrepreneurs worldwide.</p>
+						<div class="read-more">
+							<a href="/about">Read More</a>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<img class="img-reponsive" src="/themes/{{ $shop_theme }}/img/vision-side.jpg">
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
+</div>
+<div class="values" style="background-image: url('/themes/{{ $shop_theme }}/img/values-bg.jpg')">
+	<div class="container">
+		<div class="row clearfix">
+			<div class="col-md-6 left-holder match-height">
+				<div class="holder">
+					<div class="values-title">Company Core Values</div>
+					<div class="divider"></div>
+					<div class="values-sub">-Inspired by "Coach Wooden"</div>
+				</div>
+			</div>
+			<div class="col-md-6 match-height">
+				<div class="tower">
+					<div class="tower-row">
+						<div class="holder">
+							<div class="name">Excellence</div>
+							<div class="desc">Performing at your highest level</div>
+						</div>
+					</div>
+					<div class="tower-row">
+						<div class="holder">
+							<div class="name">Character</div>
+							<div class="desc">The qualities that distinguish individuals</div>
+						</div>
+						<div class="holder">
+							<div class="name">Integrity</div>
+							<div class="desc">The faithfulness to maintain high moral standards</div>
+						</div>
+					</div>
+					<div class="tower-row">
+						<div class="holder">
+							<div class="name">Team Work</div>
+							<div class="desc">Subordinating personal prominence to team efficiency</div>
+						</div>
+						<div class="holder">
+							<div class="name">Communication</div>
+							<div class="desc">The ingredient to organizational success</div>
+						</div>
+						<div class="holder">
+							<div class="name">Collaboration</div>
+							<div class="desc">The willingness to jointly achieve common goals</div>
+						</div>
+					</div>
+					<div class="tower-row">
+						<div class="holder">
+							<div class="name">Attitude</div>
+							<div class="desc">Energy, drive and enjoyment will always inspire others</div>
+						</div>
+						<div class="holder">
+							<div class="name">Discipline</div>
+							<div class="desc">Energy, drive and enjoyment will always inspire others</div>
+						</div>
+						<div class="holder">
+							<div class="name">Initiative</div>
+							<div class="desc">Failure to act will lead to the biggest failures of all</div>
+						</div>
+						<div class="holder">
+							<div class="name">Work Ethic</div>
+							<div class="desc">Success travels in the company of very hard work and dedication</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>	
 </div>
 @endsection
 

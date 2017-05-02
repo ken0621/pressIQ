709844b586748cb56083e2ad12ca41e577e5b3cb
arcy <arcylen103095@gmail.com>
2017-05-02 11:04:01 +0800

    sir arcy

--- resources/views/member/purchasing_inventory_system/view_status.blade.php
+++ resources/views/member/purchasing_inventory_system/view_status.blade.php
@@ -47,12 +47,8 @@ function submit_done(data)
 {
     if(data.status == "success")
     {
-        toastr.success("Success");
-        $('#global_modal').modal('toggle');
-        $(".sir_container").load("/member/pis/lof .sir_container");
-        data.element.modal("hide");
-        $(".all-sir").addClass("active");
-        $(".sir-class").removeClass("active");
+       toastr.success("Success");
+       location.reload();
     }
     else if(data.status == "error")
     {

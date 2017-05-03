94c9322a1c4b2230692c12851a15dec72bb50f3e
lukzpogi <lukeglennjordan@gmail.com>
2017-05-03 15:39:01 +0800

    luke

--- resources/views/member/mlm_report/report/inventory_warehouse.blade.php
+++ resources/views/member/mlm_report/report/inventory_warehouse.blade.php
@@ -24,7 +24,7 @@
             @foreach($warehouse_a as $k => $warehouse)
             <div style="overflow-x:auto;" class="table-responsive Container Flipped">
                 
-                <table class="table table-condensed table-bordered Content" width="100%">
+                <table class="table table-condensed table-bordered Content" style="width: 100%">
                     <thead>
                         <thead>
                             @if(isset($warehouse))

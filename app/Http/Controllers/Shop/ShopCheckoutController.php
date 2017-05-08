ff0f58562e1fa0a985492877f610e40805338632
Kimi no Namae wa? <tamaki@ako.desu>
2017-05-08 14:57:21 +0800

    all

--- app/Http/Controllers/Shop/ShopCheckoutController.php
+++ app/Http/Controllers/Shop/ShopCheckoutController.php
@@ -427,7 +427,7 @@ class ShopCheckoutController extends Shop
             Session::put('ipay88', $ipay88);
             Session::put('ipay88_data', Request::input());
             dd("???");
-            return redirect('/postPaymentWithIPay88');     
+            return redirect('/postPaymentWithIPay88')->send();    
         }  
     }
     /*Ipay88 Function*/

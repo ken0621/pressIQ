8a3f5e7069a8eee967717dbc7c85bbd17771dfc4
Kimi no Namae wa? <tamaki@ako.desu>
2017-05-09 11:20:14 +0800

    XD

---
+++ public/themes/intogadgets/resources/assets/frontend/js/checkout_login.js
@@ -0,0 +1,17 @@
+var checkout_login = new checkout_login();
+
+function checkout_login()
+{
+	init();
+	function init()
+	{
+		$(document).ready(function()
+		{
+			document_ready();
+		});
+	}
+	function document_ready()
+	{
+		alert(123);
+	}
+}
--- public/themes/intogadgets/views/checkout_login.blade.php
+++ public/themes/intogadgets/views/checkout_login.blade.php
@@ -7,15 +7,18 @@
 				<div class="form-header">Login or Checkout as guest</div>
 				<div class="form-content">
 					<div class="form-group">
-						<input type="text" class="form-control" placeholder="Enter your email address" name="email_address">
+						<input type="text" class="form-control" placeholder="Enter your email address" name="username">
 					</div>
 					<div class="form-group">
 						<div class="radio">
-						  <label><input type="radio" name="optradio"> Continue without password</label>
+						  <label><input type="radio" name="optradio" checked> Continue without password</label>
 						</div>
 						<div class="radio">
 						  <label><input type="radio" name="optradio"> I already have an account</label>
 						</div>
+					</div>
+					<div class="form-group">
+						<input disabled type="text" class="form-control" placeholder="Enter your password" name="password">
 					</div>
 					<div class="form-group text-right">
 						<a href="javascript:">Lost your password?</a>
@@ -91,4 +94,8 @@
 
 @section('css')
 <link rel="stylesheet" type="text/css" href="resources/assets/frontend/css/checkout_login.css">
+@endsection
+
+@section('script')
+<script type="text/javascript" src="resources/assets/frontend/js/checkout_login.js"></script>
 @endsection

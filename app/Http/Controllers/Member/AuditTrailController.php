0a8c2f80069b02331854d70914a9edf1f89116f7
ryzamakun <rgtplus.net@gmail.com>
2017-05-12 11:48:16 +0800

    commit before merrge -brain

--- app/Http/Controllers/Member/AuditTrailController.php
+++ app/Http/Controllers/Member/AuditTrailController.php
@@ -36,13 +36,9 @@ class AuditTrailController extends Member
                 /*'transaction_client'    => "Desription", */
             );
 
-        /* $sample = Tbl_audit_trail::join('tbl_user', 'tbl_user.user_id', '=', 'tbl_audit_trail.user_id')
-                    ->where(DB::raw("CONCAT('tbl_user.user_first_name', ' ', 'tbl_user.user_last_name')"), "LIKE", "%Sample%")->get();
-         dd($sample);
-*/
         if($this->hasAccess("utilities-audit","access_page"))
         {   
-            $data["_audit"] = AuditTrail::getAudit_data();
+           /* $data["_audit"] = AuditTrail::getAudit_data();
         
             if (Request::isMethod('post'))
             {
@@ -64,7 +60,7 @@ class AuditTrailController extends Member
                                     
             }
 
-            //dd(Session::get('date_from'));
+            //dd(Session::get('date_from'));*/
             return view("member.audit_trail.audit_trail",$data);
         }
         else
@@ -72,6 +68,35 @@ class AuditTrailController extends Member
             return $this->show_no_access();
         }
 
+    }
+
+
+    public function get_list()
+    {
+        $date_from  = Request::input('date_from');
+        $date_to    = Request::input('date_to');
+        $col        = Request::input('col');
+        $keyword    = Request::input('keyword');
+        //dd($date_to);
+        if ($keyword == '')
+        {
+            $data["_audit"] = AuditTrail::getAudit_data();    
+        } 
+        else
+        {
+            if ($date_from != '' && $date_to != '')
+            {
+                $data["_audit"] = AuditTrail::getSearchAuditData($col, $keyword, $date_from, $date_to);
+                //dd('sample');
+            }
+                else
+            {                 
+                $data["_audit"] = AuditTrail::getSearchAuditData($col, $keyword);    
+                //dd('pasok');
+            }
+        }
+
+        return view("member.audit_trail.audit_list", $data);
     }
 
     /**
--- resources/views/member/audit_trail/audit_trail.blade.php
+++ resources/views/member/audit_trail/audit_trail.blade.php
@@ -18,26 +18,26 @@
     <div class="panel-body form-horizontal">
 
     	<div class="form-group">
-    		<form data-target="#active-employee" method="POST" action="/member/utilities/audit">
+    		{{-- <form data-target="#active-employee" method="POST" action="/member/utilities/audit"> --}}
 	            <input type="hidden" name="_token" value="{{csrf_token()}}">
 	    		<div class="col-md-12 filter-div">
 	    			<div class="col-md-2 padding-lr-1">
 		        		<small>Date From:</small>
-	        			<input type="date" name="date_from" class="form-control perdictive perdictive-active width-100" value="{{ isset($date_from) ? $date_from : '' }}" >	
+	        			<input type="date" name="date_from" id="date_from" class="form-control perdictive perdictive-active width-100" value="" >	
 		        	</div>	
 		        	<div class="col-md-2 padding-lr-1">
 		        		<small>Date To:</small>
-	        			<input type="date" name="date_to" class="form-control perdictive perdictive-active width-100" value="{{ isset($date_to) ? $date_to : '' }}" 	>	
+	        			<input type="date" name="date_to" id="date_to" class="form-control perdictive perdictive-active width-100" value="" 	>	
 		        	</div>	
 	        	</div>
 		        <div class="col-md-12 filter-div">
 		        	
 		          <div class="col-md-2 padding-lr-1">
 		            <small>Filter by: </small>
-		            <select class="form-control filter-change filter-change-col" name="col" data-target="#active-employee" >
+		            <select class="form-control filter-change filter-change-col" name="col" id="col"  data-target="#active-employee" >
 		             {{--  <option value="0">Select Column</option> --}}
 		              @foreach($_column as $key => $value)
-		              	<option value="{{ $key }}" {{ isset($col) ? ($key==$col) ? 'selected' : '' : '' }} >{{ $value }}</option>
+		              	<option value="{{ $key }}" >{{ $value }}</option>
 		              @endforeach
 		            </select>
 		          </div>
@@ -45,17 +45,18 @@
 		            <input type="hidden" id="col" value="">
 		            <small>Search Keyword</small>
 		            <div class="input-group">
-		              <input type="search" name="keyword" id="keyword" class="form-control perdictive perdictive-active width-100" placeholder="Search  here" value="{{ isset($keyword) ? $keyword : '' }}" >
+		              <input type="search" name="keyword" id="keyword" class="form-control perdictive perdictive-active width-100" placeholder="Search  here" value="" onkeyup="ajaxLoad('/member/utilities/audit/get_list')">
 		              <span class="input-group-btn">
-		                <button class="btn btn-custom-primary" type="submit"><i class="fa fa-search"></i></button>
+		                <button class="btn btn-custom-primary" type="submit" onclick="ajaxLoad('/member/utilities/audit/get_list')"><i class="fa fa-search"></i></button>
 		              </span>
 		            </div>           
-	          </form>
+	          {{-- </form> --}}
 	        </div>
 	     </div>
 
-
-        <div class="form-group">
+	     <div id="content"></div>
+	     <div class="loading">{{-- <img src="/assets/member/img/34.gif"> --}}</div>
+        {{-- <div class="form-group">
         	<div class="col-md-12">  		
 		        <div class="form-group tab-content panel-body warehouse-container">
 		            <div id="all" class="tab-pane fade in active">
@@ -92,30 +93,46 @@
 		                    </tbody>
 		                    </table>
 		                </div>
+		              {!! $_audit->render() !!}   
 		            </div>
 		        </div>
         	</div>
-        </div>
+        </div> --}}
+
+
     </div>
 </div>
 
 <script type="text/javascript">
 	
-	/*function onSelCol()
-	{
-		var col = $(".filter-change-col").val();
-		if (col != "0" )
-		{
-			$("#col").val(col);	
-		} 
-			else 
-		{
-			$(),prop
-		}
+	 function ajaxLoad(filename, content) {
 
-		
-		
-	} 	*/
+	 	var date_from 	= $('#date_from').val();
+	 	var date_to 	= $('#date_to').val();
+	 	var col 		= $('#col').val();
+	 	var keyword 	= $('#keyword').val();
+
+	 	filename = filename+'?date_from='+date_from+'&date_to='+date_to+'&col='+col+'&keyword='+keyword;
+	 	//alert(filename);
+
+        content = typeof content !== 'undefined' ? content : 'content';
+        $('.loading').show();
+        $.ajax({
+            type: "GET",
+            url: filename,
+            contentType: false,
+            success: function (data) {
+                $("#" + content).html(data);
+                $('.loading').hide();
+            },
+            error: function (xhr, status, error) {
+                alert(xhr.responseText);
+            }
+        });
+    }
+    $(document).ready(function () {
+        ajaxLoad('/member/utilities/audit/get_list');
+    });
 
 </script>
 @endsection

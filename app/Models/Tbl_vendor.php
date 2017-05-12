868a6194270fc7442391450a8337589c3b208a7d
bryan <topherlapig@yahoo.com>
2017-05-12 11:01:22 +0800

    save

--- app/Models/Tbl_vendor.php
+++ app/Models/Tbl_vendor.php
@@ -43,10 +43,18 @@ class Tbl_vendor extends Model
                     ->where("po_shop_id", $shop_id);
         if($vendor_id) $purchase_order->where("po_vendor_id", $vendor_id);
 
+        /* ITEM RECEIPT */
+        $bill = DB::table("tbl_vendor")->selectRaw("bill_date as date, 'Item Receipt' as type, bill_id as no, bill_due_date as due_date, bill_total_amount - bill_applied_payment as balance, bill_total_amount as total, 'status' as status, date_created, 'vendor/create_bill' as reference_url")
+                    ->join("tbl_bill","bill_vendor_id","=","vendor_id")
+                    ->where("bill_shop_id", $shop_id)
+                    ->where("inventory_only", 0);
+        if($vendor_id) $bill->where("bill_vendor_id", $vendor_id);
+
         /* BILL */
         $bill = DB::table("tbl_vendor")->selectRaw("bill_date as date, 'Bill' as type, bill_id as no, bill_due_date as due_date, bill_total_amount - bill_applied_payment as balance, bill_total_amount as total, 'status' as status, date_created, 'vendor/create_bill' as reference_url")
                     ->join("tbl_bill","bill_vendor_id","=","vendor_id")
-                    ->where("bill_shop_id", $shop_id);
+                    ->where("bill_shop_id", $shop_id)
+                    >where("inventory_only", 1);
         if($vendor_id) $bill->where("bill_vendor_id", $vendor_id);
 
         /* PAY BILL */

39f25c8ca751202a464bc28750b5c399be2c9ee9
bryan <topherlapig@yahoo.com>
2017-05-15 16:53:20 +0800

    save

--- app/Http/Controllers/Member/ImportController.php
+++ app/Http/Controllers/Member/ImportController.php
@@ -130,7 +130,7 @@ class ImportController extends Member
 
 		if($ctr != $data_length)
 		{
-			$type 					= isset($value["Type"]) 				? $value["Type"] : '';					  	//1,2,3
+			$type					= isset($value["Type"]) 				? strToUpper($value["Type"]) : '';			//1,2,3
 			$name 					= isset($value["Name"]) 				? $value["Name"] : '';					  	//1,2,3
 			$sku 					= isset($value["Sku"]) 					? $value["Sku"] : '';				      	//1,2,3
 			$um 					= isset($value["UM"]) 					? $value["UM"] : '';					  	//1,2,3
@@ -183,151 +183,158 @@ class ImportController extends Member
 						$category = $has_Category->type_id;
 					}
 
-					if($has_Income_Account || $auto_income_account)
+					/* CHECK INCOME ACCOUNT */
+					if($income_account == '') // DEFAULT
 					{
-						/* CHECK INCOME ACCOUNT */
-						if(!$has_Income_Account && $auto_income_account && $income_account != '')
+						$income_account = Tbl_chart_of_account::where("account_code", "accounting-sales")
+								->where("account_shop_id", $this->getShopId())->pluck("account_id");
+					}
+					elseif(!$has_Income_Account && $auto_income_account && $income_account != '')
+					{
+						$income_account = $this->create_income_account($income_account);
+					}
+					else
+					{
+						$income_account = $has_Income_Account->account_id;
+					}
+
+					/* IF TYPE IS ACCEPTED */
+					if($type == "INVENTORY" || $type == "NON-INVENTORY" || $type == "SERVICE")
+					{
+						$insert["shop_id"]				      	  = $this->getShopId();
+						$insert["item_name"]				      = $name;
+						$insert["item_sku"]					      = $sku;
+						$insert["item_category_id"]			      = $category;
+						$insert["item_sales_information"] 	      = $sales_information;
+						$insert["item_price"] 				      = $sales_price;
+						$insert["item_income_account_id"]         = $income_account;
+						$insert["item_date_created"]         	  = Carbon::now();
+
+						$message = [];
+
+						if($type == "INVENTORY")
 						{
-							$income_account = $this->create_income_account($income_account);
-						}
-						else
-						{
-							$income_account = $has_Income_Account->account_id;
-						}
-
-						/* IF TYPE IS ACCEPTED */
-						if($type == "INVENTORY" || $type == "NON-INVENTORY" || $type == "SERVICE")
-						{
-							$insert["shop_id"]				      	  = $this->getShopId();
-							$insert["item_name"]				      = $name;
-							$insert["item_sku"]					      = $sku;
-							$insert["item_category_id"]			      = $category;
-							$insert["item_sales_information"] 	      = $sales_information;
-							$insert["item_price"] 				      = $sales_price;
-							$insert["item_income_account_id"]         = $income_account;
-							$insert["item_date_created"]         	  = Carbon::now();
-
-							$message = [];
-
-							if($type == "INVENTORY")
+							/*  Check Expense Account */
+							if($expense_account == '') // DEFAULT
 							{
-								/*  Check Expense Account */
-								if(!$has_Expense_Account && $auto_expense_account && $expense_account != '')
-								{
-									$expense_account = $this->create_expense_account($expense_account);
-								}
-								elseif($has_Expense_Account)
-								{
-									$expense_account = $has_Expense_Account->account_id;
-								}
-
-								/* Check Asset Account */
-								if(!$has_Asset_Account && $auto_asset_account && $asset_account != '')
-								{
-									$asset_account = $this->create_asset_account($asset_account);
-								}
-								elseif($has_Asset_Account)
-								{
-									$asset_account = $has_Asset_Account->account_id;
-								}
-
-								$insert["item_type_id"]				      = 1;
-								$insert["item_purchasing_information"]    = $purchasing_information;
-								$insert["item_cost"]				      = $purchase_cost;
-								$insert["item_expense_account_id"]	      = $expense_account;
-								$insert["item_barcode"]				      = $barcode;
-								$insert["item_quantity"]		          = $qty_on_hand == '' ? 0 : $qty_on_hand;
-								$insert["item_reorder_point"] 		      = $reorder_point;
-								$insert["item_date_tracked"]	          = datepicker_input($as_of_date);
-								$insert["item_asset_account_id"]          = $asset_account;
-								$insert["packing_size"]				      = $packing_size;
-								$insert["item_manufacturer_id"]	      	  = $manufacturer;
-
-								$rules["item_quantity"]				      = 'required|numeric';
-								$rules["item_cost"]					      = 'required|numeric';
-								$rules["item_expense_account_id"]		  = 'required';
-								$rules["item_asset_account_id"]			  = 'required';
-								
-								if($qty_on_hand > 0)
-								{
-									$rules["item_date_tracked"] 		  = 'required|date';
-									$message["item_date_tracked.required"]= "The As of Date field is required when Qty on Hand is not empty";
-									$message["item_date_tracked.date"]	  = "The As of Date field should be in Date format";
-								}
+								$expense_account = Tbl_chart_of_account::where("account_code", "accounting-expense")
+								->where("account_shop_id", $this->getShopId())->pluck("account_id");
 							}
-							elseif($type == "NON-INVENTORY")
+							elseif(!$has_Expense_Account && $auto_expense_account && $expense_account != '')
 							{
-								$insert["item_type_id"]				      = 2;
-								$insert["item_sale_to_customer"]		  = isset($sale_to_customer) ? $sale_to_customer : 0;
-								$insert["item_purchase_from_supplier"]	  = isset($purchase_from_supplier) ? $purchase_from_supplier : 0;
-								
-								$rules["item_sale_to_customer"]		  	  = 'required|numeric|min:0|max:1';
-								$rules["item_purchase_from_supplier"]  	  = 'required|numeric|min:0|max:1';
-				
-							}
-							elseif($type == "SERVICE")
-							{
-								$insert["item_type_id"]				      = 3;
-								$insert["item_sale_to_customer"]		  = isset($sale_to_customer) ? $sale_to_customer : 0;
-								$insert["item_purchase_from_supplier"]	  = isset($purchase_from_supplier) ? $purchase_from_supplier : 0;
-								
-								$rules["item_sale_to_customer"]		      = 'required|numeric|min:0|max:1';
-								$rules["item_purchase_from_supplier"]     = 'required|numeric|min:0|max:1';
-							}
-
-							$rules["item_name"]					      = 'required';
-							$rules["item_sku"]					      = 'required';
-							$rules["item_price"]				      = 'required|numeric';
-							$rules["item_income_account_id"]		  = 'required';
-
-							$validator = Validator::make($insert, $rules, $message);
-							if ($validator->fails())
-							{
-								$json["status"] 	= "error";
-								$json["message"]  	= $validator->errors()->first();
+								$expense_account = $this->create_expense_account($expense_account);
 							}
 							else
 							{
-								/* INSERT ITEM */ 
-								$item_id 		= Tbl_item::insertGetId($insert);
-
-								/* INSERT WAREHOUSE SLIP AND INVENTORY */
-								$insert_sub["warehouse_id"] = $this->current_warehouse->warehouse_id;
-								$insert_sub["item_id"] 		= $item_id;
-								$insert_sub["item_reorder_point"] = $reorder_point;
-								Tbl_sub_warehouse::insert($insert_sub);
-
-								$ins_slip["inventory_reason"] 		= "insert_item";
-								$ins_slip["warehouse_id"] 			= $this->current_warehouse->warehouse_id;
-								$ins_slip["inventory_remarks"] 		= "Insert Item";
-								$ins_slip["inventory_slip_date"] 	= Carbon::now();
-								$ins_slip["inventory_slip_shop_id"] = $this->getShopId();
-								$ins_slip["inventroy_source_reason"]= "item";
-								$ins_slip["inventory_source_id"] 	= $item_id;
-								$slip_id = Tbl_inventory_slip::insertGetId($ins_slip);
-
-								$ins_inven["inventory_item_id"] = $item_id;
-								$ins_inven["warehouse_id"] 		= $this->current_warehouse->warehouse_id;
-								$ins_inven["inventory_created"] = Carbon::now();
-								$ins_inven["inventory_count"] 	= $qty_on_hand;
-								$ins_inven["inventory_slip_id"] = $slip_id;
-								$inventory_id = Tbl_warehouse_inventory::insertGetId($ins_inven);
-
-								$json["status"]		= "success";
-								$json["message"]	= "Success";
-								$json["item_id"]	= $item_id;
+								$expense_account = $has_Expense_Account->account_id;
 							}
+
+							/* Check Asset Account */
+							if($asset_account == '') // DEFAULT
+							{
+								$asset_account = Tbl_chart_of_account::where("account_code", "accounting-inventory-asset")
+								->where("account_shop_id", $this->getShopId())->pluck("account_id");
+							}
+							elseif(!$has_Asset_Account && $auto_asset_account && $asset_account != '')
+							{
+								$asset_account = $this->create_asset_account($asset_account);
+							}
+							else
+							{
+								$asset_account = $has_Asset_Account->account_id;
+							}
+
+							$insert["item_type_id"]				      = 1;
+							$insert["item_purchasing_information"]    = $purchasing_information;
+							$insert["item_cost"]				      = $purchase_cost;
+							$insert["item_expense_account_id"]	      = $expense_account;
+							$insert["item_barcode"]				      = $barcode;
+							$insert["item_quantity"]		          = $qty_on_hand == '' ? 0 : $qty_on_hand;
+							$insert["item_reorder_point"] 		      = $reorder_point;
+							$insert["item_date_tracked"]	          = datepicker_input($as_of_date);
+							$insert["item_asset_account_id"]          = $asset_account;
+							$insert["packing_size"]				      = $packing_size;
+							$insert["item_manufacturer_id"]	      	  = $manufacturer;
+
+							$rules["item_quantity"]				      = 'required|numeric';
+							$rules["item_cost"]					      = 'required|numeric';
+							$rules["item_expense_account_id"]		  = 'required';
+							$rules["item_asset_account_id"]			  = 'required';
+							
+							if($qty_on_hand > 0)
+							{
+								$rules["item_date_tracked"] 		  = 'required|date';
+								$message["item_date_tracked.required"]= "The As of Date field is required when Qty on Hand is not empty";
+								$message["item_date_tracked.date"]	  = "The As of Date field should be in Date format";
+							}
+						}
+						elseif($type == "NON-INVENTORY")
+						{
+							$insert["item_type_id"]				      = 2;
+							$insert["item_sale_to_customer"]		  = isset($sale_to_customer) ? $sale_to_customer : 0;
+							$insert["item_purchase_from_supplier"]	  = isset($purchase_from_supplier) ? $purchase_from_supplier : 0;
+							
+							$rules["item_sale_to_customer"]		  	  = 'required|numeric|min:0|max:1';
+							$rules["item_purchase_from_supplier"]  	  = 'required|numeric|min:0|max:1';
+			
+						}
+						elseif($type == "SERVICE")
+						{
+							$insert["item_type_id"]				      = 3;
+							$insert["item_sale_to_customer"]		  = isset($sale_to_customer) ? $sale_to_customer : 0;
+							$insert["item_purchase_from_supplier"]	  = isset($purchase_from_supplier) ? $purchase_from_supplier : 0;
+							
+							$rules["item_sale_to_customer"]		      = 'required|numeric|min:0|max:1';
+							$rules["item_purchase_from_supplier"]     = 'required|numeric|min:0|max:1';
+						}
+
+						$rules["item_name"]					      = 'required';
+						$rules["item_sku"]					      = 'required';
+						$rules["item_price"]				      = 'required|numeric';
+						$rules["item_income_account_id"]		  = 'required';
+
+						$validator = Validator::make($insert, $rules, $message);
+						if ($validator->fails())
+						{
+							$json["status"] 	= "error";
+							$json["message"]  	= $validator->errors()->first();
 						}
 						else
 						{
-							$json["status"]		= "error";
-							$json["message"]	= "Item Type Unknown";
+							/* INSERT ITEM */ 
+							$item_id 		= Tbl_item::insertGetId($insert);
+
+							/* INSERT WAREHOUSE SLIP AND INVENTORY */
+							$insert_sub["warehouse_id"] = $this->current_warehouse->warehouse_id;
+							$insert_sub["item_id"] 		= $item_id;
+							$insert_sub["item_reorder_point"] = $reorder_point;
+							Tbl_sub_warehouse::insert($insert_sub);
+
+							$ins_slip["inventory_reason"] 		= "insert_item";
+							$ins_slip["warehouse_id"] 			= $this->current_warehouse->warehouse_id;
+							$ins_slip["inventory_remarks"] 		= "Insert Item";
+							$ins_slip["inventory_slip_date"] 	= Carbon::now();
+							$ins_slip["inventory_slip_shop_id"] = $this->getShopId();
+							$ins_slip["inventroy_source_reason"]= "item";
+							$ins_slip["inventory_source_id"] 	= $item_id;
+							$slip_id = Tbl_inventory_slip::insertGetId($ins_slip);
+
+							$ins_inven["inventory_item_id"] = $item_id;
+							$ins_inven["warehouse_id"] 		= $this->current_warehouse->warehouse_id;
+							$ins_inven["inventory_created"] = Carbon::now();
+							$ins_inven["inventory_count"] 	= $qty_on_hand;
+							$ins_inven["inventory_slip_id"] = $slip_id;
+							$inventory_id = Tbl_warehouse_inventory::insertGetId($ins_inven);
+
+							$json["status"]		= "success";
+							$json["message"]	= "Success";
+							$json["item_id"]	= $item_id;
 						}
 					}
 					else
 					{
 						$json["status"]		= "error";
-						$json["message"]	= "Income Account Not Found";
+						$json["message"]	= "Item Type Unknown";
 					}
 				}
 				else

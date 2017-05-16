97293fd9230858ed86b88be05e468706e511d03d
jimarzape@gmail.com <jimarzape@gmail.com>
2017-05-16 10:16:47 +0800

    push

--- app/Globals/Payroll.php
+++ app/Globals/Payroll.php
@@ -2530,12 +2530,12 @@ class Payroll
 			$tax_index = 'tax_fifth_range';
 		}
 
-		if($rate >= $tax->taxt_sixth_range&&  $rate < $tax->tax_seventh_range)
+		if($rate >= $tax->taxt_sixth_range &&  $rate < $tax->tax_seventh_range)
 		{
 			$tax_index = 'taxt_sixth_range';
 		}
 
-		if($rate < $tax->tax_seventh_range)
+		if($rate <= $tax->tax_seventh_range && $rate > $tax->taxt_sixth_range)
 		{
 			$tax_index = 'tax_seventh_range';
 		}
@@ -2550,7 +2550,7 @@ class Payroll
 			$tax_contribution = (($rate - $tax->$tax_index) * ($status_num / 100)) + $exemption_num;
 		}
 
-		// dd($tax_contribution);
+		// dd($tax_index);
 
 		return round($tax_contribution, 2);
 	}

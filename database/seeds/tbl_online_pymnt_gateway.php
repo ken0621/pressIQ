810ec8822ac8b4565b07d02e7becf2ca1756e20e
lukzpogi <lukeglennjordan@gmail.com>
2017-05-22 10:52:07 +0800

    luke

--- database/seeds/tbl_online_pymnt_gateway.php
+++ database/seeds/tbl_online_pymnt_gateway.php
@@ -18,7 +18,8 @@ class tbl_online_pymnt_gateway extends Seeder
             (3, 'Paynammics', 'paynamics'),
             (4, 'Dragon Pay', 'dragonpay'),
             (5, 'Other', 'other'),
-            (6, 'IPay88', 'ipay88')
+            (6, 'IPay88', 'ipay88'),
+            (7, 'E-wallet', 'e-wallet')   
             ";
 
         DB::statement($statement);

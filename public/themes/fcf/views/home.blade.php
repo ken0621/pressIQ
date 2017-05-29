9b951321e26291819da1f73102df1148ca84d695
Figs <figues@shit.noob>
2017-05-29 09:28:31 +0800

    figzz

--- public/themes/fcf/views/home.blade.php
+++ public/themes/fcf/views/home.blade.php
@@ -171,7 +171,7 @@
 				<div class="col-md-9">
 					<div class="bottom-container-txt">
 						<div class="bottom-container-title">{{ get_content($shop_theme_info, "home", "home_slogan_first") }} <span class="highlight">{{ get_content($shop_theme_info, "home", "home_slogan_highlight") }}</span> {{ get_content($shop_theme_info, "home", "home_slogan_last") }}</div>
-						<div class="bottom-container-details">This drives innovative solutions for our clients and improves our understanding of the world in which we work</div>
+						<div class="bottom-container-details">{{ get_content($shop_theme_info, "home", "home_slogan_subtitle") }}</div>
 					</div>
 				</div>
 				<div class="col-md-3">

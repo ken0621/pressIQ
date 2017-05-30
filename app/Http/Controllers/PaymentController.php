80d5db45d6047f53eee622f165f652392d7a5322
Kimi no Namae wa? <tamaki@ako.desu>
2017-05-31 06:35:16 +0800

    Dragonpay Testing

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -15,6 +15,8 @@ use App\Globals\Mlm_compute;
 use App\Globals\Mlm_gc;
 use Session;
 use Carbon\Carbon;
+use App\Globals\Ec_order;
+use DB;
 class PaymentController extends Controller
 {
     protected $_merchantid = 'MYPHONE' ;
@@ -45,54 +47,36 @@ class PaymentController extends Controller
         Dragon_RequestPayment::make($this->_merchantkey, $this->_data);   
     }
 
-    public function postback_url()
+    public function dragonpay_postback()
     {
-        dd("postback");
-    }
+        $request = Request::all();
 
-    public function return_url()
-    {
-        $status = Request::input("status");
+        $insert["log_date"] = Carbon::now();
+        $insert["content"]  = serialize($request);
+        DB::table("tbl_dragonpay_logs")->insert($insert);
 
-        if ($status == "S") 
+        if ($request["status"] == "S") 
         {
-            // $shop_id =  Session::get('shop_id_session');
-            $shop_id = 5;
-            $register_session = Session::get('mlm_register_step_1');
-            $customer_id = Mlm_member::register_slot_insert_customer($shop_id, $register_session);
-            
+            $from = $request["param1"];
 
-            $register_session_2 = Session::get('mlm_register_step_2');
-            $slot_sponsor = Tbl_mlm_slot::where('slot_nick_name', $register_session['sponsor'])->first();
-            $insert['slot_no'] = Mlm_plan::set_slot_no($shop_id, null);
-            $insert['shop_id'] = $shop_id;
-            $insert['slot_owner'] = $customer_id;
-            $insert['slot_created_date'] = Carbon::now();
-            $insert['slot_membership'] = $register_session_2['membership'];
-            $insert['slot_status'] = 'PS';
-            $insert['slot_sponsor'] = $slot_sponsor->slot_id;
-            // $insert['slot_nick_name'] = 
-            $id = Tbl_mlm_slot::insertGetId($insert);
-            $a = Mlm_compute::entry($id);
-            $c = Mlm_gc::slot_gc($id);
-            $slot_info = Mlm_compute::get_slot_info($id);
-            Mlm_compute::set_slot_nick_name_2($slot_info);
-            $data['status'] = 'success';
-            $data['message'][0] = 'Membership Code Already Used.';
-            $data['link'] = '/mlm';
+            if ($from == "checkout") 
+            {
+                $order_id = $request["param2"];
 
-            Session::forget('mlm_register_step_1');
-            Session::forget('mlm_register_step_2');
-            Session::forget('mlm_register_step_3');
-
-            Mlm_member::add_to_session_edit($shop_id, $customer_id, $id);
-
-            return Redirect::to("/mlm");
-        }
-        else
-        {
-            dd("Transaction failed. </br> Please do not refresh the page and wait while we are processing your payment. This can take a few minutes.");
+                try 
+                {
+                    $update['ec_order_id'] = $order_id;
+                    $update['order_status'] = "Processing";
+                    $update['payment_status'] = 1;
+                    $order = Ec_order::update_ec_order($update);
+                } 
+                catch (\Exception $e) 
+                {
+                    $last["log_date"] = Carbon::now();
+                    $last["content"]  = $e->getMessage();
+                    DB::table("tbl_dragonpay_logs")->insert($last);  
+                }      
+            }
         }
     }
-
 }

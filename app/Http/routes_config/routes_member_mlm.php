0c188ff2dc1804fc6e22dbfac5b622511d45c577
lukzpogi <lukeglennjordan@gmail.com>
2017-05-31 11:00:41 +0800

    luke

--- app/Http/routes_config/routes_member_mlm.php
+++ app/Http/routes_config/routes_member_mlm.php
@@ -174,6 +174,7 @@ Route::any('/member/mlm/encashment/view/type/cheque/edit', 'Member\Mlm_Encashmen
 Route::any('/member/mlm/encashment/view/type/bank/add', 'Member\Mlm_EncashmentController@bank_add');//luke
 Route::any('/member/mlm/encashment/view/type/bank/archive', 'Member\Mlm_EncashmentController@bank_archive');//luke
 Route::any('/member/mlm/encashment/view/type/bank/edit/name', 'Member\Mlm_EncashmentController@bank_edit_name');//luke
+Route::any('/member/mlm/encashment/currency', 'Member\Mlm_EncashmentController@set_currency');
 
 Route::any('/member/mlm/encashment/add/to/list', 'Member\Mlm_EncashmentController@add_to_list');//luke
 Route::any('/member/mlm/encashment/add/to/list/date', 'Member\Mlm_EncashmentController@add_to_list_date');//luke

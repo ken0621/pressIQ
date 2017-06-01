6270b3877179241f2cecc24bc0d88b6f31212cea
lukzpogi <lukeglennjordan@gmail.com>
2017-06-01 14:27:26 +0800

    luke

---
+++ resources/views/member/item/pis/item_price_history.blade.php
@@ -0,0 +1,13 @@
+
+<div class="modal-header">
+	<button type="button" class="close" data-dismiss="modal">&times;</button>
+	<h4 class="modal-title">Item Price History</h4>
+</div>
+<div class="modal-body add_new_package_modal_body clearfix">
+    <div class="col-md-12">
+       {!! $text !!}
+    </div>
+</div>
+<div class="modal-footer ">
+    <div class="col-md-6 col-xs-6 pull-right"><button data-dismiss="modal" class="btn btn-def-white btn-custom-white form-control">Close</button></div>
+</div>  

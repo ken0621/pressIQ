35950c02630593f2a474339a5958d202861606d6
bryan <topherlapig@yahoo.com>
2017-06-13 17:06:20 +0800

    Test

--- app/Globals/Cart.php
+++ app/Globals/Cart.php
@@ -1003,12 +1003,12 @@ class Cart
 
             $itemAmount = new ItemAmount();
             $itemAmount->currency = "PHP";
-            $itemAmount->value = (string)number_format($product->evariant_price, 2);
+            $itemAmount->value = (string)$product->evariant_price;
             $itemAmount->details = $itemAmountDetails;
 
             $itemTotalAmount = new ItemAmount();
             $itemTotalAmount->currency = "PHP";
-            $itemTotalAmount->value = (string)number_format($product->evariant_price * $value["quantity"], 2);
+            $itemTotalAmount->value = (string)$product->evariant_price * $value["quantity"];
             $itemTotalAmount->details = $itemAmountDetails;
 
             $totalAmount->currency = "PHP";
@@ -1032,7 +1032,7 @@ class Cart
         $order_id = Cart::submit_order($shop_id, $payment_status, $order_status, isset($customer['customer_info']->customer_id) ? $customer['customer_info']->customer_id : null);
         Cart::clear_all($shop_id);
 
-        $totalAmount->value = number_format($total, 2);
+        $totalAmount->value = $total;
 
         $itemCheckout->items = $item;
         $itemCheckout->totalAmount = $totalAmount;

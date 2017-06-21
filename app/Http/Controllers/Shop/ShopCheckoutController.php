a5d3fe18445ad791cd7eef49bbe66a0eab83764c
bryan <topherlapig@yahoo.com>
2017-06-21 16:21:04 +0800

    Live

--- app/Http/Controllers/Shop/ShopCheckoutController.php
+++ app/Http/Controllers/Shop/ShopCheckoutController.php
@@ -207,8 +207,8 @@ class ShopCheckoutController extends Shop
             $update['order_status']   = "Processing";
             $update['payment_status'] = 1;
             $order = Ec_order::update_ec_order($update);
-            $logs["order_id"]         = $order_id;
-            DB::table("tbl_paymaya_logs")->update($logs);
+            // $logs["order_id"]         = $order_id;
+            // DB::table("tbl_paymaya_logs")->update($logs);
             $this->after_email_payment($order_id);
 
             if ($from == "checkout") 

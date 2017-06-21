c7fdf55480a515070bb7feff5b0138193f9d4040
lukzpogi <lukeglennjordan@gmail.com>
2017-06-21 13:43:10 +0800

    luke

--- app/Globals/Item_code.php
+++ app/Globals/Item_code.php
@@ -768,7 +768,7 @@ class Item_code
 
             if($tbl_ec_order_slot->order_slot_customer_id    != 0 &&  $tbl_ec_order_slot->order_slot_used           != 1)
             {
-                dd($tbl_ec_order_slot->order_slot_used);
+                dd($tbl_ec_order_slot);
                 $table_ec_order_slot = $tbl_ec_order_slot;
                 $tbl_ec_order_item = DB::table('tbl_ec_order_item')->where('ec_order_id', $order_id)
                 ->get();

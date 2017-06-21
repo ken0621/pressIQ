a9a4e725569baef8868b2ee091f72a023083ff7e
lukzpogi <lukeglennjordan@gmail.com>
2017-06-21 13:05:07 +0800

    luke

--- app/Globals/Item_code.php
+++ app/Globals/Item_code.php
@@ -799,11 +799,11 @@ class Item_code
                                     {
                                          $insert['slot_sponsor'] = $tbl_ec_order_slot->order_slot_sponsor;
                                     }
-                                    $id = Tbl_mlm_slot::insertGetId($insert);
-                                    $a = Mlm_compute::entry($id);
-
                                     $update_s['order_slot_used'] = 1;
                                     DB::table('tbl_ec_order_slot')->where('order_slot_ec_order_id', $order_id)->update($update_s);
+
+                                    $id = Tbl_mlm_slot::insertGetId($insert);
+                                    $a = Mlm_compute::entry($id);
 
                                     Mlm_member::add_to_session_edit($shop_id, $tbl_ec_order_slot->order_slot_customer_id, $id);
                                 }

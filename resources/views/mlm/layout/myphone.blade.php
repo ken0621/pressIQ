ea80d15c593b1e89599d9d8538e69399790023fd
Rikka Takanashi <edwardguevarra2003@gmail.com>
2017-06-24 05:29:11 +0000

    Ltrim

--- resources/views/mlm/layout/myphone.blade.php
+++ resources/views/mlm/layout/myphone.blade.php
@@ -155,7 +155,7 @@
                   <!-- menu profile quick info -->
                   <div class="profile clearfix">
                      <div class="profile_pic">
-                        <img src="{{$profile}}" alt="..." class="img-circle profile_img" height="65spx;">
+                        <img src="{{ ltrim($profile, '/') }}" alt="..." class="img-circle profile_img" height="65spx;">
                      </div>
                      <div class="profile_info">
                         <span>Magandang Araw (Good Day) ,</span>

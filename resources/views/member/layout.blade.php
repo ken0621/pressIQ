14ae2bd1821d8398230a4e440ab85731e2961248
Rikka Takanashi <edwardguevarra2003@gmail.com>
2017-06-27 06:28:35 +0000

    Check Bugs

--- resources/views/member/layout.blade.php
+++ resources/views/member/layout.blade.php
@@ -464,7 +464,7 @@
                     {
                         old_new_base = old_new_base + '/' + split[i];
                     }
-                    console.log(old_new_base);
+                    console.log(split);
                     settings.url = '{{url()}}' + old_new_base;
                 }
                 console.log("settings.url: " + settings.url);

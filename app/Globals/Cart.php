2d5552ec952054163b33f55f177ab5b6662dcb12
Rikka Takanashi <edwardguevarra2003@gmail.com>
2017-06-27 16:00:59 +0000

    myphone

--- app/Globals/Cart.php
+++ app/Globals/Cart.php
@@ -1062,7 +1062,7 @@ class Cart
                 }
             }
         }
-        
+        dd(Webhook::retrieve());
         $successWebhook = new Webhook();
         $successWebhook->name = Webhook::CHECKOUT_SUCCESS;
         $successWebhook->callbackUrl = URL::to("/payment/paymaya/webhook/success?order_id=" . Crypt::encrypt($order_id) . "&from=" . $from);

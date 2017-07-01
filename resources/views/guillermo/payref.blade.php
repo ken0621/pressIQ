bbfa5a45bc181e3a347f482a276cc8a3614aa0c6
Rikka Takanashi <edwardguevarra2003@gmail.com>
2017-07-01 01:32:25 +0000

    paymayachecking

--- resources/views/guillermo/payref.blade.php
+++ resources/views/guillermo/payref.blade.php
@@ -95,7 +95,7 @@
                 {
                     alert("Payamaya details updated successfully.");
                 }
-                else if($(".tr-order[order_id=" + $order_id + "]").find(".response").html() != "")
+                else if($(".tr-order[order_id=" + $order_id + "]").find(".response").html() != "" || $(".tr-order[order_id=" + $order_id + "]").find(".response").html() != "PENDING")
                 {
                     $(".tr-order[order_id=" + $order_id + "]").find(".status").html('<i style="color: green;" class="glyphicon glyphicon-check"></i>');
                     setTimeout(function()

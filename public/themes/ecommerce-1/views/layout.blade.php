659f0fd0b2dea2a94f663bc490b107db91b0147e
lukzpogi <lukeglennjordan@gmail.com>
2017-07-05 17:26:04 +0800

    luke

--- public/themes/ecommerce-1/views/layout.blade.php
+++ public/themes/ecommerce-1/views/layout.blade.php
@@ -103,7 +103,7 @@
                 <div class="holder"><a href="http://202.54.157.7/PhilTechInc/BKWLTOlogin.aspx">Travel and Tours</a></div>
                 <div class="holder"><div class="linya"></div></div>
                 <div class="holder"><a href="https://philtechglobalinc.vmoney.com">E-money</a></div>
-                <div class="holder"><div class="linya"></div></div>
+                <!-- <div class="holder"><div class="linya"></div></div> -->
     		</div>
     	</div>
     	<div class="header-nav-middle">

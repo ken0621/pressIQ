c3653c576c7990dc32eb4eb3705ffe3e924dffa9
jimarzape@gmail.com <jimarzape@gmail.com>
2017-07-07 16:20:13 +0800

    push

--- resources/views/member/payroll/reload/reload_report_table.blade.php
+++ resources/views/member/payroll/reload/reload_report_table.blade.php
@@ -2,30 +2,26 @@
 	<thead>
 		<tr>
 			<th></th>
-			@if(isset($_columns))
-				@foreach($_columns as $column)
-				<th class="block">{{$column}}</th>
-				@endforeach
-			@endif
+			@foreach($data['_columns'] as $column)
+			<th class="block">{{$column}}</th>
+			@endforeach
 		</tr>
 	</thead>
 	<tbody>
-		@if(isset($_columns))
-			@foreach($_emp as $emp)
-			<tr>
-				<td>{!!$emp['name']!!}</td>
-				@foreach($emp['_record'] as $record)
-				<td class="text-right">
-					{!!$record!!}
-				</td>
-				@endforeach
-			</tr>
-			
+		@foreach($data['_emp'] as $emp)
+		<tr>
+			<td>{!!$emp['name']!!}</td>
+			@foreach($emp['_record'] as $record)
+			<td class="text-right">
+				{!!$record!!}
+			</td>
 			@endforeach
-		@endif
+		</tr>
+		
+		@endforeach
 		<tr>
 			<td><b>Total</b></td>
-			@foreach($_total as $total)
+			@foreach($data['_total'] as $total)
 			<td class="text-right">
 				<b>{{number_format($total, 2)}}</b>
 			</td>

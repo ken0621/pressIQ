d631d5d02a0d30be505058a7eb310addbcdf7b7b
jimarzape@gmail.com <jimarzape@gmail.com>
2017-07-07 16:18:18 +0800

    push

--- app/Http/Controllers/Member/PayrollController.php
+++ app/Http/Controllers/Member/PayrollController.php
@@ -6965,7 +6965,7 @@ class PayrollController extends Member
 
           $data = Self::generate_custom_report($id, $date);
 
-          // dd($data);
+          dd($data);
           return view('member.payroll.payroll_view_report', $data);
      }
 
@@ -6977,7 +6977,6 @@ class PayrollController extends Member
           $payroll_reports_id = Request::input('payroll_reports_id');
 
           $data = Self::generate_custom_report($payroll_reports_id, $date);
-          // dd($data);
           return view('member.payroll.reload.reload_report_table', $data);
      }
 

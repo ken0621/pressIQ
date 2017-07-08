68320ade23b1090679823f5986338fc7f3cdad69
arcy <arcylen103095@gmai.com>
2017-07-08 18:22:08 +0800

    arcy

--- app/Http/Controllers/Member/TabletSyncController.php
+++ app/Http/Controllers/Member/TabletSyncController.php
@@ -89,7 +89,9 @@ class TabletSyncController extends Controller
      */
     public function sync($table, $date)
     {
-
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Headers: Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With');
+        header('Access-Control-Allow-Methods: GET, PUT, POST');
         $return = [];
         if($table == "tbl_shop")
         {

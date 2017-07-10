045d51a888bdcc60614ac7b8d29b90b811fb132d
Guillermo Tabligan <gtplus.net@gmail.com>
2017-07-10 07:02:39 +0000

    luke

--- resources/views/mlm/eon/index.blade.php
+++ resources/views/mlm/eon/index.blade.php
@@ -23,8 +23,8 @@ $data['icon'] = 'icon-credit-card';
                             <tr>
                                 <td>{{$value->slot_no}}</td>
                                 <td><input type="text" class="form-control slot_eon_{{$value->slot_id}}" name="slot_eon" value="@if(!$value->slot_eon){{$customer_info_active->first_name . ' ' . $customer_info_active->middle_name . ' ' .  $customer_info_active->last_name  }}@else{{$value->slot_eon}}@endif"></td>
-                                <td><input type="tel" id="txt_cardNumber" pattern="[0-9.]+" onkeypress="return checkDigit(event, $(this))"class="form-control slot_eon_account_no_{{$value->slot_id}}" name="slot_eon_account_no" value="{{$value->slot_eon_account_no}}"></td>
-                                <td><input type="tel" id="txt_cardNumber" pattern="[0-9.]+" onkeypress="return checkDigit(event, $(this))" class="form-control slot_eon_card_no_{{$value->slot_id}}" name="slot_eon_card_no" value="{{$value->slot_eon_card_no}}"></td>
+                                <td><input type="tel" placeholder="000000000000" id="txt_cardNumber" pattern="[0-9.]+" onkeypress="return checkDigitaccnt(event, $(this))"class="form-control slot_eon_account_no_{{$value->slot_id}}" name="slot_eon_account_no" value="{{$value->slot_eon_account_no}}"></td>
+                                <td><input type="tel" placeholder="0000000000000000" id="txt_cardNumber" pattern="[0-9.]+" onkeypress="return checkDigit(event, $(this))" class="form-control slot_eon_card_no_{{$value->slot_id}}" name="slot_eon_card_no" value="{{$value->slot_eon_card_no}}"></td>
                                 <td>
                                     <button class="btn btn-primary" onClick="update_eon({{$value->slot_id}})">
                                       <i class="fa fa-check" aria-hidden="true"></i>
@@ -57,14 +57,14 @@ $data['icon'] = 'icon-credit-card';
     
     var slot_eon_account_no_hidden_l = $('.slot_eon_account_no_hidden').val();
     var slot_eon_card_no_hidden_l = $('.slot_eon_card_no_hidden').val();
-    // if( slot_eon_account_no_hidden_l.length == 16 && slot_eon_card_no_hidden_l.length == 16)
-    // {
+    if( slot_eon_account_no_hidden_l.length == 12 && slot_eon_card_no_hidden_l.length == 16)
+    {
         $('.form_update_eon').submit();
-    // }
-    // else
-    // {
-    //     toastr.warning('Card #/ Account # must be 16 digits');
-    // }
+    }
+    else
+    {
+        toastr.warning('Card #/ Account # must be 16 digits');
+    }
     
  }
  function submit_done(messagee){
@@ -88,7 +88,19 @@ function checkDigit (e, ito) {
     {
         return false;
     }
-    // $(this).val(current);
+    return true;
+}
+function checkDigitaccnt(e, ito)
+{
+    if ((e.which < 48 || e.which > 57) && (e.which !== 8) && (e.which !== 0)) {
+        return false;
+    }
+
+    var current = $(ito).val();
+    if(current.length >= 12)
+    {
+        return false;
+    }
     return true;
 }
 

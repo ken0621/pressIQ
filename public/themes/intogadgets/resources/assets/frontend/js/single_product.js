8a87cef8b2ea11ea156d68f0cd103a523eb17d5e
arcy <arcylen103095@gmai.com>
2017-07-11 16:32:20 +0800

    arcy

--- public/themes/intogadgets/resources/assets/frontend/js/single_product.js
+++ public/themes/intogadgets/resources/assets/frontend/js/single_product.js
@@ -28,8 +28,9 @@ $(document).ready(function()
 
 function action_zoom_image()
 {
-	$(".single-product-img").elevateZoom({
-		zoomLens : false
+	// $(".single-product-img").removeClass("first-img");
+	// $(".single-product-img.key-0").addClass("first-img");
+	$(".first-img").elevateZoom({
 	});
 }
 
@@ -47,8 +48,9 @@ function event_change_image()
 
 		image_crop(".4-3-ratio", 4, 3);
 
-		$(".single-product-img").elevateZoom({
-			zoomLens : false
+		$(".single-product-img").removeClass("first-img");
+		// $(".single-product-img.key-0").addClass("first-img");
+		$(".first-img").elevateZoom({
 		});
 	});
 }
@@ -131,6 +133,12 @@ function action_select_variation(e)
 			
 			$('.single-product-content').addClass("hide");
 			$('.single-product-content[variant-id="'+variant_id+'"]').removeClass("hide");
+
+			$(".single-product-img").removeClass("first-img");
+			$('.single-product-img[variant-id="'+variant_id+'"]').addClass("first-img");
+
+			$(".first-img").elevateZoom({
+			});
 			$('.4-3-ratio').keepRatio({ ratio: 4/3, calculate: 'height' });
 			event_slick();
 

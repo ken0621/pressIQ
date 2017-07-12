6f00c0d67ada9746936a5bc57d3aae96667a93a8
bryan <topherlapig@yahoo.com>
2017-07-12 17:50:42 +0800

    Finished

--- app/Http/Controllers/Mlm/MlmWalletVMoneyController.php
+++ app/Http/Controllers/Mlm/MlmWalletVMoneyController.php
@@ -22,107 +22,137 @@ class MlmWalletVMoneyController extends Mlm
 {
     public function index()
     {
-        $data["_logs"] = DB::table("tbl_vmoney_wallet_logs")->where("customer_id", Self::$customer_id)->get();
+        if (Self::$slot_id) 
+        {
+            $data["_logs"] = DB::table("tbl_vmoney_wallet_logs")->where("customer_id", Self::$customer_id)->get();
+            $data["wallet"] = Mlm_slot_log::get_sum_wallet(Self::$slot_id);
+            $data["minimum"] = DB::table("tbl_settings")->where("shop_id", Self::$shop_id)->where("settings_key", "vmoney_minimum_encashment")->first()->settings_value;
 
-        return view('mlm.vmoney.index', $data);
+            return view('mlm.vmoney.index', $data);
+        }
+        else
+        {
+            dd("Slot not found");
+        }
     }
     
     public function transfer()
     {
         $slot = DB::table("tbl_mlm_slot")->where("slot_id", Self::$slot_id)->first();
-
+        $wallet = Mlm_slot_log::get_sum_wallet($slot->slot_id);
+        $minimum_encashment = DB::table("tbl_settings")->where("shop_id", Self::$shop_id)->where("settings_key", "vmoney_minimum_encashment")->first()->settings_value;
+        
         if (isset($slot) && $slot) 
         {
-            if (Request::input('vmoney_email')) 
+            if ($minimum_encashment < Request::input("wallet_amount")) 
             {
-                if(Request::input('wallet_amount'))
+                if ($wallet > Request::input('wallet_amount')) 
                 {
-                    /* Set URL Sandbox or Live */
-                    $url = "http://test.vmoney.com/gtcvbankmerchant/";
-                    
-                    /* API */
-                    $post = 'mxtransfer.svc';
-                    $environment = "sandbox";
-
-                    if ($environment == "sandbox") 
+                    if (Request::input('vmoney_email')) 
                     {
-                        $pass["apiKey"] = 'Vqzs90pKLb6iwsGQhnRS'; // Vendor API Key issued by VMoney
-                        $pass["merchantId"] = 'M239658948226'; // Merchant ID registered within VMoney
-                    }
-                    else
-                    {
-                        $pass["apiKey"] = 'z9Gy1dBbnyj9cxMqXSKF'; // Vendor API Key issued by VMoney
-                        $pass["merchantId"] = 'M132582139240'; // Merchant ID registered within VMoney
-                    }
+                        if(Request::input('wallet_amount'))
+                        {
+                            /* Set URL Sandbox or Live */
+                            $url = "http://test.vmoney.com/gtcvbankmerchant/";
+                            
+                            /* API */
+                            $post = 'mxtransfer.svc';
+                            $environment = DB::table("tbl_settings")->where("shop_id", Self::$shop_id)->where("settings_key", "vmoney_environment")->first()->settings_value;
 
-                    $pass["recipient"] = Request::input("vmoney_email"); // Recipient's email address
-                    $pass["merchantRef"] = Self::$customer_id . time(); // Merchant reference number
-                    $pass["amount"] = Request::input("wallet_amount"); // Amount of the transaction
-                    $pass["currency"] = 'PHP'; // Currency being transferred (ie PHP)
-                    $pass["message"] = 'Philtech VMoney Wallet Transfer'; // Memo or notes for transaction
+                            /* Sandbox */
+                            if ($environment == 0) 
+                            {
+                                $pass["apiKey"] = 'Vqzs90pKLb6iwsGQhnRS'; // Vendor API Key issued by VMoney
+                                $pass["merchantId"] = 'M239658948226'; // Merchant ID registered within VMoney
+                            }
+                            /* Production */
+                            else
+                            {
+                                $pass["apiKey"] = 'z9Gy1dBbnyj9cxMqXSKF'; // Vendor API Key issued by VMoney
+                                $pass["merchantId"] = 'M132582139240'; // Merchant ID registered within VMoney
+                            }
 
-                    $post_params = $url . $post . "?" . http_build_query($pass);
+                            $pass["recipient"] = Request::input("vmoney_email"); // Recipient's email address
+                            $pass["merchantRef"] = Self::$customer_id . time(); // Merchant reference number
+                            $pass["amount"] = Request::input("wallet_amount"); // Amount of the transaction
+                            $pass["currency"] = 'PHP'; // Currency being transferred (ie PHP)
+                            $pass["message"] = 'Philtech VMoney Wallet Transfer'; // Memo or notes for transaction
 
-                    try 
-                    {
-                        $client = new Client();
-                        $response = $client->post($post_params, $pass);
-                        $stream = $response->getBody();
-                        $contents = $stream->getContents(); // returns all the contents
-                        $contents = $stream->getContents(); // empty string
-                        $stream->rewind(); // Seek to the beginning
-                        $contents = $stream->getContents(); // returns all the contents
-                        $data_decoded = json_decode($contents);
+                            $post_params = $url . $post . "?" . http_build_query($pass);
 
-                        /* Result */
-                        if ($data_decoded->resultCode == "000") 
-                        {   
-                            $data_a['status'] = "success";
-                            $logs["status"] = 1;
+                            try 
+                            {
+                                $client = new Client();
+                                $response = $client->post($post_params, $pass);
+                                $stream = $response->getBody();
+                                $contents = $stream->getContents(); // returns all the contents
+                                $contents = $stream->getContents(); // empty string
+                                $stream->rewind(); // Seek to the beginning
+                                $contents = $stream->getContents(); // returns all the contents
+                                $data_decoded = json_decode($contents);
 
-                            $arry_log['wallet_log_slot'] = $slot->slot_id;
-                            $arry_log['shop_id'] = $slot->shop_id;
-                            $arry_log['wallet_log_slot_sponsor'] = $slot->slot_id;
-                            $arry_log['wallet_log_details'] = 'You have transfered ' . $pass["amount"] . ' To your E-wallet. ' . $pass["amount"] . ' is deducted to your wallet.';
-                            $arry_log['wallet_log_amount'] = -($pass["amount"]);
-                            $arry_log['wallet_log_plan'] = "V_MONEY";
-                            $arry_log['wallet_log_status'] = "released";   
-                            $arry_log['wallet_log_claimbale_on'] = Carbon::now();
+                                /* Result */
+                                if ($data_decoded->resultCode == "000") 
+                                {   
+                                    $data_a['status'] = "success";
+                                    $logs["status"] = 1;
 
-                            Mlm_slot_log::slot_array($arry_log);
+                                    $arry_log['wallet_log_slot'] = $slot->slot_id;
+                                    $arry_log['shop_id'] = $slot->shop_id;
+                                    $arry_log['wallet_log_slot_sponsor'] = $slot->slot_id;
+                                    $arry_log['wallet_log_details'] = 'You have transfered ' . $pass["amount"] . ' To your E-Money. ' . $pass["amount"] . ' is deducted to your wallet.';
+                                    $arry_log['wallet_log_amount'] = -($pass["amount"]);
+                                    $arry_log['wallet_log_plan'] = "E_MONEY";
+                                    $arry_log['wallet_log_status'] = "released";   
+                                    $arry_log['wallet_log_claimbale_on'] = Carbon::now();
+
+                                    Mlm_slot_log::slot_array($arry_log);
+                                }
+                                else
+                                {
+                                    $data_a['status'] = "error";
+                                    $logs["status"] = 0;
+                                }
+                                
+                                $data_a['message'] = $data_decoded->resultMsg;
+
+                                $logs["vmoney_wallet_logs_date"] = Carbon::now();
+                                $logs["vmoney_wallet_logs_email"] = Request::input("vmoney_email");
+                                $logs["vmoney_wallet_logs_amount"] = Request::input("wallet_amount");
+                                $logs["customer_id"] = Self::$customer_id;
+                                $logs["txnId"] = isset($data_decoded->txnId) ? $data_decoded->txnId : "None";
+                                $logs["merchantRef"] = isset($data_decoded->merchantRef) ? $data_decoded->merchantRef : "None";
+                                $logs["message"] = isset($data_decoded->resultMsg) ? $data_decoded->resultMsg : "None";
+                                Tbl_vmoney_wallet_logs::insert($logs);
+                            } 
+                            catch (\Exception $e) 
+                            {
+                                $data_a['status'] = "error";
+                                $data_a['message'] = 'Caught exception: ' .  $e->getMessage();    
+                            }
                         }
                         else
                         {
                             $data_a['status'] = "error";
-                            $logs["status"] = 0;
+                            $data_a['message'] = "Wallet Amount is required";  
                         }
-                        
-                        $data_a['message'] = $data_decoded->resultMsg;
-
-                        $logs["vmoney_wallet_logs_date"] = Carbon::now();
-                        $logs["vmoney_wallet_logs_email"] = Request::input("vmoney_email");
-                        $logs["vmoney_wallet_logs_amount"] = Request::input("wallet_amount");
-                        $logs["customer_id"] = Self::$customer_id;
-                        $logs["txnId"] = $data_decoded->txnId;
-                        $logs["merchantRef"] = $data_decoded->merchantRef;
-                        Tbl_vmoney_wallet_logs::insert($logs);
-                    } 
-                    catch (\Exception $e) 
+                    }
+                    else
                     {
                         $data_a['status'] = "error";
-                        $data_a['message'] = 'Caught exception: ' .  $e->getMessage();    
+                        $data_a['message'] = "Email Recipient is required";   
                     }
                 }
                 else
                 {
                     $data_a['status'] = "error";
-                    $data_a['message'] = "Wallet Amount is required";  
+                    $data_a['message'] = "Not enough wallet";   
                 }
             }
             else
             {
                 $data_a['status'] = "error";
-                $data_a['message'] = "Email Recipient is required";   
+                $data_a['message'] = "The minimum_encashment is PHP. " . number_format($minimum_encashment, 2);   
             }
         }
         else

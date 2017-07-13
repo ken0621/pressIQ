ca612d3bdbed4690fa0710d748a3d1ab1790378b
arcy <arcylen103095@gmai.com>
2017-07-13 13:23:31 +0800

    intogadgets

--- public/themes/intogadgets/views/product.blade.php
+++ public/themes/intogadgets/views/product.blade.php
@@ -77,7 +77,7 @@
         <div class="holder sshide">
             <div class="title text-left">Top Rated Products</div>
             @foreach(get_collection(get_content($shop_theme_info, "product", "top_rated_products"), $shop_id) as $collection)
-                <a href="/product/view/{{ $collection['eprod_id'] }}" class="text">
+                <a href="/product/view/{{ $collection['ec_product_id'] }}" class="text">
                     <div class="product-top">
                         <div class="text">
                             <div class="name">{{ get_collection_first_name($collection) }}</div>
@@ -91,7 +91,7 @@
         <div class="holder sshide">
             <div class="title text-left">Most Viewed</div>
             @foreach(get_collection(get_content($shop_theme_info, "product", "most_viewed"), $shop_id) as $collection)
-                <a href="/product/view/{{ $collection['eprod_id'] }}" class="text">
+                <a href="/product/view/{{ $collection['ec_product_id'] }}" class="text">
                     <div class="product-top">
                         <div class="text">
                             <div class="name">{{ get_collection_first_name($collection) }}</div>

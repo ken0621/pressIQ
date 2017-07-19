840b769841a3506d7421c4db3d2692f8e54ac932
Ezarule <guevarra129@gmail.com>
2017-07-19 18:20:15 +0800

    commit

--- resources/views/member/mlm_product_code/mlm_product_code_customer_slot.blade.php
+++ resources/views/member/mlm_product_code/mlm_product_code_customer_slot.blade.php
@@ -1,4 +1,4 @@
-<label>Customer</label>
+<label>Slot No</label>
 <select name="membership_code" class="form-control chosen-select slot_id_container" onChange="bar_code_membership_code(this)">
   @foreach($_slot as $slot)
     <option value="{{$slot->slot_no}}">{{$slot->slot_no}}</option>

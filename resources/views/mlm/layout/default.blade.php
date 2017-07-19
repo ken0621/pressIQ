97d3b64ff8d1294cba7fc330dd46270c23ba1659
bryan <topherlapig@yahoo.com>
2017-07-19 13:27:25 +0800

    Enable for ALl

--- resources/views/mlm/layout/default.blade.php
+++ resources/views/mlm/layout/default.blade.php
@@ -500,7 +500,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                                     Airline Wallet
                                 </a> 
                             @endif
-                            @if($shop_info->shop_wallet_vmoney == 1 && $slot_now->slot_id == 2)
+                            @if($shop_info->shop_wallet_vmoney == 1)
                                 <a href="/mlm/wallet/vmoney" class="subnav-text">
                                     E-Money Wallet
                                 </a> 

6d5052e81c378a4111e7787a40ddf4a50448b8dc
arcy <arcylen103095@gmai.com>
2017-07-22 17:28:57 +0800

    arrange stocks

--- app/Globals/Warehouse.php
+++ app/Globals/Warehouse.php
@@ -288,7 +288,6 @@ class Warehouse
     {
     	$data = Tbl_warehouse::Warehouseitem()
                              ->select_inventory($warehouse_id)
-				    		 ->orderBy('product_warehouse_stocks','DESC')
     						 ->paginate(10);
         foreach($data as $key => $value)
         {   //cycy
--- app/Http/Controllers/Member/WarehouseController.php
+++ app/Http/Controllers/Member/WarehouseController.php
@@ -112,7 +112,7 @@ class WarehouseController extends Member
         $access = Utilities::checkAccess('item-warehouse', 'access_page');
         if($access == 1)
         { 
-            $data["_warehouse"] = Tbl_warehouse::inventory()->select_info($this->user_info->shop_id, 0)->groupBy("tbl_warehouse.warehouse_id")->get();
+            $data["_warehouse"] = Tbl_warehouse::inventory()->select_info($this->user_info->shop_id, 0)->groupBy("tbl_warehouse.warehouse_id")->take(1)->get();
             if(Request::input("search_txt"))
             {
                $data["_warehouse"] = Tbl_warehouse::inventory()->select_info($this->user_info->shop_id, 0)->where("warehouse_name","LIKE","%".Request::input("search_txt")."%")->groupBy("tbl_warehouse.warehouse_id")->get();            

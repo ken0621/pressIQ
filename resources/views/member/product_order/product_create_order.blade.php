2dd1e7bc3dad09168b228f3403d3dffb05c3e077
Edward Guevarra <edward.guevarra@digimaweb.solutions>
2017-07-25 17:54:18 +0800

    Show Mobile Number in Order

--- resources/views/member/product_order/product_create_order.blade.php
+++ resources/views/member/product_order/product_create_order.blade.php
@@ -65,6 +65,12 @@
                                 <label>Invoice Number</label>
                                 <input type="text" class="form-control input-sm" name="manual_inv_number" value="{{$inv->manual_inv_number != '' ? $inv->manual_inv_number : $inv->ec_order_id }}">
                             </div>
+                            @if(isset($log_name) && isset($log_reference))
+                            <div class="col-sm-4">
+                                <label>{{ $log_name }}</label>
+                                <input type="text" disabled value="{{ $log_reference }}" class="form-control">
+                            </div>
+                            @endif
                         </div>
                         <div class="row clearfix">
                             <div class="col-sm-4">
@@ -77,12 +83,10 @@
                                 <label>Customer Email</label>
                                 <input {{isset($inv) ? 'disabled' : ''}} type="text" class="form-control input-sm customer-email" name="inv_customer_email" placeholder="E-Mail (Separate E-Mails with comma)" value="{{$inv->customer_email or ''}}"/>
                             </div> 
-                            @if(isset($log_name) && isset($log_reference))
                             <div class="col-sm-4">
-                                <label>{{ $log_name }}</label>
-                                <input type="text" disabled value="{{ $log_reference }}" class="form-control">
-                            </div>
-                            @endif
+                                <label>Customer Mobile</label>
+                                <input {{isset($inv) ? 'disabled' : ''}} type="text" class="form-control input-sm customer-mobile" name="inv_customer_mobile" placeholder="Mobile Number" value="{{$inv->customer_mobile or ''}}"/>
+                            </div> 
                         </div>
                     </div>
                     

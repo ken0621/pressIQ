da398fc726d98005fdf4d260de3cdc1a04d375ce
Guillermo Tabligan <guillermotabligan@gmail.com>
2017-07-26 09:11:35 +0800

    change

--- resources/views/member/payroll2/employee_timesheet.blade.php
+++ resources/views/member/payroll2/employee_timesheet.blade.php
@@ -99,3 +99,10 @@
         width: 1100px !important;
     }
 </style>
+
+<div class="view-debug-mode modal-footer">
+    <div onclick='$(".debug-view").removeClass("hidden")' style="text-align: center; cursor: pointer; color: #005fbf">DEBUG MODE (DEVELOPER ONLY) &nbsp; <i class="fa fa-caret-down"></i></div>
+    <div class="debug-view hidden text-left" style="padding-top: 10px;">
+        {{ dd($_timesheet) }}
+    </div>
+</div>

822a29712da4906aa46b299b82dc253726fd6571
arcy <arcylen103095@gmai.com>
2017-08-01 15:00:14 +0800

    arc

--- app/Globals/AuditTrail.php
+++ app/Globals/AuditTrail.php
@@ -83,7 +83,7 @@ class AuditTrail
     }
     public static function getSearchAuditData($col, $key, $from=null, $to=null)
     {      
-        $query = Tbl_audit_trail::user()->orderBy("created_at","DESC")->where("audit_shop_id",AuditTrail::getShopId());   
+        $query = Tbl_audit_trail::user()->orderBy("tbl_audit_trail.created_at","DESC")->where("audit_shop_id",AuditTrail::getShopId());   
 
         switch ($col) {            
             case 'remarks':

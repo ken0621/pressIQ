bfbddc7130104f463cb5ceec91b31939575f6235
Guillermo Tabligan <guillermotabligan@gmail.com>
2017-08-01 08:31:42 +0800

    change

--- resources/views/member/payroll/payroll_timekeeping_table_processed.blade.php
+++ resources/views/member/payroll/payroll_timekeeping_table_processed.blade.php
@@ -2,8 +2,10 @@
     <thead style="text-transform: uppercase">
         <tr>
             <th class="text-center">Date Covered</th>
+            <th class="text-center">Period</th>
             <th class="text-center">Company</th>
             <th class="text-center">Total Net Salary</th>
+            <th class="text-center">Grand Total</th>
             <th class="text-center"></th>
         </tr>
     </thead>
@@ -13,10 +15,13 @@
     		<tr>
     			<td class="text-center">
                     {{ date('M d, Y',strtotime($period->payroll_period_start)).' - '.date('M d, Y',strtotime($period->payroll_period_end)) }}
-                    <div>{{ $period->payroll_period_category }} ({{ code_to_word($period->period_count) }} of {{ $period->month_contribution }}  {{ $period->year_contribution }})</div>
+                </td>
+                <td class="text-center">
+                    {{ $period->payroll_period_category }} ({{ code_to_word($period->period_count) }} of {{ $period->month_contribution }}  {{ $period->year_contribution }})
                 </td>
     			<td class="text-center">{{ $period->payroll_company_name }}</td>
-                <td class="text-center"><a href="javascript:">{{ payroll_currency($period->payroll_period_total_net) }}</a></td>
+                <td class="text-center">{{ payroll_currency($period->payroll_period_total_net) }}</td>
+                <td class="text-center"><a href="javascript:">{{ payroll_currency($period->payroll_period_total_grand) }}</a></td>
     			<td class="text-center"><a href="javascript:">VIEW PAYSLIP</a></td>
     		</tr>
     		@endforeach

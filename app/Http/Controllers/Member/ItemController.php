77aa6143ad4fc28c8a8a4ce353894cd79be5687b
lukeglennjordan <lukeglennjordan@gmail.com>
2017-08-02 18:05:56 +0800

    luke

--- app/Http/Controllers/Member/ItemController.php
+++ app/Http/Controllers/Member/ItemController.php
@@ -48,42 +48,88 @@ class ItemController extends Member
         $data['can_approve_item_request'] = Utilities::checkAccess('item-list', 'can_approve_item_request');
 		$data['can_edit_other_item'] = Utilities::checkAccess('item-list', 'can_edit_other_item');
 		$data['user_id'] = $this->user_info->user_id;
+		$data['is_merchant'] = $this->user_info->user_is_merchant;
         if($access == 1)
         {
 			$shop_id        		   = $this->user_info->shop_id;
 			$warehouse_id 			   = Tbl_warehouse::where("main_warehouse", 1)->where("warehouse_shop_id", $this->user_info->shop_id)->pluck("warehouse_id");
-	        $item 		    		   = Tbl_item::inventory()
-	        							->leftJoin("tbl_item_merchant_request","tbl_item_merchant_request.merchant_item_id","=","tbl_item.item_id")
-	        							->leftjoin('tbl_user', 'tbl_user.user_id','=', 'tbl_item_merchant_request.item_merchant_requested_by')
-	        							->where("tbl_item.archived",0)->where("shop_id",$shop_id)->type()->category();
+	        
+			if($data['is_merchant'] == 1)
+			{
+				$item 		    		   = Tbl_item::inventory()
+		        							->leftJoin("tbl_item_merchant_request","tbl_item_merchant_request.merchant_item_id","=","tbl_item.item_id")
+		        							->leftjoin('tbl_user', 'tbl_user.user_id','=', 'tbl_item_merchant_request.item_merchant_requested_by')
+		        							// ->leftJoin("tbl_item_merchant_request","tbl_item_merchant_request.merchant_item_id","=","tbl_item.item_id")
+		        							// ->leftjoin('tbl_user', 'tbl_user.user_id','=', 'tbl_item_merchant_request.item_merchant_requested_by')  
+		        							->where("tbl_item.archived",0)->where("shop_id",$shop_id)
+		        							->where(function($query){
+										         $query->where('item_merchant_request_status',"=","Approved");
+											})
+		        							->where('item_merchant_requested_by', $data['user_id'])
+											->type()->category();
 
-	        /* CHECK IF THE ITEM IS ON MERCHANT REQUESTED ITEM THEN IT WOULDN'T BE INCLUDED ON ARCHIVED ITEM WHEN IT IS STILL ON PENDING STATUS */
-	        $item_archived  		   = Tbl_item::where("tbl_item.archived",1)
-	        								     ->where("shop_id",$shop_id)
-	        								     ->type()
-	        								     ->leftJoin("tbl_item_merchant_request","tbl_item_merchant_request.merchant_item_id","=","tbl_item.item_id")
-	        								     ->leftjoin('tbl_user', 'tbl_user.user_id','=', 'tbl_item_merchant_request.item_merchant_requested_by')
-	        								     ->category()
-											     ->where(function($query)
-											     {
-											         $query->where('item_merchant_request_status',"!=","PENDING");
-											         $query->orWhereNull('item_merchant_request_status');
-											     });
+		        /* CHECK IF THE ITEM IS ON MERCHANT REQUESTED ITEM THEN IT WOULDN'T BE INCLUDED ON ARCHIVED ITEM WHEN IT IS STILL ON PENDING STATUS */
+		        $item_archived  		   = Tbl_item::where("tbl_item.archived",1)
+		        								     ->where("shop_id",$shop_id)
+		        								     ->type()
+		        								     ->leftJoin("tbl_item_merchant_request","tbl_item_merchant_request.merchant_item_id","=","tbl_item.item_id")
+		        								     ->leftjoin('tbl_user', 'tbl_user.user_id','=', 'tbl_item_merchant_request.item_merchant_requested_by')
+		        								     ->category()
+		        								     ->where('item_merchant_requested_by', $data['user_id'])
+												     ->where(function($query)
+												     {
+												         $query->where('item_merchant_request_status',"!=","PENDING");
+												         $query->orWhereNull('item_merchant_request_status');
+												     });
 
-	        $item_pending  		       = Tbl_item::where("tbl_item.archived",1)
-        								         ->where("tbl_item.shop_id",$shop_id)
-        								         ->type()
-        								         ->leftJoin("tbl_item_merchant_request","tbl_item_merchant_request.merchant_item_id","=","tbl_item.item_id")
-        								         ->leftJoin("tbl_warehouse","tbl_warehouse.warehouse_id","=","tbl_item_merchant_request.merchant_warehouse_id")
-        								         ->leftJoin("tbl_customer","tbl_customer.customer_id","=","tbl_item_merchant_request.item_merchant_requested_by")
-        								         ->leftjoin('tbl_user', 'tbl_user.user_id','=', 'tbl_item_merchant_request.item_merchant_requested_by')
-        								         ->category()
-										         ->where(function($query)
-										         {
-										             $query->where('item_merchant_request_status',"PENDING");
-										         });
+		        $item_pending  		       = Tbl_item::where("tbl_item.archived",1)
+	        								         ->where("tbl_item.shop_id",$shop_id)
+	        								         ->type()
+	        								         ->leftJoin("tbl_item_merchant_request","tbl_item_merchant_request.merchant_item_id","=","tbl_item.item_id")
+	        								         ->leftJoin("tbl_warehouse","tbl_warehouse.warehouse_id","=","tbl_item_merchant_request.merchant_warehouse_id")
+	        								         ->leftJoin("tbl_customer","tbl_customer.customer_id","=","tbl_item_merchant_request.item_merchant_requested_by")
+	        								         ->leftjoin('tbl_user', 'tbl_user.user_id','=', 'tbl_item_merchant_request.item_merchant_requested_by')
+	        								         ->category()
+	        								         ->where('item_merchant_requested_by', $data['user_id'])
+											         ->where(function($query)
+											         {
+											             $query->where('item_merchant_request_status',"PENDING");
+											         });
+			}
+			else
+			{
+		        $item 		    		   = Tbl_item::inventory()
+		        							->leftJoin("tbl_item_merchant_request","tbl_item_merchant_request.merchant_item_id","=","tbl_item.item_id")
+		        							->leftjoin('tbl_user', 'tbl_user.user_id','=', 'tbl_item_merchant_request.item_merchant_requested_by')
+		        							->where("tbl_item.archived",0)->where("shop_id",$shop_id)->type()->category();
 
-										     
+		        /* CHECK IF THE ITEM IS ON MERCHANT REQUESTED ITEM THEN IT WOULDN'T BE INCLUDED ON ARCHIVED ITEM WHEN IT IS STILL ON PENDING STATUS */
+		        $item_archived  		   = Tbl_item::where("tbl_item.archived",1)
+		        								     ->where("shop_id",$shop_id)
+		        								     ->type()
+		        								     ->leftJoin("tbl_item_merchant_request","tbl_item_merchant_request.merchant_item_id","=","tbl_item.item_id")
+		        								     ->leftjoin('tbl_user', 'tbl_user.user_id','=', 'tbl_item_merchant_request.item_merchant_requested_by')
+		        								     ->category()
+												     ->where(function($query)
+												     {
+												         $query->where('item_merchant_request_status',"!=","PENDING");
+												         $query->orWhereNull('item_merchant_request_status');
+												     });
+
+		        $item_pending  		       = Tbl_item::where("tbl_item.archived",1)
+	        								         ->where("tbl_item.shop_id",$shop_id)
+	        								         ->type()
+	        								         ->leftJoin("tbl_item_merchant_request","tbl_item_merchant_request.merchant_item_id","=","tbl_item.item_id")
+	        								         ->leftJoin("tbl_warehouse","tbl_warehouse.warehouse_id","=","tbl_item_merchant_request.merchant_warehouse_id")
+	        								         ->leftJoin("tbl_customer","tbl_customer.customer_id","=","tbl_item_merchant_request.item_merchant_requested_by")
+	        								         ->leftjoin('tbl_user', 'tbl_user.user_id','=', 'tbl_item_merchant_request.item_merchant_requested_by')
+	        								         ->category()
+											         ->where(function($query)
+											         {
+											             $query->where('item_merchant_request_status',"PENDING");
+											         });
+
+			}							     
 	        $item_type				   = Request::input("item_type");
 	        $search_name			   = Request::input("search_name");
 
--- resources/views/member/item/list.blade.php
+++ resources/views/member/item/list.blade.php
@@ -125,9 +125,9 @@
                     </div>
                 </div>
             </div>
+        </div><!-- 
         </div>
-        </div>
-    </div>    
+    </div>     -->
         <div id="pending" class="tab-pane fade in">
             <div class="form-group order-tags"></div>
             <div class="table-responsive">

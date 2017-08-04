f98a16dfdd4cab646c09d76bebee262f0c877262
Kim Briel Oraya <kimbrieloraya@digimaweb.solutions>
2017-08-04 13:37:42 +0800

    flexitime break hours done

--- public/assets/member/payroll/js/shift-template.js
+++ public/assets/member/payroll/js/shift-template.js
@@ -11,6 +11,8 @@ function shifting()
 		event_time_entry(); //GUILLERMO
 		event_remove_time_entry();
 		action_set_time_entry(); //GUILLERMO
+		// event_check_flexitime(); //KIM BRIEL
+		// action_show_break_hour(); // KIM BRIEL
 		
 	}
 	function event_add_sub_time()
@@ -58,6 +60,28 @@ function shifting()
 			$(e.currentTarget).closest(".main-con").find(".time-entry.in").timeEntry({'maxTime':$(e.currentTarget).val(), ampmPrefix: ' ', defaultTime: new Date(0, 0, 0, 12, 0, 0)});
 		});
 	}
+
+	function event_check_flexitime()
+	{
+
+		$(".flexitime-check").change(function(e) 
+		{
+			if(e.currentTarget.checked) 
+			{
+				console.log("a");
+        		$(e.currentTarget).closest(".main-time").find(".break_hours").removeClass("hidden");
+   			}
+   			else
+   			{
+   				console.log("b");
+   				$(e.currentTarget).closest(".main-time").find(".break_hours").addClass("hidden");
+   				$(e.currentTarget).closest(".main-time").find(".break_hours").val("0");
+   			}
+		});
+	}
+
+
+
 	function event_remove_time_entry()
 	{
 		$(".remove-time-entry").unbind("click");
@@ -67,6 +91,21 @@ function shifting()
 		});
 	}
 
+	// function event_flexitime_check()
+	// {
+	// 	$(".flexitime-check").change(function(e){
+	// 		if (e.currentTarget.checked) 
+	// 		{
+	// 			$(e.currentTarget).closest(".main-time").find(".-check").removeClass("hidden");
+	// 		}
+	// 		else
+	// 		{
+	// 			$(e.currentTarget).closest(".main-time").find(".flexitime-check").addClass("hidden");
+	// 			$(e.currentTarget).closest(".main-time").find(".flexitime-check").val("0");
+	// 		}
+	// 	});
+	// }
+
 	function action_set_time_entry()
 	{
 		$(".time-entry.in").timeEntry('destroy');
@@ -74,13 +113,29 @@ function shifting()
 		$(".time-entry.in").timeEntry({ampmPrefix: ' ', defaultTime: new Date(0, 0, 0, 0, 0, 0)});
 		$(".time-entry.out").timeEntry({ampmPrefix: ' ', defaultTime: new Date(0, 0, 0, 12, 0, 0)});
 	}
+
+	function action_show_break_hour()
+	{
+		$(".flexitime-check").each(function(e)
+		{
+			if(e.currentTarget.checked) 
+			{
+				$(e.currentTarget).closest(".main-time").find(".break_hours").removeClass("hidden");
+			}
+			else
+			{
+				$(e.currentTarget).closest(".main-time").find(".break_hours").addClass("hidden");
+				$(e.currentTarget).closest(".main-time").find(".break_hours").val("0");
+			}
+		});
+	}
+
 	
 	function action_append_sub_time($target)
 	{
 		$day = $target.closest(".main-time").attr("day");
 
 		$append = '<tr class="editable sub-time main-con">' +
-							'<td></td>' +
 							'<td></td>' +
 							'<td></td>' +
 							'<td class="editable">' +

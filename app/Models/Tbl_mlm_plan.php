faca470415fffc423ba308997ba4e7e0cb34972e
Guillermo Tabligan <gtplus.net@gmail.com>
2017-08-09 09:08:17 +0800

    change

--- app/Models/Tbl_mlm_plan.php
+++ app/Models/Tbl_mlm_plan.php
@@ -18,4 +18,18 @@ class Tbl_mlm_plan extends Model
     // 2 = daily
     // 3 = weekly
     // 4= monthly
+    public function scopeCode($query, $code)
+    {
+        $query->where("tbl_mlm_plan.marketing_plan_code", $code);
+    }
+
+    public function scopeEnable($query, $enable)
+    {
+        $query->where("tbl_mlm_plan.marketing_plan_enable", $enable);
+    }
+
+    public function scopeTrigger($query, $trigger)
+    {
+        $query->where("tbl_mlm_plan.marketing_plan_trigger", $trigger);
+    }
 }

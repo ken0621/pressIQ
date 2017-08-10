c60ba2a2e588b22c2e9a77ce9e606db28b6b811c
Rommel Cuneta <cunetarommel0620@gmail.com>
2017-08-10 17:38:32 +0800

    creating a login page

--- public/themes/digimahouse_landingpage/views/signin.blade.php
+++ public/themes/digimahouse_landingpage/views/signin.blade.php
@@ -6,23 +6,32 @@
 	<div class="signin-form-container">
 		<div class="container">
 			<div class="row clearfix">
+
 				<div class="col-md-4">
 					<div class="signin-form">
 						
-						<div class="signin-text-header">Welcome to Digima House!</div>
-						<div class="signin-subtext-header">Not a Member?  <span><a href="">Sign Up >></a></span></div>
+						<div class="text signin-text-header">Welcome to Digima House!</div>
+						<div class="text signin-subtext-header">Not a Member? <span><a href="#" class="signup">Sign Up >></a></span></div>
+						
+						<div class="text textbox-label">Signin your Shop Now</div>
+						
+						<div class="textbox-container">
+	                     	<input type="text" class="form-control email-input" placeholder="Email">
+                        	<input type="Password" class="form-control password-input" placeholder="Password">
+						</div>
 
-						<!-- <input type="text" class="email_textbox">
-						<input type="Password" class="password_textbox">
-						<button>Sign in to you account</button> -->
+						
+
 					</div>
 				</div>
 
 				<div class="col-md-8">
-
+					<img src="/themes/{{ $shop_theme }}/img/mlm-adv.png">
 				</div>
 
 			</div>
+
+			<div>Digima Web Solutions provides us with critical insight into our business which allows us to make fast decision and ultimately become more dynamic and competitive in marketplace. Visit www.digimaweb.solutions</div>
 		</div>
 	</div>
 

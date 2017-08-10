440d866b9de57528ae04cf7af6b6cb399f36a187
Edward Guevarra <edward.guevarra@digimaweb.solutions>
2017-08-10 11:35:40 +0800

    Switch Branch

--- resources/views/member/payroll/government_forms/bir.blade.php
+++ resources/views/member/payroll/government_forms/bir.blade.php
@@ -139,7 +139,7 @@
 											</tr>
 											<tr>
 												<td style="border-top: 0;">
-													<div class="input-container" style="border-top: 2px solid #000; margin-left: -1.5px; margin-right: -1.5px; padding-left: 1px; padding-right: 1px; width: 100%; display: block;">
+													<div class="input-container" style="border-top: 2px solid #000; padding-left: 1px; padding-right: 1px; width: 100%; display: block;">
 														<div style="height: 13px;">
 															<div class="number">4</div>
 															<div class="label" style="text-align: left;">Employee's Name (Last Name, First Name, Middle Name)</div>
@@ -164,7 +164,7 @@
 											</tr>
 											<tr>
 												<td style="border-top: 0;">
-													<div class="input-container" style="border-top: 2px solid #000; margin-left: -1.5px; margin-right: -1.5px; padding-left: 1px; padding-right: 1px; width: 100%; display: block;">
+													<div class="input-container" style="border-top: 2px solid #000; padding-left: 1px; padding-right: 1px; width: 100%; display: block;">
 														<div style="height: 13px;">
 															<div class="number">6</div>
 															<div class="label" style="text-align: left;">Registered Address</div>
@@ -191,7 +191,7 @@
 											</tr>
 											<tr>
 												<td style="border-top: 0;">
-													<div class="input-container" style="border-top: 2px solid #000; margin-left: -1.5px; margin-right: -1.5px; padding-left: 1px; padding-right: 1px; width: 100%; display: block;">
+													<div class="input-container" style="border-top: 2px solid #000; padding-left: 1px; padding-right: 1px; width: 100%; display: block;">
 														<div style="height: 13px;">
 															<div class="number">6B</div>
 															<div class="label" style="text-align: left;">Local Home Adddress</div>
@@ -218,9 +218,82 @@
 											</tr>
 											<tr>
 												<td style="border-top: 0;">
-													<div class="input-container" style="border-top: 2px solid #000; margin-left: -1.5px; margin-right: -1.5px; padding-left: 1px; padding-right: 1px; width: 100%; display: block;">
+													<div class="input-container" style="border-top: 2px solid #000; padding-left: 1px; padding-right: 1px; width: 100%; display: block;">
 														<div style="height: 13px;">
 															<div class="number">6D</div>
+															<div class="label" style="text-align: left;">Foreign Address</div>
+															<div class="number" style="margin-left: 181px;">6E</div>
+															<div class="label" style="text-align: left;">Zip Code</div>
+														</div>
+														<div class="arrow">
+															<i class="fa fa-caret-right" aria-hidden="true"></i>
+														</div>
+														<div class="input-value">
+															<div class="text" style="width: 240px;">Japan dun sa bandang Akihabara</div>
+														</div>
+														<div class="input-value">
+															<div class="text">1</div>
+															<div class="divide"></div>
+															<div class="text">2</div>
+															<div class="divide"></div>
+															<div class="text">3</div>
+															<div class="divide"></div>
+															<div class="text">4</div>
+														</div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td style="border-top: 0;">
+													<table>
+														<tbody>
+															<tr>
+																<td>
+																	<div class="input-container">
+																		<div style="height: 13px;">
+																			<div class="number">7</div>
+																			<div class="label" style="text-align: left;">Date of Birth (MM/DD/YY)</div>
+																		</div>
+																		<div class="input-value">
+																			<div class="text">0</div>
+																			<div class="divide"></div>
+																			<div class="text">8</div>
+																			<div class="divider"></div>
+																			<div class="text">2</div>
+																			<div class="divide"></div>
+																			<div class="text">5</div>
+																			<div class="divider"></div>
+																			<div class="text">0</div>
+																			<div class="divide"></div>
+																			<div class="text">8</div>
+																			<div class="divide"></div>
+																			<div class="text">0</div>
+																			<div class="divide"></div>
+																			<div class="text">8</div>
+																		</div>
+																	</div>
+																</td>
+																<td>
+																	<div class="input-container">
+																		<div style="height: 13px;">
+																			<div class="number">8</div>
+																			<div class="label" style="text-align: left;">Telephone Number</div>
+																		</div>
+																		<div class="input-value" style="margin-left: 15px;">
+																			<div class="text" style="width: 150px;"></div>
+																		</div>
+																	</div>
+																</td>
+															</tr>
+														</tbody>
+													</table>
+												</td>
+											</tr>
+											<tr>
+												<td style="border-top: 0;">
+													<div class="input-container" style="border-top: 2px solid #000; padding-left: 1px; padding-right: 1px; width: 100%; display: block;">
+														<div style="height: 13px;">
+															<div class="number">9</div>
 															<div class="label" style="text-align: left;">Foreign Address</div>
 															<div class="number" style="margin-left: 181px;">6E</div>
 															<div class="label" style="text-align: left;">Zip Code</div>
@@ -247,7 +320,361 @@
 									</table>
 								</td>
 								<td class="input">
-									
+									<table class="no-input">
+										<tbody>
+											<tr>
+												<td></td>
+												<td style="text-align: center;">Amount</td>
+											</tr>
+											<tr>
+												<td colspan="2" class="label-header">A. NON-TAXABLE/EXEMPT COMPENSATION INCOME</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">32</div>
+													<div class="label" style="text-align: left;">Basic Salary/</br>Statutory Minimum Wage</br>Minimum Wage Earner (MWE)</div>
+												</td>
+												<td>
+													<div class="number">32</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">33</div>
+													<div class="label" style="text-align: left;">Holiday Pay (MWE)</div>
+												</td>
+												<td>
+													<div class="number">33</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">34</div>
+													<div class="label" style="text-align: left;">Overtime Pay (MWE)</div>
+												</td>
+												<td>
+													<div class="number">34</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">35</div>
+													<div class="label" style="text-align: left;">Night Shift Differencial (MWE)</div>
+												</td>
+												<td>
+													<div class="number">35</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">36</div>
+													<div class="label" style="text-align: left;">Hazard Pay (MWE)</div>
+												</td>
+												<td>
+													<div class="number">36</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">37</div>
+													<div class="label" style="text-align: left;">13th Month Pay and Other Benefits</div>
+												</td>
+												<td>
+													<div class="number">37</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">38</div>
+													<div class="label" style="text-align: left;">De Minimis Benefits</div>
+												</td>
+												<td>
+													<div class="number">38</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">39</div>
+													<div class="label" style="text-align: left;">SSS, GSIS, PHIC & Pag-ibig</br>Contributions, & Union Dues</br>(Employee share only)</div>
+												</td>
+												<td>
+													<div class="number">39</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">40</div>
+													<div class="label" style="text-align: left;">Salaries & Other Forms of </br>Compensation</div>
+												</td>
+												<td>
+													<div class="number">40</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">41</div>
+													<div class="label" style="text-align: left;">Total Non-Taxable/Exempt</br>Compensation Income</div>
+												</td>
+												<td>
+													<div class="number">41</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td colspan="2" class="label-header" style="padding-top: 15px;">B. TAXABLE COMPENSATION INCOME</br>REGULAR</td>
+											</tr>	
+											<tr>
+												<td>
+													<div class="number">42</div>
+													<div class="label" style="text-align: left;">Basic Salary</div>
+												</td>
+												<td>
+													<div class="number">42</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">43</div>
+													<div class="label" style="text-align: left;">Representation</div>
+												</td>
+												<td>
+													<div class="number">43</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">44</div>
+													<div class="label" style="text-align: left;">Transportation</div>
+												</td>
+												<td>
+													<div class="number">44</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">45</div>
+													<div class="label" style="text-align: left;">Cost of Living Allowance</div>
+												</td>
+												<td>
+													<div class="number">45</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">46</div>
+													<div class="label" style="text-align: left;">Fixed Housing Allowance</div>
+												</td>
+												<td>
+													<div class="number">46</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">47</div>
+													<div class="label" style="text-align: left;">Others (Specify)</div>
+												</td>
+												<td>
+													
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">47A</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+												<td>
+													<div class="number">47A</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">47B</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+												<td>
+													<div class="number">47B</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td colspan="2" class="label-header" style="padding-top: 15px;">SUPPLEMENTARY</td>
+											</tr>	
+											<tr>
+												<td>
+													<div class="number">48</div>
+													<div class="label" style="text-align: left;">Commission</div>
+												</td>
+												<td>
+													<div class="number">48</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">49</div>
+													<div class="label" style="text-align: left;">Profit Sharing</div>
+												</td>
+												<td>
+													<div class="number">49</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">50</div>
+													<div class="label" style="text-align: left;">Fees Including Director's Fee</div>
+												</td>
+												<td>
+													<div class="number">50</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">51</div>
+													<div class="label" style="text-align: left;">Taxable 13th Month Pay</br>and Other Benefits</div>
+												</td>
+												<td>
+													<div class="number">51</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">52</div>
+													<div class="label" style="text-align: left;">Hazard Pay</div>
+												</td>
+												<td>
+													<div class="number">52</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">53</div>
+													<div class="label" style="text-align: left;">Overtime Pay</div>
+												</td>
+												<td>
+													<div class="number">53</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">54</div>
+													<div class="label" style="text-align: left;">Others (Specify)</div>
+												</td>
+												<td>
+													
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">54A</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+												<td>
+													<div class="number">54A</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">54B</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+												<td>
+													<div class="number">54B</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+											<tr>
+												<td>
+													<div class="number">55</div>
+													<div class="label" style="text-align: left;">Total Taxable Compensation</br>Income</div>
+												</td>
+												<td>
+													<div class="number">55</div>
+													<div class="input-value" style="margin-left: 5px;">
+														<div class="text" style="width: 150px;"></div>
+													</div>
+												</td>
+											</tr>
+										</tbody>
+									</table>
 								</td>
 							</tr>
 						</tbody>

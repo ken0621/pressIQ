77f2816d192ad6fb5a636a9069aff2dc19d06863
Ezarule <guevarra129@gmail.com>
2017-08-22 18:03:23 +0800

    cashier pricelevel add complete

--- public/assets/member/js/price_level/add.js
+++ public/assets/member/js/price_level/add.js
@@ -18,6 +18,7 @@ function add_price_level()
 		event_change_price_level_select();
 		action_change_price_level_select();
 		event_check_all_item_click();
+		event_adjust_marked_items();
 	}
 	function event_change_price_level_select()
 	{
@@ -81,6 +82,58 @@ function add_price_level()
 			    }
 			});
 		}
+	}
+	function event_adjust_marked_items()
+	{
+		$(".adjust-marked-btn").click(function()
+		{
+			action_adjust_marked_items();
+		});
+	}
+	function action_adjust_marked_items()
+	{
+		$(".checkboxs").each(function() 
+		{
+		    if($(this).is(":checked"))
+		    {
+		    	adjust_target_item(this);
+		    }
+		});
+	}
+	function adjust_target_item($row)
+	{
+		$adjust_price_type    = $(".adjust-price-type").val();
+		$adjust_price_percent = $(".adjust-price-percent").val();
+		$adjust_price_range   = $(".adjust-price-range").val();
+		$adjust_price_roundup = $(".adjust-price-roundup").val();
+		$target_item 		  = $($row).closest("tr");
+		$value                = 0;
+		$total                = 0;
+
+		$adjust_price_percent = parseFloat($adjust_price_percent);
+
+		if($adjust_price_type == "standard price")
+		{
+			$value = $target_item.find(".item-price").attr("item-value");
+		}
+		else if($adjust_price_type == "cost price")
+		{
+			$value = $target_item.find(".item-cost").attr("item-value");
+		}
+
+		$value = parseFloat($value);
+
+		if($adjust_price_range == "lower")
+		{
+			$total = $value - ($value * ($adjust_price_percent/100));
+		}
+		else if($adjust_price_range == "upper")
+		{
+			$total = $value + ($value * ($adjust_price_percent/100));
+		}
+
+	
+		$target_item.find(".custom-price-textbox").val($total);
 	}
 
 }

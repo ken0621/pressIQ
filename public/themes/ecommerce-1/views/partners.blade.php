dcf90c8fbf9c57c9317727311efecffdedb3a472
Edward Guevarra <edward.guevarra@digimaweb.solutions>
2017-09-01 17:29:22 +0800

    Member Register Static Fixed

--- public/themes/ecommerce-1/views/partners.blade.php
+++ public/themes/ecommerce-1/views/partners.blade.php
@@ -20,32 +20,26 @@
       @foreach($_company_information as $company_information)
 
         <div class="col-md-3 col-sm-6" style="padding: 4px;">
-
-        <div class="partners-div clearfix" style="height: auto; padding-bottom: 15px;">
-
-          <div class="partner-header">
-            <img class="match-height" style="object-fit: contain;" src="{{ $company_information->company_logo }}" width="100%">
-          </div>
-
-          <div class="partner-body">
-            <div style="text-align: center; padding: 30px 0px 30px 0px; font-weight: 700;">{{ $company_information->company_name }}
-          </div>
-          
-          <div id="company-address">
-            <div class="col-xs-2"><i class="fa fa-map-marker" aria-hidden="true" id="element"></i></div>
-            <div class="col-xs-10" ><p"> {{ $company_information ->company_address }} </p></div>
-          </div>
-
-          <div>
-            <div class="col-xs-2"><i class="fa fa-phone" aria-hidden="true" id="element2"></i></div>
-            <div class="col-xs-10"><p> {{ $company_information ->company_contactnumber }}</p></div>
-          </div>
-
+           <div class="partners-div clearfix" style="height: auto; padding-bottom: 15px;">
+              <div class="partner-header">
+                 <img class="match-height" style="object-fit: contain;" src="{{ $company_information->company_logo }}" width="100%">
+              </div>
+              <div class="partner-body">
+                 <div style="text-align: center; padding: 30px 0px 30px 0px; font-weight: 700;">{{ $company_information->company_name }}
+                 </div>
+                 <div id="company-address">
+                    <div class="col-xs-2"><i class="fa fa-map-marker" aria-hidden="true" id="element"></i></div>
+                    <div class="col-xs-10" ><p> {{ $company_information ->company_address }} </p></div>
+                 </div>
+                 <div>
+                    <div class="col-xs-2"><i class="fa fa-phone" aria-hidden="true" id="element2"></i></div>
+                    <div class="col-xs-10">
+                       <p> {{ $company_information ->company_contactnumber }}</p>
+                    </div>
+                 </div>
+              </div>
+           </div>
         </div>
-
-      </div>
-
-      </div>
 
       @endforeach
 

313b680515fe61f940a5551632101e659f06817d
Edward Guevarra <edward.guevarra@digimaweb.solutions>
2017-09-07 18:15:30 +0800

    Map Title Contact Fix
    Fixes

--- public/themes/additions/views/contact.blade.php
+++ public/themes/additions/views/contact.blade.php
@@ -5,7 +5,7 @@
 	    <div class="container">
 	        <div class="row clearfix">
 	            <div class="col-md-6">
-	                <div class="title">{{ get_front_divide_string($shop_theme_info, "contact", "contact_title", 2, 0) }} <font class="intouch-label">{{ get_front_divide_string($shop_theme_info, "contact", "contact_title", 2, 0) }}</font></div>
+	                <div class="title">{{ get_front_divide_string($shop_theme_info, "contact", "contact_title", 2, 0) }} <font class="intouch-label">{{ get_front_divide_string($shop_theme_info, "contact", "contact_title", 2, 1) }}</font></div>
 	                <div class="form-group-container">
 	                    <div class="row clearfix">
 	                        <div class="col-md-6">
@@ -91,7 +91,7 @@
 	        </div>
 	    </div>
 	</div>
-	<div class="map-title">FIND US <font class="map-title-pink">ON MAP</font></div>
+	<div class="map-title">{{ get_front_divide_string($shop_theme_info, "contact", "map_title", 2, 0) }} <font class="map-title-pink">{{ get_front_divide_string($shop_theme_info, "contact", "map_title", 2, 1) }}</font></div>
 	<div class="map-container">
 		<div class="container">
 			<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3861.007280409033!2d120.97894086009421!3d14.598660949454503!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sph!4v1468556209677" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>

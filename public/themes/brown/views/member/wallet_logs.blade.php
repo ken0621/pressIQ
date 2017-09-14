e9da49f37b9745ff11e1a2a318c99adfa3327489
Rommel Cuneta <cunetarommel0620@gmail.com>
2017-09-14 13:53:15 +0800

    mel

--- public/themes/brown/views/member/wallet_logs.blade.php
+++ public/themes/brown/views/member/wallet_logs.blade.php
@@ -15,27 +15,6 @@
 		<div class="wallet-profile">
 			<div class="img">
 				<img src="/themes/{{ $shop_theme }}/img/wallet-pic.png">
-<<<<<<< HEAD
-			</div>
-			{{-- </br>
-			</br>
-			</br> --}}
-			<div>
-				<div class="name"><b>Mr. Brown Lorem Ipsum</b></div>
-				<div class="label-name">Name</div>
-				<div class="p1"><b>brown&proudsolidgroup@gmail.com</b></div>
-				<div class="label-name1">Email Address</div>
-				<div class="p2"><b>1958-11-30</b></div>
-				<div class="label-name2">Birthdate</div>
-			</div>
-		</div>
-	</div>
-	</br>
-	</br>
-	<div class="container">
-		<p class="p3"><b>Income Summary</b></p>
-		<br>
-=======
 			</div>	
 			<div class="name">Mr. Brown Lorem Ipsum</div>
 			<div class="label-name">Name</div>
@@ -46,8 +25,7 @@
 		</div>
 	</div>
 	<div class="container">
-	<p class="income-salary">Income Summary</p>            
->>>>>>> origin/master
+	<p class="income-salary">Income Summary</p>
 		<table class="table table-hover table_report">
 			<thead>
 				<tr>
@@ -59,53 +37,6 @@
 			</thead>
 			<tbody>
 				<tr>
-<<<<<<< HEAD
-					<td><p>PHP 0.00</p>
-					<p>PHP 20,500.00</p>
-					<p>Date: 2017-06-28</p>
-				</td>
-				<td><p>Direct Referral Bonus</p>
-				<p>Pairing Reward</p></td>
-				<td><p>PHP 20,500.00</p>
-				<p>PHP 36,000.00</p></td>
-				<td><p>PHP 20,500.00</p>
-				<p>PHP 56,500.00</p>
-				<p>End Balance: PHP 56,500.00</p>
-			</td>
-		</tr>
-	</tr>
-	<tr>
-		<td><p>PHP 0.00</p>
-		<p>PHP 20,500.00</p>
-		<p>Date: 2017-06-28</p>
-	</td>
-	<td><p>Direct Referral Bonus</p>
-	<p>Pairing Reward</p></td>
-	<td><p>PHP 20,500.00</p>
-	<p>PHP 36,000.00</p></td>
-	<td><p>PHP 20,500.00</p>
-	<p>PHP 56,500.00</p>
-	<p>End Balance: PHP 56,500.00</p>
-</td>
-</tr>
-<tr>
-<td><p>PHP 0.00</p>
-<p>PHP 20,500.00</p>
-<p>Date: 2017-06-28</p>
-</td>
-<td><p>Direct Referral Bonus</p>
-<p>Pairing Reward</p></td>
-<td><p>PHP 20,500.00</p>
-<p>PHP 36,000.00</p></td>
-<td><p>PHP 20,500.00</p>
-<p>PHP 56,500.00</p>
-<p>End Balance: PHP 56,500.00</p>
-</td>
-</tr>
-</tbody>
-</table>
-</div>
-=======
 					<td>
 						<p>PHP 0.00</p>
 						<p>PHP 20,500.00</p>
@@ -165,7 +96,6 @@
 			</tbody>
 		</table>
 	</div>
->>>>>>> origin/master
 </div>
 @endsection
 @section("member_script")

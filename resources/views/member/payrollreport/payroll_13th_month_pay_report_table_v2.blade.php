46d19b1609f384a8d0255283df7ea0e72cc545d3
kimbriel <kimbriel21@gmail.com>
2017-09-20 16:32:51 +0800

    done

--- resources/views/member/payrollreport/payroll_13th_month_pay_report_table_v2.blade.php
+++ resources/views/member/payrollreport/payroll_13th_month_pay_report_table_v2.blade.php
@@ -10,7 +10,7 @@
     <tbody>
         @foreach($_period as $period)
         <tr>
-            <td class="text-center">{{$period->payroll_period_start}} - {{$period->payroll_period_end}}</td>
+            <td class="text-center">{{$period->month_contribution}} ( {{$period->period_count}} )</td>
             <td class="text-center"><a href="javascript: action_load_link_to_modal('/member/payroll/company_timesheet2/income_summary/{{ $period->payroll_period_company_id }}/{{ $period->payroll_employee_id }}', 'lg')">SUMMARY</a></td>
             <td class="text-center">{{ number_format($period->payroll_13th_month_basis,2) }}</td>
             <td class="text-center">{{ number_format($period->payroll_13th_month_contribution,2) }}</td>

971f4e4273c28e029610ad4679de7e5f347919bc
Arcylen Gutierrez <arcylen103095@gmail.com>
2017-09-21 09:08:02 +0800

    arc

--- app/Http/Controllers/Shop/ShopMemberController.php
+++ app/Http/Controllers/Shop/ShopMemberController.php
@@ -509,6 +509,7 @@ class ShopMemberController extends Shop
         $data["page"] = "Genealogy";
         $data['_slot'] = Tbl_mlm_slot::where("slot_owner", Self::$customer_info->customer_id)->get();
         $slot = Tbl_mlm_slot::where("slot_owner", Self::$customer_info->customer_id)->first();
+        $data['slot_no'] = 0;
         if($slot)
         {
             $data['slot_no'] = $slot->slot_no;

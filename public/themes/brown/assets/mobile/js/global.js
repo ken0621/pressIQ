be48c6012b9c80c0d9fe051009558ee5c7b077c4
Edward Guevarra <edward.guevarra@digimaweb.solutions>
2017-09-29 11:33:28 +0800

    Branch

---
+++ public/themes/brown/assets/mobile/js/global.js
@@ -0,0 +1,19 @@
+var global = new global();
+
+function global()
+{
+	init();
+
+	function init()
+	{
+		$(document).ready(function()
+		{
+			document_ready();
+		});
+	}
+
+	function document_ready()
+	{
+		
+	}
+}

e63133c0397ff02a629511f22142e976e7edbe72
Ephraim <ejlfcleader@gmail.com>
2017-09-29 15:54:04 +0800

    ephraim super_admin

--- resources/views/super_admin/admin_client_user_accounts.blade.php
+++ resources/views/super_admin/admin_client_user_accounts.blade.php
@@ -3,8 +3,9 @@
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
   <section class="content-header">
-    <h1 class="text_user_account" style="padding-top: 10px;"><center>
-      Shop User Accounts</center>
+    <h1 class="text_user_account" style="padding-top: 10px;">
+      Shop User Accounts
+      <small> table</small>
     </h1>
     <ol class="breadcrumb">
       <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
@@ -12,6 +13,15 @@
     </ol>
   </section>
    <!-- Main content -->
+<section class="content">
+  <div class="row">
+    <div class="col-xs-12">
+      <div class="box">
+        <div class="box-header">
+          <h3 class="box-title">Client User's Account Table</h3>
+        </div>
+            <!-- /.box-header -->
+    <div class="box-body-user-account">
    <div class="container filter">
    <select class="selectpicker" id="filteredrr">
     @foreach($_shop as $shop)
@@ -20,45 +30,22 @@
   </select>
   </div>
 <input type="hidden" value="{{csrf_token()}}" class="token" name="">
-<table class="table table-condensed shop_table">
-  <thead class="thead thead-inverse">
-    <tr>
-      <th>First Name</th>
-      <th>Last Name</th>
-      <th>Email</th>
-      <th>Contact Number</th>
-      <th>User Level</th>
-      <th>Shop Name</th>
-      <th>Action</th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach($_user_account as $user_accounts)
-    <tr>
-      <td>{{$user_accounts->user_first_name }}</td>
-      <td>{{$user_accounts->user_last_name }}</td>
-      <td>{{$user_accounts->user_email }}</td>
-      <td>{{$user_accounts->user_contact_number }}</td>
-      <td>{{$user_accounts->user_level }}</td>
-      <td>{{$user_accounts->shop_key }}</td>
-      <td class="text-center">
-        <div class="dropdown">
-          <button class="btn btn-primary dropdown-toggle btn-xs" type="button" data-toggle="dropdown">Action
-          <span class="caret"></span></button>
-          <ul class="dropdown-menu dropdown-menu-custom-update">
-            <li>
-              <a href="/admin/shop_user_accounts_update/{{$user_accounts->user_id }}">&nbsp;Edit</a>
-            </li>
-          </ul>
+<!-- <div class="border"> -->
+<div class="shop_table_container">
+
+</div>
+</div>         
+</div>
+<!-- </div> -->
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
         </div>
-        </td>
-      </tr>
-    @endforeach
-  </tbody>
-  </table>
-  <span class="pagination-container"><?php echo $_user_account->render(); ?></span>
-           
-</div>  
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </section>
+    <!-- /.content -->  
 @section("js")
 <script type="text/javascript" src ="/admin_assets/js/filtering_shop.js"></script>
 @endsection
--- resources/views/super_admin/admin_edit_user_accounts.blade.php
+++ resources/views/super_admin/admin_edit_user_accounts.blade.php
@@ -1,26 +1,135 @@
 @extends("super_admin/admin_layout")
 @section("content")
 <div class="content-wrapper">
-  <!-- Content Header (Page header) -->
-  <section class="content-header">
-    <h1 class="text_user_account" style="padding-top: 10px;">
-      Update Shop User Accounts
-      <small>Control panel</small>
-    </h1>
-    <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-      <li class="active">Dashboard</li>
-    </ol>
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Update User's Account
+        <small>Information</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Users' Account</a></li>
+        <li class="active">Information</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+	
+      <!-- SELECT2 EXAMPLE -->
+      <!-- <div class="box box-default">
+        <div class="box-header with-border">
+          <h3 class="box-title">Update User Account Info</h3>
+      
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-remove"></i></button>
+          </div>
+        </div>
+        /.box-header
+        <div class="box-body">
+        	 <form method="post" action="/admin/shop_user_accounts_update_submit/{{$_user_info_update->user_id}}" role="login">
+        		{{ csrf_field() }}
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label>First Name</label>
+                <input type="text" name="user_first_name" required class="form-control input-lg" value="{{$_user_info_update->user_first_name}}" /> 
+              </div>
+              	<div class="form-group">
+                <label>Last Name</label>
+                <input type="text" name="user_last_name" required class="form-control input-lg" value="{{$_user_info_update->user_last_name}}" /> 
+              </div>
+      				<div class="form-group">
+                <label>Email</label>
+                <input type="text" name="user_email" required class="form-control input-lg" value="{{$_user_info_update->user_email}}" /> 
+              </div>
+              <div class="form-group">
+                <label>Contact Number</label>
+                <input type="text" name="user_contact_number" required class="form-control input-lg" value="{{$_user_info_update->user_contact_number}}" /> 
+              </div>
+              <div class="form-group">
+                <label>User Level</label>
+                <input type="text" name="user_level" required class="form-control input-lg" value="{{$_user_info_update->user_level}}" /> 
+              </div>
+              <div class="form-group">
+                <label>Shop</label>
+                <input type="text" name="shop_key" required class="form-control input-lg" value="{{$_user_info_update->shop_key}}" /> 
+              </div>
+              <div class="form-group">
+                <label>Password</label>
+                <input type="text" name="password" required class="form-control input-lg" value="{{$password1}}" /> 
+              </div>
+              <button type="submit" name="go" class="btn btn-lg btn-primary btn-block">Update Password</button>     
+            </div>
+          </div>
+          </form>
+        </div> 
+          	</div> -->
+
+	<div class="box box-default">
+		<div class="box-header with-border">
+			<h3 class="box-title">Update User Account Info</h3>
+	          <div class="box-tools pull-right">
+	            <!-- <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+	            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-remove"></i></button> -->
+	          </div>
+
+			<div class="box-body">
+				<form class ="update_form" method="post" action="/admin/shop_user_accounts_update_submit/{{$_user_info_update->user_id}}" role="login">
+				<div class="form-group">
+        			{{ csrf_field() }}
+
+						<div class="col-md-6">
+							<div class="form-group">
+								<label class="form_label">First Name</label>
+								<input type="text" name="user_first_name" required class="form-control input_first_name input-lg" value="{{$_user_info_update->user_first_name}}" /> 
+							</div>
+							<div class="form-group">
+								<label class="form_label_last-name">Last Name</label>
+								<input type="text" name="user_last_name" required class="form-control input_last_name input-lg" value="{{$_user_info_update->user_last_name}}" /> 
+							</div>
+						</div>
+
+						<div class="">
+							<div class="form-group">
+								<label class="form_label_email">Email</label>
+								<input type="text" name="user_email" required class="form-control input_email input-lg" value="{{$_user_info_update->user_email}}" /> 
+							</div>
+							<div class="form-group">
+								<label class="form_label_contact_number">Contact Number</label>
+								<input type="text" name="user_contact_number" required class="form-control input_contact_number input-lg" value="{{$_user_info_update->user_contact_number}}" /> 
+							</div>
+						</div>
+
+						<div class="">
+							<div class="form-group">
+								<label class="form_label_user_level">User Level</label>
+								<input type="text" name="user_level" required class="form-control input_user_level input-lg" value="{{$_user_info_update->user_level}}" /> 
+							</div>
+							<div class="form-group">
+								<label class="form_label_shop">Shop</label>
+								<input type="text" name="shop_key" required class="form-control input_shop input-lg" value="{{$_user_info_update->shop_key}}" /> 
+							</div>
+						</div>
+
+						<div class="">
+							<div class="form-group">
+								<label class="form_label_password">Password</label>
+								<input type="text" name="password" required class="form-control input_password input-lg" value="{{$password1}}" /> 
+							</div>
+						</div>
+						<button type="submit" name="go" class="btn btn-lg btn-primary button_submit btn-block">Update Password</button>   
+				</div>
+			</form>
+			</div>
+
+
+		</div>
+	</div>
+
+
   </section>
-    <div class="col-md-4">
-      <section class="login-form">
-        <form method="post" action="/admin/shop_user_accounts_update_submit/{{$_user_password_update->user_id}}" role="login">
-          {{ csrf_field() }}
-          <img src="/admin_assets/IT_Pics/digimahouse-logo.png" class="img-responsive" alt="" />
-          <input type="text" name="password" required class="form-control input-lg" value="{{$password1}}" /> 
-          <button type="submit" name="go" class="btn btn-lg btn-primary btn-block">Update Password</button>   
-        </form>
-      </section>  
-      </div>
-</div>  
+</div>
 @endsection

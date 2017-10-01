f426796581d9599d54303ac27b98ddb7203e60e2
gtplus.net@gmail.com <gtplus.net@gmail.com>
2017-10-01 11:42:37 +0000

    change

--- app/Http/Controllers/TesterController.php
+++ app/Http/Controllers/TesterController.php
@@ -54,6 +54,7 @@ class TesterController extends Controller
             
             Cart2::clear_cart();
             Cart2::add_item_to_cart($shop_id, 57, 1);
+            dd(Cart::get_cart_info());
             
             echo "<br>" . $invoice->invoice_number . " (" . $customer->first_name . " " . $customer->last_name . ")";
             

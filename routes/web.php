e8592097d09792e09ad0a36bae82ba08ca37e6a3
gtplus.net@gmail.com <gtplus.net@gmail.com>
2017-10-02 11:30:23 +0000

    change

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
-Route::any('/inspirer', function()
+Route::any('/inspirers', function()
 {
-	return redirect('http://162.251.151.81/inspirer');
+	return redirect('http://162.251.151.81/inspirers');
 });
 
 Route::any('/ncabot', 'SampleTesting@ncabot');

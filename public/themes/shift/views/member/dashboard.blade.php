38b5b420ad74543921755aacc0d947d141be146c
Rommel Cuneta <cunetarommel0620@gmail.com>
2017-10-04 18:38:59 +0800

    nonmember

--- public/themes/shift/views/member/dashboard.blade.php
+++ public/themes/shift/views/member/dashboard.blade.php
@@ -6,23 +6,43 @@
 @if(!$mlm_member)
 	<div class="dashboard">
 	    <!-- TOP DASHBOARD-->
+<!-- 	    <div class="dashboard-top">
+    <div class="row clearfix">
+        <div class="col-md-8">
+            <div class="img-container">
+                <img src="/themes/{{ $shop_theme }}/img/brown-img1.png">
+            </div>
+        </div>
+        <div class="col-md-12">
+            <div class="join-container">
+                <div class="btn btn-text">
+                    <div class="text-header1">Become a member now!</div>
+                    <div class="text-header2">Enroll now and become one of us!</div>
+                </div>
+                <div class="btn-container">
+                    <a href="#" id="btn-buy-a-kit"><button class="btn-buy-a-kit">Buy a Kit</button></a><br>
+                    <img src="/themes/{{ $shop_theme }}/img/or-1.png"><br>
+                    <a href="#" id="btn-enter-a-code"><button class="btn-enter-a-code">Enter a Code</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> -->
+
 	    <div class="dashboard-top">
 	        <div class="row clearfix">
-	            <!-- <div class="col-md-8">
-	                <div class="img-container">
-	                    <img src="/themes/{{ $shop_theme }}/img/brown-img1.png">
-	                </div>
-	            </div> -->
 	            <div class="col-md-12">
-	                <div class="join-container">
+	            	<div class="title">NON-MEMBER | <span>DASHBOARD</span> </div>
+	                <div class="join-container" style="background-image: url('/themes/{{ $shop_theme }}/img/nonmember-bg.png');">
 	                    <div class="btn btn-text">
-	                        <div class="text-header1">Become a member now!</div>
-	                        <!-- <div class="text-header2">Enroll now and become one of us!</div> -->
+	                        <div class="text-header1">Become A Member Now!</div>
+	                        <div class="text-header2">Opportunity awaits you. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.<br>Aenean commodo ligula eget dolor.</div>
 	                    </div>
 	                    <div class="btn-container">
-	                        <a href="#" id="btn-buy-a-kit"><button class="btn-buy-a-kit">Buy a Kit</button></a><br>
-	                        <img src="/themes/{{ $shop_theme }}/img/or-1.png"><br>
-	                        <a href="#" id="btn-enter-a-code"><button class="btn-enter-a-code">Enter a Code</button></a>
+	                        <a href="#" id="btn-buy-a-kit"><button class="btn-buy-a-kit">BUY A KIT</button></a>
+	                        <!-- <img src="/themes/{{ $shop_theme }}/img/or-1.png"><br> -->
+	                        <span class="or">OR</span>
+	                        <a href="#" id="btn-enter-a-code"><button class="btn-enter-a-code">ENTER A CODE</button></a>
 	                    </div>
 	                </div>
 	            </div>
@@ -30,7 +50,7 @@
 	    </div>
 
 	    <!-- BOTTOM DASHBOARD -->
-	    <div class="dashboard-bottom">
+	    <!-- <div class="dashboard-bottom">
 	        <div class="text-header">Profile Information</div>
 	        <div class="row clearfix">
 	            <div class="col-md-4">
@@ -79,7 +99,8 @@
 	                </div>
 	            </div>
 	        </div>
-	    </div>
+	    </div> -->
+
 	</div>
 @else
 	<div class="dashboard">

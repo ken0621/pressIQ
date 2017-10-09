12135d8717fde5fe2540c8f9f6319a039f183f38
Figs <figues@shit.noob>
2017-10-09 17:07:30 +0800

    ward

---
+++ resources/views/frontend/ghead.blade.php
@@ -0,0 +1,20 @@
+<!-- BOOTSTRAP -->
+<link rel="stylesheet" href="/assets/initializr/css/bootstrap.min.css">
+<link rel="stylesheet" href="/assets/initializr/css/bootstrap-theme.min.css">
+<!-- FONT AWESOME -->
+<link rel="stylesheet" type="text/css" href="/assets/font-awesome/css/font-awesome.min.css">
+<!-- SLICK CSS -->
+<link rel="stylesheet" type="text/css" href="/assets/slick/slick.css">
+<link rel="stylesheet" type="text/css" href="/assets/slick/slick-theme.css">
+<!-- LOADER CSS -->
+<link rel="stylesheet" type="text/css" href="/assets/front/css/loader.css">
+<!-- PARALLAX -->
+<link rel="stylesheet" type="text/css" href="/assets/front/css_external/parallax.css">
+<!-- CUSTOM ICON -->
+<link rel="stylesheet" type="text/css" href="/assets/brown-icon/styles.css">
+<!-- LIGHTBOX -->
+<link rel="stylesheet" type="text/css" href="/assets/lightbox/css/lightbox.css">
+<!-- WOW JS -->
+<link rel="stylesheet" type="text/css" href="/assets/wow/css/animate.css">
+<!-- THEME COLOR -->
+<link href="/themes/{{ $shop_theme }}/css/{{ $shop_theme_color }}.css" rel="stylesheet" type="text/css">

cfa029945f134243efbf14e7a63a312678188330
kimbriel <kimbriel21@gmail.com>
2017-10-10 14:12:32 +0800

    done biometric importation

--- resources/views/member/payroll/modal/modal_biometrics.blade.php
+++ resources/views/member/payroll/modal/modal_biometrics.blade.php
@@ -3,7 +3,13 @@
 	<button type="button" class="close" data-dismiss="modal">×</button>
 	<h4 class="modal-title"><i class="fa fa-file-excel-o color-green" aria-hidden="true"></i>&nbsp;Import Time Sheet</h4>
 </div>
+
 <div class="modal-body clearfix form-horizontal">
+	<div class="form-group">
+		<div class="col-md-6">
+			<button class="btn btn-custom-primary btn-import-biometric"><i class="fa fa-calendar" aria-hidden="true"></i> &nbsp; Import From Biometric</button>
+		</div>
+	</div>
 	<form class="form-group" action="/member/payroll/import_bio/template_global" method="GET">
 		<input type="hidden" value="{{csrf_token()}}" class="_token" name="_token">
 		<div class="col-md-6">
@@ -27,6 +33,7 @@
 		
 		 
 	</form>
+
 	<div class="form-group">
 		<div class="col-md-6">
 			<label class="btn btm-custom-green"><input type="file" name="" id="bio-file" class="hide" accept="application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, text/plain, .dat">Choose File</label>
@@ -51,11 +58,7 @@
 			<button class="btn btn-custom-primary btn-import">Import File</button>
 		</div>
 	</div>
-	<div class="form-group">
-		<div class="col-md-6">
-			<button class="btn btn-custom-primary btn-import-biometric"><i class="fa fa-calendar" aria-hidden="true"></i> &nbsp; Import From Biometric</button>
-		</div>
-	</div>
+	
 	<div class="form-group">
 		<div class="col-md-12 import-status">
 			

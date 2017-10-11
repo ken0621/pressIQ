48cb648ef0370673444e6944ac48ebb09b1050bc
Rommel Cuneta <cunetarommel0620@gmail.com>
2017-10-11 18:19:14 +0800

    mel

--- public/themes/brown/views/member/dashboard.blade.php
+++ public/themes/brown/views/member/dashboard.blade.php
@@ -206,9 +206,9 @@
 												<div class="titles">{{$event->event_title}}</div>
 												<div class="description">{{$event->event_sub_title}}</div>
 												<div class="action">
-													<a class="popup" size="md" link="/members/event-details?id={{$event->event_id}}"><i class="fa fa-check-circle"></i> Details</a>
+													<a style="cursor: pointer;" class="popup" size="md" link="/members/event-details?id={{$event->event_id}}"><i class="fa fa-check-circle"></i> Details</a>
 													@if($event->is_reserved == 0) 
-													<a class="popup" size="md" link="/members/event-reserve?id={{$event->event_id}}"><i class="fa fa-calendar-check-o"></i> Reserve a Seat</a>
+													<a style="cursor: pointer;" class="popup" size="md" link="/members/event-reserve?id={{$event->event_id}}"><i class="fa fa-calendar-check-o"></i> Reserve a Seat</a>
 													@else
 													<a href="javascript:"><i class="fa fa-calendar-check-o"></i> Reserved</a>
 													@endif

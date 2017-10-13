7322182a42dc53f98c5ca6927bc78dbd33ba6b12
Edward Guevarra <edward.guevarra@digimaweb.solutions>
2017-10-13 18:08:42 +0800

    Mobile LuL

--- public/themes/brown/views/member/mobile/script.blade.php
+++ public/themes/brown/views/member/mobile/script.blade.php
@@ -18,126 +18,23 @@
         <div class="profile-ul">
             <ul>
                 <li><a href="#" data-popup=".popup-basic-info" class="open-popup">Basic Information</a></li>
-                <li><a href="javascript:">Rewards Configuration</a></li>
-                <li><a href="javascript:">Profile Picture</a></li>
-                <li><a href="javascript:">Password</a></li>
-            </ul>
-        </div>
-    </div>
-</div>
-{{-- Basic Information Popup --}}
-<div class="popup popup-basic-info">
-    <div class="content-block">
-        <a href="#" class="close-popup">&laquo; Back</a>
-        <div class="list-block">
-            <ul>
-                <li>
-                    <div class="item-content">
-                        <div class="item-inner">
-                            <div class="item-title floating-label">First Name</div>
-                            <div class="item-input">
-                                <input type="text" name="name">
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="item-content">
-                        <div class="item-inner">
-                            <div class="item-title floating-label">Middle Name</div>
-                            <div class="item-input">
-                                <input type="text" name="name">
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="item-content">
-                        <div class="item-inner">
-                            <div class="item-title floating-label">Last Name</div>
-                            <div class="item-input">
-                                <input type="text" name="name">
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="item-content">
-                        <div class="item-inner">
-                            <div class="item-title floating-label">Birth Date</div>
-                            <div class="item-input">
-                                <input type="text" name="name">
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="item-content">
-                        <div class="item-inner">
-                            <div class="item-title floating-label">Country</div>
-                            <div class="item-input">
-                                <input type="text" name="name">
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="item-content">
-                        <div class="item-inner">
-                            <div class="item-title floating-label">Province</div>
-                            <div class="item-input">
-                                <input type="text" name="name">
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="item-content">
-                        <div class="item-inner">
-                            <div class="item-title floating-label">City</div>
-                            <div class="item-input">
-                                <input type="text" name="name">
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="item-content">
-                        <div class="item-inner">
-                            <div class="item-title floating-label">Barangay</div>
-                            <div class="item-input">
-                                <input type="text" name="name">
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div class="item-content">
-                        <div class="item-inner">
-                            <div class="item-title floating-label">Full Address</div>
-                            <div class="item-input">
-                                <input type="text" name="name">
-                            </div>
-                        </div>
-                    </div>
-                </li>
+                <li><a href="#" data-popup=".popup-reward-config" class="open-popup">Rewards Config</a></li>
+                <li><a href="#" data-popup=".popup-profile-picture" class="open-popup">Profile Picture</a></li>
+                <li><a href="#" data-popup=".popup-password" class="open-popup">Password</a></li>
             </ul>
         </div>
     </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="/themes/{{ $shop_theme }}/assets/initializr/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 <script type="text/javascript" src="/themes/{{ $shop_theme }}/assets/mobile/framework7/dist/js/framework7.js"></script>
 <script type="text/javascript" src="/themes/{{ $shop_theme }}/assets/mobile/framework7/kitchen-sink-material/js/kitchen-sink.js"></script>
 {{-- EXTERNAL JS --}}
 <script type="text/javascript" src='/assets/chartjs/Chart.bundle.min.js'></script>
 <!-- GLOBAL JS -->
 <script type="text/javascript" src="/themes/{{ $shop_theme }}/assets/mobile/js/global.js"></script>
-<script type="text/javascript" src="/themes/{{ $shop_theme }}/js/non_member.js?version=2.1"></script>
-<script type="text/javascript">
-var myApp = new Framework7();
-var $$ = Dom7;
-</script>
+<script type="text/javascript" src="/themes/{{ $shop_theme }}/js/non_member_mobile.js?version=2.1"></script>
 <!-- BEGIN JIVOSITE CODE -->
 <script type='text/javascript'>
 (function(){ var widget_id = 'OcvyPjoHBr';var d=document;var w=window;function l(){ var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();

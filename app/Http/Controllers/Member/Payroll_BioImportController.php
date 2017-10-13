669f25dfe0fe4a694ba3cca6c86ba3342744b634
kimbriel <kimbriel21@gmail.com>
2017-10-13 09:11:36 +0800

    done

--- app/Http/Controllers/Member/Payroll_BioImportController.php
+++ app/Http/Controllers/Member/Payroll_BioImportController.php
@@ -151,7 +151,6 @@ class Payroll_BioImportController extends Member
 
 	public function save_time_record($_time_record, $company ,$shop_id, $biometric_name)
 	{
-		
 		$success = 0;
 		$failed = 0;
 		$incomplete = 0;
@@ -235,8 +234,8 @@ class Payroll_BioImportController extends Member
 					$failed++;
 				}
 			}
+			
 		}
-		
 		$data["success"] = $success;
 		$data["failed"] = $failed;
 		$data["overwritten"] = $overwritten;
@@ -585,8 +584,6 @@ class Payroll_BioImportController extends Member
     	$_time = Excel::selectSheetsByIndex(0)->load($file, function($reader){})->get(array('no','datetime'));
     	if(isset($_time[0]['no']) && isset($_time[0]['datetime']))
     	{
-
-
 	    	$success_count = 0;
 	    	$temp_date = '';
 	    	$insert_time_record = array();
@@ -640,7 +637,6 @@ class Payroll_BioImportController extends Member
 	    			}
 	    			
 	    		}
-	    		
 	    	}
 	    	$message = '<center><span class="color-gray">Nothing to insert</span></center>';
 	    	if(!empty($insert_time_record))
@@ -1217,45 +1213,73 @@ class Payroll_BioImportController extends Member
     	$_time = Excel::selectSheetsByIndex(0)->load($file, function($reader){})->get(array('company_code','employee_no','date','in_1','out_1','in_2','out_2','in_3','out_3','in_4','out_4','in_5','out_5','in_6','out_6'));
 
     	$incomplete = 0;
-    	
+    	$_record = null;
     	if(isset($_time[0]['company_code']) && isset($_time[0]['employee_no']) && isset($_time[0]['date']) && isset($_time[0]['in_1']) && isset($_time[0]['out_1']) && isset($_time[0]['in_2']) && isset($_time[0]['out_2']) && isset($_time[0]['in_3']) && isset($_time[0]['out_3']))
     	{
-
-    		
-
-    	 foreach ($_time as $key => $value) 
+    	 foreach ($_time as $key => $time) 
     	 {
 
-    	 	if ($value['date'] != null && $value['in_1'] != null && $value['out_1'] != null && $value['employee_no'] != null) 
+    	 	if ($time['date'] != null && $time['in_1'] != null && $time['out_1'] != null && $time['employee_no'] != null) 
     	 	{
-    	 		$employee_number = $value["employee_no"];
-    	 		if (is_object($value["date"])) 
+    	 		$employee_number = $time["employee_no"];
+    	 		$time_in  = "";
+    	 		$time_out = "";
+
+    	 		$column_in_out = array('in_1','in_2','in_3','in_4','in_5','in_6','out_1','out_2','out_3','out_4','out_5','out_6');
+			 	if (is_object($time["date"])) 
     	 		{
-    	 			$date = date('Y-m-d', strtotime($value['date']->toDateTimeString()));
+    	 			$date = date('Y-m-d', strtotime($time['date']->toDateTimeString()));
     	 		}
     	 		else
     	 		{
-    	 			$date = date('Y-m-d', strtotime($value['date']));
+    	 			$date = date('Y-m-d', strtotime($time['date']));
     	 		}
-    	 		
 
-    	 		if(is_object($value["in_1"]) && is_object($value["out_1"]))
-	 			{
-			 		$_record[$date][$employee_number]['time_in']  = date('H:i:s', strtotime($value['in_1']->toDateTimeString()));
-					$_record[$date][$employee_number]['time_out'] = date('H:i:s', strtotime($value['out_1']->toDateTimeString()));	
-	 			}
-	 			else
-	 			{
-	 				$_record[$date][$employee_number]['time_in']  = date('H:i:s', strtotime($value['in_1']));
-					$_record[$date][$employee_number]['time_out'] = date('H:i:s', strtotime($value['out_1']));
-	 			}
+    			foreach ($time as $key => $value) 
+    			{
+    				
+					if (in_array($key, $column_in_out)) 
+					{
+						$time_record = null;			
+						if (is_object($value)) 
+						{
+							$time_record = date('H:i:s', strtotime($value->toDateTimeString())); 
+
+						}
+						else
+						{
+							$time_record = date('H:i:s', strtotime($value)); 
+						}
+
+
+						if (!isset($_record[$date][$employee_number]['time_in'])) 
+						{
+
+							$_record[$date][$employee_number]['time_in']   = $time_record;
+ 							$_record[$date][$employee_number]['time_out']  = $time_record;
+						}
+						else
+						{
+ 							
+							if ($_record[$date][$employee_number]['time_in']  > $time_record)
+							{
+								$_record[$date][$employee_number]['time_in'] = $time_record;
+							}
+							if ($_record[$date][$employee_number]['time_out'] < $time_record) 
+							{
+								$_record[$date][$employee_number]['time_out'] = $time_record;
+							}
+						}
+					}
+    			}
+    			
+    			dd($_record);
     	 	}
     	 	else
     	 	{
     	 		$incomplete++;
     	 	}
     	 }
-    	 
     	 $data = Self::save_time_record($_record, $company, $this->user_info->shop_id, "ANVIZ Biometrics EP Series");
    
     	 echo "<div><h4 class='text-success'>SUCCESS: ".$data["success"]."</h4><h4 class='text-primary'>OVERWRITTEN: ".$data["overwritten"]."</h4><h4 class='text-danger'>FAILED: ".$data["failed"]."</h4></div>";

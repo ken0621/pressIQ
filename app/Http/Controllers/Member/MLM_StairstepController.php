225361efd7166bedc0e23cadcd4092c68bc5f2aa
Ezarule <guevarra129@gmail.com>
2017-10-16 17:47:06 +0800

    bug fix

--- app/Http/Controllers/Member/MLM_StairstepController.php
+++ app/Http/Controllers/Member/MLM_StairstepController.php
@@ -183,7 +183,7 @@ class MLM_StairstepController extends Member
 						   					    ->where("points_log_type","SRB")
 						   					    ->where("points_log_converted","0")
 						   					    ->sum("points_log_points");
-                                                dd($dynamic_compression);
+
 				$settings         = Tbl_mlm_stairstep_settings::where("stairstep_id",$list->stairstep_rank)->first();
                 $give_wallet      = 0;
 				$give_srb         = 0;

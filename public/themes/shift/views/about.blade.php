fd516f27df68210084b01388000dfda08178be75
Edward Guevarra <edward.guevarra@digimaweb.solutions>
2017-10-16 15:13:14 +0800

    Shift

---
+++ public/themes/shift/views/about.blade.php
@@ -0,0 +1,120 @@
+@extends("layout")
+@section("content")
+<div class="about">
+    <header class="header" style="background-image: url('/themes/{{ $shop_theme }}/img/about-bg.jpg');">
+        <div class="container">
+        	<div class="first">We Are</div>
+        	<div class="second">Passionate</div>
+        </div>
+    </header>
+    <section class="first-row">
+    	<div class="container">
+    		<div class="row clearfix">
+	    		<div class="col-md-4">
+	    			<div class="title">Who Are We</div>
+	    			<div class="desc">"The company was conceptualized by young entrepreneurs who believes that everyone can start their own bsuiness small and then make it big as long as they are passionate, dedicated and results driven."</div>
+	    		</div>
+	    		<div class="col-md-4">
+	    			<div class="title">Vision</div>
+	    			<div class="desc">"With the help of information technology and the internet, reaching your team, clients and target market can now be easily accomplished."</div>
+	    		</div>
+	    		<div class="col-md-4">
+	    			<div class="title">Mission</div>
+	    			<div class="desc">"The company believes in equal opportunity and compensation. Thus, the company have very aggressive sales platforms and developmental training programs aimed at maximizing the income of every S.H.I.F.T. PARTNER."</div>
+	    		</div>
+	    	</div>
+    	</div>
+    </section>
+    <section class="second-row">
+    	<div class="container">
+    		<div class="title">Our <span>Team</span></div>
+	    	<div class="main">
+	    		<div class="img"><img class="img-responsive" src="/themes/{{ $shop_theme }}/img/team/main.png"></div>
+	    		<div class="name">Florentino Gemora Garcia</div>
+	    		<div class="sub">Vice President</div>
+	    		<div class="desc">He was born in Davao City from a business-minded parents who owns a gas station, movie house, restaurant & hospital. Though he was a son born with a silver platter, he was trained by his parents to be self-sufficient. He became a working student and finished college a little longer with a Bachelor of Science in Commerce (Major in Accounting) at Cor Jesu College, Digos, Davao del Sur. Incidentally, our beloved President, Rody Duterte, became one of his schoolmates.</div>
+	    		<div class="desc">His first employment was in January 1980 with a softdrinks company, Coca-Cola Bottlers Philippines, Inc. (Davao Plant). He rose from the ranks as a Route Salesman and 3 years after, he was promoted and was considered as the youngest District Sales Supervisor of Davao Coca-Cola Plant. With so much dedication in his work, accompanied with a track record of sales and administrative performance, he was promoted as Branch Manager in 1987. Ag ain, with ambitions to reach the top, he was promoted the following year as Regional Sales Manager in 1988. This time with Key Result Areas being achieved, he got numerous awards which includes Travel Incentives to Australia and other Asian Countries. In 1993, he was hired to be the Regional Manager of Pepsi-Cola Products Philippines to handle Eastern Visayas Operations.</div>
+	    		<div class="desc">In 1997, he shifted his management career as a Senior Regional Manager of Cebu with Ayala Plans, Inc. Though the products and services are intangible, he was able to bring the company to the limelight of success as TOP 3 in Sales; Administration and Recruitment Operations.</div>
+	    		<div class="desc">In 2007, he was hired as the pioneering General Manager of a networking company, Vital C Health Products, Inc. His experience in a networking company was very challenging because he was able to build a strong base of over 40,000 Distributors and created a great number of Millionaire Club Members. This is where he concluded that in MLM or Networking Business, ordinary people can earn extraordinary income!</div>
+	    	</div>
+	    	<div class="other">
+	    		<div class="row clearfix">
+	    			<div class="col-md-3">
+	    				<div class="img"><img class="img-responsive" src="/themes/{{ $shop_theme }}/img/team/1.png"></div>
+	    				<div class="name">Mitchel Despi Barawidan</div>
+	    				<div class="role">Sales Executive</div>
+	    			</div>
+	    			<div class="col-md-3">
+	    				<div class="img"><img class="img-responsive" src="/themes/{{ $shop_theme }}/img/team/2.png"></div>
+	    				<div class="name">Baltazar Bagolor</div>
+	    				<div class="role">Sales Executive</div>
+	    			</div>
+	    			<div class="col-md-3">
+	    				<div class="img"><img class="img-responsive" src="/themes/{{ $shop_theme }}/img/team/3.png"></div>
+	    				<div class="name">Sam Dacullo</div>
+	    				<div class="role">Sales Executive</div>
+	    			</div>
+	    			<div class="col-md-3">
+	    				<div class="img"><img class="img-responsive" src="/themes/{{ $shop_theme }}/img/team/4.png"></div>
+	    				<div class="name">Anthony Rodriguez</div>
+	    				<div class="role">Sales Executive</div>
+	    			</div>
+	    			<div class="col-md-3">
+	    				<div class="img"><img class="img-responsive" src="/themes/{{ $shop_theme }}/img/team/5.png"></div>
+	    				<div class="name">Abel Echon</div>
+	    				<div class="role">Sales Executive</div>
+	    			</div>
+	    			<div class="col-md-3">
+	    				<div class="img"><img class="img-responsive" src="/themes/{{ $shop_theme }}/img/team/6.png"></div>
+	    				<div class="name">Tiffany Bascon</div>
+	    				<div class="role">Sales Executive</div>
+	    			</div>
+	    			<div class="col-md-3">
+	    				<div class="img"><img class="img-responsive" src="/themes/{{ $shop_theme }}/img/team/7.png"></div>
+	    				<div class="name">Jefrrey Blando</div>
+	    				<div class="role">Sales Executive</div>
+	    			</div>
+	    			<div class="col-md-3">
+	    				<div class="img"><img class="img-responsive" src="/themes/{{ $shop_theme }}/img/team/8.png"></div>
+	    				<div class="name">Julie Baronda</div>
+	    				<div class="role">Sales Executive</div>
+	    			</div>
+	    			<div class="col-md-3">
+	    				<div class="img"><img class="img-responsive" src="/themes/{{ $shop_theme }}/img/team/9.png"></div>
+	    				<div class="name">Josie Noble</div>
+	    				<div class="role">Sales Executive</div>
+	    			</div>
+	    			<div class="col-md-3">
+	    				<div class="img"><img class="img-responsive" src="/themes/{{ $shop_theme }}/img/team/10.png"></div>
+	    				<div class="name">Oliver Mahilum</div>
+	    				<div class="role">Sales Executive</div>
+	    			</div>
+	    			<div class="col-md-3">
+	    				<div class="img"><img class="img-responsive" src="/themes/{{ $shop_theme }}/img/team/11.png"></div>
+	    				<div class="name">Lady Mae Baronca</div>
+	    				<div class="role">Sales Executive</div>
+	    			</div>
+	    			<div class="col-md-3">
+	    				<div class="img"><img class="img-responsive" src="/themes/{{ $shop_theme }}/img/team/12.png"></div>
+	    				<div class="name">Ma. Antonia Zabala</div>
+	    				<div class="role">Sales Executive</div>
+	    			</div>
+	    			<div class="col-md-3">
+	    				<div class="img"><img class="img-responsive" src="/themes/{{ $shop_theme }}/img/team/13.png"></div>
+	    				<div class="name">Jerome Olanda</div>
+	    				<div class="role">Sales Executive</div>
+	    			</div>
+	    		</div>
+	    	</div>
+    	</div>
+    </section>
+</div>
+@endsection
+
+@section("css")
+<link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/about.css">
+@endsection
+
+@section("script")
+
+@endsection
--- public/themes/shift/views/home.blade.php
+++ public/themes/shift/views/home.blade.php
@@ -1,26 +1,134 @@
 @extends("layout")
 @section("content")
-<div class="content">
-    <!-- Media Slider -->
-    <div class="fullscreen background parallax top-container" style="background-image: url('/themes/{{ $shop_theme }}/img/front-banner.jpg');" data-img-width="1600" data-img-height="1139" data-diff="100">
-        <div class="container">
-            <div class="caption-logo-container"><img src="/themes/{{ $shop_theme }}/img/top-logo.png"></div>
-            <div class="caption-container animated fadeInDown">
-                <h1>SCENTS.HEALTH.INFORMATION.FASHION.TECHNOLOGY</h1>
+<!-- Media Slider -->
+<div class="fullscreen background parallax top-container" style="background-image: url('/themes/{{ $shop_theme }}/img/front-banner.jpg');" data-img-width="1600" data-img-height="1139" data-diff="100">
+    <div class="container">
+        <div class="caption-logo-container"><img src="/themes/{{ $shop_theme }}/img/top-logo.png"></div>
+        <div class="caption-container animated fadeInDown">
+            <h1>SCENTS.HEALTH.INFORMATION.FASHION.TECHNOLOGY</h1>
+        </div>
+    </div>
+    <div class="scroll-down"><img src="/themes/{{ $shop_theme }}/img/scroll-down.png"></div>
+</div>
+<!-- SCROLL TO TOP -->
+<div class="scroll-up"><img src="/themes/{{ $shop_theme }}/img/scroll-up.png"></div>
+<section class="brand-container">
+    <div class="container">
+        <div class="title">Our <span>Brand</span></div>
+        <div class="sub">Our brand is a call to action for everyone that an alternative, more superior option is available for you. And it is just a click away. Try everything that we offer and you will see that SHIFTing to our brand is the best decision for you.</div>
+        <div class="brand-row">
+            <div class="brand-holder">
+                <div class="img"><img src="/themes/{{ $shop_theme }}/img/icon/scent.png"></div>
+                <div class="name a">SCENT</div>
+            </div>
+            <div class="brand-holder">
+                <div class="img"><img src="/themes/{{ $shop_theme }}/img/icon/health.png"></div>
+                <div class="name b">HEALTH</div>
+            </div>
+            <div class="brand-holder">
+                <div class="img"><img src="/themes/{{ $shop_theme }}/img/icon/info.png"></div>
+                <div class="name c">INFORMATION</div>
+            </div>
+            <div class="brand-holder">
+                <div class="img"><img src="/themes/{{ $shop_theme }}/img/icon/fashion.png"></div>
+                <div class="name d">FASHION</div>
+            </div>
+            <div class="brand-holder">
+                <div class="img"><img src="/themes/{{ $shop_theme }}/img/icon/technology.png"></div>
+                <div class="name e">TECHNOLOGY</div>
             </div>
         </div>
     </div>
-    <!-- SCROLL TO TOP -->
-    <div class="scroll-up"><img src="/themes/{{ $shop_theme }}/img/scroll-up.png"></div>
-</div>
+</section>
+<section class="opportunity-container" style="background-image: url('/themes/{{ $shop_theme }}/img/center-bg.jpg');">
+    <div class="container">
+        <div class="title">Your Opportunity</div>
+        <div class="sub">If you like the brands we have, we would like to ask you to join our ever increasing team of distributors and become our partner in promoting S.H.I.F.T.! A very good and competitive compensation package comes with it. A perfect way to add an extra flow of cash in your basket. To find out more, contact any of our TEAM LEADERS or DIRECTORS.</div>
+        <div class="row clearfix">
+            <div class="col-md-4">
+                <div class="opportunity-holder">
+                    <div class="title"><img src="/themes/{{ $shop_theme }}/img/icon/cloud.png"> Downloadable Forms</div>
+                    <ul>
+                        <li><a href="javascript:">SHIFT Distributor Application Form</a></li>
+                        <li><a href="javascript:">ID Application Form</a></li>
+                        <li><a href="javascript:">Password Reset / Retreival Form</a></li>
+                        <li><a href="javascript:">Spelling Correction Form</a></li>
+                        <li><a href="javascript:">SHIFT Cares Claim Form</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="opportunity-holder">
+                    <div class="title"><img src="/themes/{{ $shop_theme }}/img/icon/powerpoint.png"> Shift Business Presentation</div>
+                    <ul>
+                        <li><a href="javascript:">SHIFT Business Presentation</a></li>
+                    </ul>
+                    <div class="title" style="margin-top: 60px;">Shift Cellcare Presentation</div>
+                    <ul>
+                        <li><a href="javascript:">CELLCARE Complete Presentation</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="opportunity-holder">
+                    <div class="title">
+                        {{-- <img src="/themes/{{ $shop_theme }}/img/icon/cup.png"> --}}
+                        Leadership Bonus Withdrawal Form
+                    </div>
+                    <div class="sub">Steps to withdraw your leadership bonus:</div>
+                    <ol>
+                        <li>Please download this form Payout Request Form.</li>
+                        <li>Fill up the information required.</li>
+                        <li>Submit to management@myshiftbusiness.com</li>
+                        <li>Cut-off is every Sunday and releasing of checks is on Friday.</li>
+                    </ol>
+                    <div class="sub">Please take note that Business Centers outside of Cebu will receive payout deliveries on Monday via LBC or AP Cargo.</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="belief-container">
+    <div class="container">
+        <div class="row clearfix">
+            <div class="col-md-6">
+                <div class="title">Our Belief</div>
+                <div class="desc">We believe that we will make a difference in every consumers chopice. We will make it very convenient for our patrons to shop for what they need at a very affordable price. We also believe that every SHIFT partner will have a huge change in lifestyle as more and more people will SHIFT! So what are you waiting for, SHIFT to our products and become a SHIFT partner now! It's EASY... It's SIMPLE!</div>
+                <div class="btn-container">
+                    <button class="btn btn-primary">Join Us Today</button>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <img class="img-responsive" src="/themes/{{ $shop_theme }}/img/belief-bg.jpg">
+            </div>
+        </div>
+    </div>
+</section>
+<section class="brand2-container">
+    <div class="container">
+        <div class="title">Our <span>Brand</span></div>
+        <div class="row clearfix">
+            <div class="col-md-6">
+                <img class="img-responsive" src="/themes/{{ $shop_theme }}/img/brand.jpg">
+            </div>
+            <div class="col-md-6">
+                <div class="info">
+                    <div class="title">Cellcare Health Supplement</div>
+                    <div class="desc">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. </div>
+                    <div class="btn-container">
+                        <div class="btn btn-primary">Shop Now</div> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
 
 @section("css")
-<!-- <link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/home.css"> -->
+<link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/home.css">
 @endsection
 
-@section("js")
-
-<script type="text/javascript">
-</script>
+@section("script")
+<script type="text/javascript" src="/themes/{{ $shop_theme }}/js/home.js"></script>
 @endsection

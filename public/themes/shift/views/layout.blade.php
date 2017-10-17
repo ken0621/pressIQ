10d51cd761899dfd3f797828318a165f851c3931
Edward Guevarra <edward.guevarra@digimaweb.solutions>
2017-10-17 11:35:21 +0800

    Responsive 10%

--- public/themes/shift/views/layout.blade.php
+++ public/themes/shift/views/layout.blade.php
@@ -19,6 +19,8 @@
         <link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/global.css">
         <!-- OTHER CSS -->
         @yield("css")
+        {{-- RESPONSIVE CSS --}}
+        <link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/responsive.css">
         <script src="/themes/{{ $shop_theme }}/assets/initializr/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     </head>
     <body>
@@ -47,8 +49,8 @@
     <div class="header-container">
         <div class="container">
             <div class="row clearfix">
-                <div class="col-md-2">
-                    <div class="image-logo-holder">
+                <div class="col-md-2 col-sm-12">
+                    <div class="image-logo-holder clearfix">
                         <a class="clearfix" href="/"><img src="/themes/{{ $shop_theme }}/img/shift-logo.png"></a>                       
                     </div>
                     <div class="menu-nav">
@@ -84,12 +86,12 @@
         <div class="container">
             <div class="footer-container">
                 <div class="upper row clearfix">
-                    <div class="col-md-4">
+                    <div class="col-md-4 col-sm-6">
                         <div class="inner-content">
                             <img src="/themes/{{ $shop_theme }}/img/footer-banner.jpg">
                         </div>
                     </div>
-                     <div class="col-md-4">
+                     <div class="col-md-4 col-sm-6">
                          <div class="inner-content">
                              <div class="inner-content-title">Information</div>
                              <p>FAQ</p>
@@ -98,7 +100,7 @@
                              <p>Company</p>
                          </div>
                      </div> 
-                     <div class="col-md-4">
+                     <div class="col-md-4 col-sm-12">
                          <div class="inner-content">
                              <div class="inner-content-title">Contact Information</div>
                              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. </p>
@@ -124,154 +126,6 @@
 
     <!-- FB WIDGET -->
     <div id="fb-root"></div>
-<<<<<<< HEAD
-    <script>
-        (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10";
-        fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    </script>
-
-    <script type="text/javascript">
-        $window = $(window);
-        $window.scroll(function() {
-          $scroll_position = $window.scrollTop();
-            if ($scroll_position > 32.2167) { 
-                $('.header-container').addClass('header-fixed');
-                $('.subheader-container').addClass('header-fixed');
-
-                header_height = $('.your-header').innerHeight();
-                $('body').css('padding-top' , header_height);
-            } else {
-                $('body').css('padding-top' , '0');
-                $('.header-container').removeClass('header-fixed');
-                $('.subheader-container').removeClass('header-fixed');
-            }
-         });
-
-    </script>
-
-    <script type="text/javascript">
-            
-            $('.slider3').diyslider({
-                width: "580px", // width of the slider
-                height: "120px", // height of the slider
-                display: 5, // number of slides you want it to display at once
-                loop: false // disable looping on slides
-                }); // this is all you need!
-
-        // use buttons to change slide
-        $('#gotoleft').bind("click", function(){
-            // Go to the previous slide
-            $('.slider3').diyslider("move", "back");
-        });
-        $('#gotoright').unbind("click")
-        $('#gotoright').bind("click", function(){
-            // Go to the previous slide
-            $('.slider3').diyslider("move", "forth");
-        });
-
-
-        // NAVIRINO CLICK TOGGLE
-        $(".menu-nav").click(function()
-        {
-            $(".navirino").toggle("slow");
-        });
-
-
-        /*PRODUCT HOVER TOGGLE*/
-        $('.product-hover').hover(function()
-        {
-            $('.product-dropdown').stop();
-            $('.product-dropdown').fadeIn(400);
-        },
-        function()
-        {
-            $('.product-dropdown').stop();
-            $('.product-dropdown').fadeOut(400);
-        });
-
-        $('.company-hover').hover(function()
-        {
-            $('.company-dropdown').stop();
-            $('.company-dropdown').fadeIn(400);
-        },
-        function()
-        {
-            $('.company-dropdown').stop();
-            $('.company-dropdown').fadeOut(400);
-        });
-
-        $('.cart-hover').hover(function()
-        {
-            $('.cart-dropdown').stop();
-            $('.cart-dropdown').fadeIn(400);
-        },
-        function()
-        {
-            $('.cart-dropdown').stop();
-            $('.cart-dropdown').fadeOut(400);
-        });
-
-        /*scroll up*/
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 700) {
-                $('.scroll-up').fadeIn();
-            } else {
-                $('.scroll-up').fadeOut();
-            }
-        });
-
-        $('.scroll-up').click(function () {
-            $("html, body").animate({
-                scrollTop: 0
-            }, 700);
-            return false;
-        });
-
-        /*TEXT FADEOUT*/
-        $(window).scroll(function(){
-                $(".caption-container, .caption-logo-container").css("opacity", 1 - $(window).scrollTop() / 250);
-        });
-
-        //START MISSION AND VISION
-        $(".title-vision").click(function()
-        {
-            $("#vision").removeClass("hide");
-            $("#mission").addClass("hide");
-            $(".title-vision").addClass("highlighted");
-            $(".title-mission").removeClass("highlighted");
-            
-        });
-        
-        $(".title-mission").click(function()
-        {
-            $("#vision").addClass("hide");
-            $("#mission").removeClass("hide");
-            $(".title-mission").addClass("highlighted");
-            $(".title-vision").removeClass("highlighted");
-        });
-        //END MISSION ANF VISION
-
-    </script>
-
-    {{-- START GLOBAL MODAL --}}
-    <div id="global_modal" class="modal fade" role="dialog" >
-        <div class="modal-dialog">
-            <!-- Modal content-->
-            <div class="modal-content modal-content-global clearfix">
-            </div>
-        </div>
-    </div>
-    {{-- END GLOBAL MODAL --}}
-    {{-- GLOBAL MULTIPLE MODAL --}}
-    <div class="multiple_global_modal_container"></div>
-    {{-- END GLOBAL MULTIPLE MODAL --}}
-=======
->>>>>>> mod_edw_front_shift
 
     @yield("script")
     </body>

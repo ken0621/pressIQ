f5f5622800f2cf968f94e3b182c7ca80782bdd7f
Rommel Cuneta <cunetarommel0620@gmail.com>
2017-10-26 15:24:45 +0800

    almost done

--- public/themes/unitywealth/views/layout.blade.php
+++ public/themes/unitywealth/views/layout.blade.php
@@ -3,53 +3,50 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-        <title>{{ ucfirst($shop_info->shop_key) }} | {{ isset($page) ? $page : "" }}</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        {{-- <link rel="apple-touch-icon" href="apple-touch-icon.png"> --}}
-        <link rel="icon" href="/themes/{{ $shop_theme }}/img/unity-wealth-icon.png" type="image/png" />
-
-        <!-- GOOGLE FONT -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500" rel="stylesheet">
-        <!-- GLOBAL CSS -->
-        <link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/global.css">
-        <link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/push_sidenav.css">
-        <!-- <link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/new_sidenav.css"> -->
-
-        <!-- COUNTDOWN TIMER -->
-        <link rel="stylesheet" href="/themes/{{ $shop_theme }}/assets/css/animate.css">
-        {{-- <link rel="stylesheet" href="/themes/{{ $shop_theme }}/assets/css/form-elements.css"> --}}
-        <link rel="stylesheet" href="/themes/{{ $shop_theme }}/assets/css/style.css">
-        <link rel="stylesheet" href="/themes/{{ $shop_theme }}/assets/css/media-queries.css">    
-        
-        @include("frontend.ghead")
-        <!-- OTHER CSS -->
-        @yield("css")
-        <style type="text/css">
-        .content
-        {
-            background-color: transparent;
-        }
-        .navbar.sticky
-        {
-            position: fixed;
-            top: 0;
-            left: 0;
-            right: 0;
-            z-index: 100;
-        }
-        </style>
-        <script src="/themes/{{ $shop_theme }}/assets/initializr/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
-    </head>
-    <body class="pushmenu-push">
-        
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>{{ ucfirst($shop_info->shop_key) }} | {{ isset($page) ? $page : "" }}</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    {{-- <link rel="apple-touch-icon" href="apple-touch-icon.png"> --}}
+    <link rel="icon" href="/themes/{{ $shop_theme }}/img/unity-wealth-icon.png" type="image/png" />
+    <!-- GOOGLE FONT -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu:300,400,500" rel="stylesheet">
+    <!-- GLOBAL CSS -->
+    <link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/global.css">
+    <link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/push_sidenav.css">
+    <!-- <link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/new_sidenav.css"> -->
+    <!-- COUNTDOWN TIMER -->
+    <link rel="stylesheet" href="/themes/{{ $shop_theme }}/assets/css/animate.css">
+    {{-- <link rel="stylesheet" href="/themes/{{ $shop_theme }}/assets/css/form-elements.css"> --}}
+    <link rel="stylesheet" href="/themes/{{ $shop_theme }}/assets/css/style.css">
+    <link rel="stylesheet" href="/themes/{{ $shop_theme }}/assets/css/media-queries.css">
+    
+    @include("frontend.ghead")
+    <!-- OTHER CSS -->
+    @yield("css")
+    <style type="text/css">
+    .content
+    {
+    background-color: transparent;
+    }
+    .navbar.sticky
+    {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: 100;
+    }
+    </style>
+    <script src="/themes/{{ $shop_theme }}/assets/initializr/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
+<body class="pushmenu-push">
+    
     <div class="loader hide">
-      <span><img src="/resources/assets/frontend/img/loader.gif"></span>
+        <span><img src="/resources/assets/frontend/img/loader.gif"></span>
     </div>
     <!-- HEADER -->
     <!-- <ul class="navigation">
@@ -83,24 +80,57 @@
         <div class="container">
             <div class="row clearfix">
                 <div class="col-md-3">
-
+                    @if(request()->segment(1) != "replicated")
                     <div id="nav_list"><i class="fa fa-bars hamburger"></i></div>
-
+                    @endif
                     <nav class="pushmenu pushmenu-left">
-                      <ul class="links">
-                        <li> <a href="#">Home</a> </li>
-                        <li> <a href="#">About</a> </li>
-                        <li> <a href="#">Projects</a> </li>
-                        <li> <a href="#">Blog</a> </li>
-                        <li> <a href="#">Contacts</a> </li>
-                      </ul>
+
+                        @if($customer)
+                        <div class="space1"></div>
+                        <span>BROWSE</span>
+                        <ul class="links">
+                            <li> <a href="/">Home</a> </li>
+                            <li> <a href="javascript:">Company</a> </li>
+                            <li> <a href="javascript:">Mission & Vision</a> </li>
+                            <li> <a href="javascript:">Contact Us</a> </li>
+                        </ul>
+                        
+                        <div class="space2"></div>
+                        <span>MEMBERS AREA</span>
+                        <ul class="links">
+                            <li> <a href="/members">Dashboard</a> </li>
+                            <li> <a href="/members/profile">Profile</a> </li>
+                            @if($mlm_member)
+                            <li> <a href="javascript:">Videos</a> </li>
+                            <li> <a href="javascript:">Ebooks</a> </li>
+                            <li> <a href="/members/products">Products</a> </li>
+                            <li> <a href="/members/certificate">Certificate</a> </li>
+                            <li> <a href="/members/genealogy?mode=sponsor">Genealogy</a> </li>
+                            <li> <a href="/members/report">Reports</a> </li>
+                            <li> <a href="/members/wallet-encashment">Wallet</a> </li>
+                                @if($customer)
+                                    <li class="user-logout"> <a href="/members/logout">Logout &nbsp;<i class="fa fa-long-arrow-right" aria-hidden="true"></i></a> </li>
+                                @endif
+                            @else
+                            @endif
+                        </ul>
+                        @else
+                            <div class="space1"></div>
+                            <span>BROWSE</span>
+                            <ul class="links">
+                                <li> <a href="/">Home</a> </li>
+                                <li> <a href="javascript:">Company</a> </li>
+                                <li> <a href="javascript:">Mission & Vision</a> </li>
+                                <li> <a href="javascript:">Contact Us</a> </li>
+                            </ul>
+                        @endif
+
                     </nav>
                     <div class="image-logo-holder">
                         <a class="clearfix" href="/">
                             <img src="/themes/{{ $shop_theme }}/img/header-logo.png">
-                        </a>                       
+                        </a>
                     </div>
-
                     @if(request()->segment(1) != "replicated")
                     <!-- <div class="menu-nav">
                         <span></span>
@@ -110,19 +140,19 @@
                     @endif
                 </div>
                 <div class="col-md-9">
-                <!-- NAVIGATION -->
+                    <!-- NAVIGATION -->
                     <nav class="navirino">
                         <ul>
                             @if(Request::segment(1)=="members")
-                                <li><a class="smoth-scroll" href="/#home">HOME</a></li>
-                                <li><a class="smoth-scroll" href="/#company">COMPANY</a></li>
-                                <li><a class="smoth-scroll" href="/#mission-vision">MISSION & VISION</a></li>
-                                <li><a class="smoth-scroll" href="/#contact">CONTACT US</a></li>
+                            <li><a class="smoth-scroll" href="/#home">HOME</a></li>
+                            <li><a class="smoth-scroll" href="/#company">COMPANY</a></li>
+                            <li><a class="smoth-scroll" href="/#mission-vision">MISSION & VISION</a></li>
+                            <li><a class="smoth-scroll" href="/#contact">CONTACT US</a></li>
                             @else
-                                <li><a class="smoth-scroll" href="#home">HOME</a></li>
-                                <li><a class="smoth-scroll" href="#company">COMPANY</a></li>
-                                <li><a class="smoth-scroll" href="#mission-vision">MISSION & VISION</a></li>
-                                <li><a class="smoth-scroll" href="#contact">CONTACT US</a></li>
+                            <li><a class="smoth-scroll" href="#home">HOME</a></li>
+                            <li><a class="smoth-scroll" href="#company">COMPANY</a></li>
+                            <li><a class="smoth-scroll" href="#mission-vision">MISSION & VISION</a></li>
+                            <li><a class="smoth-scroll" href="#contact">CONTACT US</a></li>
                             @endif
                         </ul>
                     </nav>
@@ -132,9 +162,8 @@
     </div>
     <!-- CONTENT -->
     <div id="scroll-to" class="clearfix">
-       @yield("content")
+        @yield("content")
     </div>
-
     <!-- FOOTER -->
     <footer id="bottom-footer">
         <div class="container">
@@ -197,7 +226,7 @@
         </div>
     </footer>
     <div class="container">
-        <div class="bottom">                           
+        <div class="bottom">
             <div class="ftr-title">© UnityWealth. All Right Reserved</div>
             <div class="ftr-title-2">Powered By: DIGIMA WEB SOLUTIONS, Inc.</div>
         </div>
@@ -206,32 +235,27 @@
     @include("frontend.gfoot")
     {{-- GLOBALS --}}
     <script type="text/javascript" src="/themes/{{ $shop_theme }}/js/theme_custom.js"></script>
-
     <!-- COUNTDOWN TIMER -->
     <script src="/themes/{{ $shop_theme }}/assets/countdown/jquery.backstretch.min.js"></script>
     <script src="/themes/{{ $shop_theme }}/assets/countdown/jquery.countdown.min.js"></script>
     <script src="/themes/{{ $shop_theme }}/assets/countdown/wow.min.js"></script>
     <script src="/themes/{{ $shop_theme }}/assets/countdown/scripts.js?version=1"></script>
-
     <script type="text/javascript">
-        $window = $(window);
-        $window.scroll(function() {
-          $scroll_position = $window.scrollTop();
-            if ($scroll_position > 100) { 
-                $('.header-container').addClass('header-fixed');
-
-                header_height = $('.your-header').innerHeight();
-                $('body').css('padding-top' , header_height);
-            } else {
-                $('.header-container').removeClass('header-fixed');
-                $('body').css('padding-top' , '0');
-            }
-         });
-
+    $window = $(window);
+    $window.scroll(function() {
+    $scroll_position = $window.scrollTop();
+    if ($scroll_position > 100) {
+    $('.header-container').addClass('header-fixed');
+    header_height = $('.your-header').innerHeight();
+    $('body').css('padding-top' , header_height);
+    } else {
+    $('.header-container').removeClass('header-fixed');
+    $('body').css('padding-top' , '0');
+    }
+    });
     </script>
-
     <!-- FB WIDGET -->
     <div id="fb-root"></div>
     @yield("script")
-    </body>
+</body>
 </html>

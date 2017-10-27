1df1e4c61ae5a467522b1cca59c2bcd211fe4c60
Arcylen gutierrez <arcylen103095@gmail.com>
2017-10-27 13:33:47 +0800

    arc 102718

--- database/migrations/2017_10_27_132159_create_tbl_commission_102717121PM.php
+++ database/migrations/2017_10_27_132159_create_tbl_commission_102717121PM.php
@@ -15,6 +15,11 @@ class CreateTblCommission102717121PM extends Migration
     {
         Schema::create('tbl_commission', function (Blueprint $table) {
             $table->increments('commission_id');
+            $table->integer('customer_id');
+            $table->string('customer_email');
+            $table->integer('agent_id');
+            $table->date('date_created');
+            $table->date('due_date');
         });
     }
 

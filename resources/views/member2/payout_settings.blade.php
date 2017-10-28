f7790625dda62e81c28ec91ad4288540d8e48323
Arcylen gutierrez <arcylen103095@gmail.com>
2017-10-28 16:47:20 +0800

    Arc

--- resources/views/member2/payout_settings.blade.php
+++ resources/views/member2/payout_settings.blade.php
@@ -93,6 +93,79 @@
 	            </div>
 	        </div>
 		</div>
+
+		{{-- PALAWAN EXPRESS --}}
+		<div class="row bank-table payout-mode-container" content="palawan_express">
+	        <div class="clearfix modal-body">
+	            <div class="col-md-12">
+	                <div class="table-responsive">
+	                    <table class="table table-bordered">
+	                        <thead style="text-transform: uppercase">
+	                            <tr>
+	                                <th class="text-center" style="width: 100px;">SLOT CODE</th>
+	                                <th class="text-center" width="120px">NAME</th>
+	                                <th class="text-center" width="120px">Contact Number</th>
+	                            </tr>
+	                        </thead>
+	                        <tbody>
+	                        	@foreach($_slot as $slot)
+	                            <tr>
+	                            	<input type="hidden" name="remittance_slot_no[]" value="{{ $slot->slot_no }}">
+	                            	<input type="hidden" name="money_remittance_type[]" value="palawan_express">
+	                                <td class="text-center" style="vertical-align: middle;">{{ $slot->slot_no }}</td>
+	                                <td class="text-center">
+	                                	<div class="row clearfix">
+	                                		<div class="col-md-4">
+	                                			<input type="text" class="form-control" placeholder="First Name" name="first_name[]" value="{{ $slot->first_name }}">
+	                                		</div>
+	                                		<div class="col-md-4">
+	                                			<input type="text" class="form-control" placeholder="Middle Name" name="middle_name[]" value="{{ $slot->middle_name }}">
+	                                		</div>
+	                                		<div class="col-md-4">
+	                                			<input type="text" class="form-control" placeholder="Last Name" name="last_name[]" value="{{ $slot->last_name }}">	                                		
+	                                		</div>
+	                                	</div>
+	                                </td>
+	                              	<td class="text-center">
+	                                	<input name="contact_number[]" type="text" value="{{ $slot->contact_number }}" class="form-control text-center" >
+	                                </td>
+	                            </tr>
+	                            @endforeach
+	                        </tbody>
+	                    </table>
+	                </div>
+	            </div>
+	        </div>
+		</div>
+
+		{{-- COINS.PH CARD --}}
+		<div class="row payout-mode-container" content="coinsph">
+	        <div class="clearfix modal-body"> 
+	                <div class="col-md-12">
+	                    <div class="table-responsive">
+	                        <table class="table table-bordered">
+	                            <thead style="text-transform: uppercase">
+	                                <tr>
+	                                    <th class="text-center" style="width: 100px;">SLOT CODE</th>
+	                                    <th class="text-center">WALLET ADDRESS</th>
+	                                </tr>
+	                            </thead>
+	                            <tbody>
+	                            	@foreach($_slot as $slot)
+	                                <tr>
+	                                	<input type="hidden" name="coinsph_slot_no[]" value="{{ $slot->slot_no }}">
+	                                    <td class="text-center" style="vertical-align: middle;">{{ $slot->slot_no }}</td>
+	                                    <td class="text-center">
+	                                    	<input type="text" class="form-control text-center" name="wallet_address[]" value="{{ $slot->wallet_address }}">
+	                                    </td>
+	                                </tr>
+	                                @endforeach
+	                            </tbody>
+	                        </table>
+	                    </div>
+	                </div>
+	        </div>
+		</div>
 	</div>
 	<div class="modal-footer">
 		<button type="button" class="btn btn-def-white btn-custom-white" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>

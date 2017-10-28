df245221d206924d394839fd37ea6c4522df8b24
Guillermo Tabligan <gtplus.net@gmail.com>
2017-10-28 19:20:35 +0800

    change

--- app/Http/Controllers/Member/MlmDeveloperController.php
+++ app/Http/Controllers/Member/MlmDeveloperController.php
@@ -866,6 +866,7 @@ class MlmDeveloperController extends Member
         $shop_id = $this->user_info->shop_id;
         $slot_id = request("slot_id");
         $membership_id = request("membership_id");
+        $setting    = Tbl_mlm_plan_setting::where("shop_id",$shop_id)->first();
 
         $sponsor_info = Tbl_mlm_slot::where("slot_no", request("sponsor"))->where("shop_id", $shop_id)->first();
         $placement_info = Tbl_mlm_slot::where("slot_no", request("placement"))->where("shop_id", $shop_id)->first();
@@ -942,14 +943,16 @@ class MlmDeveloperController extends Member
                 $update["slot_placement"] = $placement_info->slot_id;
             }
             
+            Tbl_tree_placement::where("placement_tree_parent_id", $slot_id)->delete();
+            Tbl_tree_placement::where("placement_tree_child_id", $slot_id)->delete();
+            Tbl_tree_sponsor::where("sponsor_tree_parent_id", $slot_id)->delete();
+            Tbl_tree_sponsor::where("sponsor_tree_child_id", $slot_id)->delete();
+            Tbl_mlm_slot_wallet_log::where("wallet_log_slot_sponsor", $slot_id)->delete();
+            Tbl_mlm_slot_points_log::where("points_log_Sponsor", $slot_id)->delete();
+
+
             if(request('membership_id') == 'delete_slot')
             {
-                Tbl_tree_placement::where("placement_tree_parent_id", $slot_id)->delete();
-                Tbl_tree_placement::where("placement_tree_child_id", $slot_id)->delete();
-                Tbl_tree_sponsor::where("sponsor_tree_parent_id", $slot_id)->delete();
-                Tbl_tree_sponsor::where("sponsor_tree_child_id", $slot_id)->delete();
-                Tbl_mlm_slot_wallet_log::where("wallet_log_slot_sponsor", $slot_id)->delete();
-                Tbl_mlm_slot_points_log::where("points_log_Sponsor", $slot_id)->delete();
                 Tbl_mlm_slot::where("slot_id", $slot_id)->delete();
             }
             else
@@ -957,9 +960,17 @@ class MlmDeveloperController extends Member
                 $update["slot_position"] = request("position");
                 $update["slot_membership"] = request("membership_id");
                 Tbl_mlm_slot::where("slot_id", $slot_id)->update($update);
+
+                $slot_info_e = Tbl_mlm_slot::where('slot_id', $slot_id)->first();
+
+                if($setting->plan_settings_placement_required == 1)
+                {
+                    Mlm_tree::insert_tree_placement($slot_info_e, $slot_info_e, 1);
+                }
+
+                Mlm_tree::insert_tree_sponsor($slot_info_e, $slot_info_e, 1);
+                MLM2::entry($shop_id, $slot_id);
             }
-
-
 
             $return["status"] = "success";
             $return["call_function"] = "modify_slot_success";

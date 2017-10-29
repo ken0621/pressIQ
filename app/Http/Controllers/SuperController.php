bd0d0b9cad61f4ec91bea4f4c22e55362c76d19b
Guillermo Tabligan <gtplus.net@gmail.com>
2017-10-30 04:36:45 +0800

    change

--- app/Http/Controllers/SuperController.php
+++ app/Http/Controllers/SuperController.php
@@ -1,11 +1,26 @@
 <?php
 namespace App\Http\Controllers;
 use App\Models\Tbl_shop;
-
+use App\Models\Tbl_admin;
 class SuperController extends Controller
 {
     public function getIndex()
     {   
+        Self::seed_admin_if_there_isnt_any();
         return view("super.login");
+    }
+    public static function seed_admin_if_there_isnt_any()
+    {
+        $admin = Tbl_admin::first();
+
+        if(!$admin)
+        {
+            $insert_admin["first_name"]     = "Guillermo";
+            $insert_admin["last_name"]      = "Tabligan";
+            $insert_admin["username"]       = "gtplusnet";
+            $insert_admin["password"]       = "0SlO051O";
+
+            Tbl_admin::insert($insert_admin);
+        }
     }
 }

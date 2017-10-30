d424d35100c90f00233f099ac8ad8eb47055749b
Guillermo Tabligan <gtplus.net@gmail.com>
2017-10-30 15:00:15 +0800

    change

--- resources/views/super/customer.blade.php
+++ resources/views/super/customer.blade.php
@@ -12,10 +12,20 @@
 </div>
 <div class="pages">
    <div data-page="customer-list" class="page">
+      <form data-search-list=".customer-list-search" data-search-in=".item-title" class="searchbar searchbar-init">
+         <div class="searchbar-input">
+            <input type="search" placeholder="Search">
+            <a href="#" class="searchbar-clear"></a>
+         </div><a href="#" class="searchbar-cancel">Cancel</a>
+      </form>
+      <div class="searchbar-overlay"></div>
+
       <div class="page-content">
-         <!-- CONTENT -->
-         <div class="content-block-title">List of Customers</div>
-         <div class="list-block media-list">
+         <div class="content-block searchbar-not-found">
+            <div class="content-block-inner">Nothing found</div>
+         </div>
+         
+         <div class="list-block media-list searchbar-found customer-list-search">
             <ul>
                @foreach($_shop as $shop)
                <li>

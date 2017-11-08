a16be1dfb48d15d66107c4103e735154cd0b291c
Rommel Cuneta <cunetarommel0620@gmail.com>
2017-11-08 18:07:07 +0800

    sov

--- public/themes/sovereign/views/member/dashboard.blade.php
+++ public/themes/sovereign/views/member/dashboard.blade.php
@@ -70,11 +70,6 @@
 						</div>
 					</div>
 				</div>
-
-				<div class="btn-container">
-					<button class="btn btn-primary">Enter a code</button>
-				</div>
-				
 			</div>
 
 			<div class="col-md-6">

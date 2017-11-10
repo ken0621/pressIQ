90f864b091d6d59f082283d4b212026a043d8338
kimbriel <kimbriel21@gmail.com>
2017-11-10 09:18:41 +0800

    done

--- app/Http/Controllers/Member/Payroll_BioImportController.php
+++ app/Http/Controllers/Member/Payroll_BioImportController.php
@@ -653,6 +653,7 @@ class Payroll_BioImportController extends Member
 
     public function import_ZKTime_5_0($file, $company)
     {
+    	
     	$message = '<center><i><span class="color-red"><b>Invalid File Format</b></span></i></center>';
 
     	$_time = Excel::selectSheetsByIndex(0)->load($file, function($reader){})->get(array('no','datetime'));

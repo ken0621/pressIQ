de6f45dfc9f132b475e8ab4e547612c3d59ecc40
DigiMAI <mai.digima@gmail.com>
2017-11-16 09:25:54 +0800

    nice home

--- public/themes/nice/views/home.blade.php
+++ public/themes/nice/views/home.blade.php
@@ -10,6 +10,206 @@
             </div>
         </div>
     </div>
+
+    <div class="wrapper-1">
+        <div class="title-container">Who we are</div>
+        <div class="container">
+            <div class="row clearfix">
+                <div class="col-md-6">
+                    <div class="left-wrapper-1">
+                        <div class="paragraph-container">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate sunt, quisquam quas, at itaque voluptatibus animi molestias voluptatem, dignissimos modi laboriosam dolorem ipsam dolorum molestiae repellat rerum ea neque odio doloremque. Id nam et, repudiandae, reiciendis officiis iusto rem iure dolor similique, suscipit at quis alias expedita exercitationem, quidem dolorem dolorum? Placeat unde reiciendis laborum soluta quasi neque, molestias ab.
+                        </div>
+                        <div  class="button-container">
+                            <a href="#">More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="image-container">
+                        <img src="/themes/{{ $shop_theme }}/img/wrapper1-image.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="wrapper-2">
+        <div class="title-container">Why Join in NICEnterprises</div>
+        <div class="container">
+            <div class="row-no-padding clearfix">
+                <div class="col-md-4">
+                    <div class="image-container">
+                        <img src="/themes/{{ $shop_theme }}/img/wrapper2-image1.png">
+                        <div class="title-image-container">Lorem Ipsum.</div>
+                        <div class="info-container">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis cumque minima, molestias totamPorro perspici atis eveniet veniam voluptatum Illum officiis magnam eos, fuga architecto nobi culpa commodi, sunt deleniti assumenda voluptatibus dicta?</div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="image-container center">
+                        <img src="/themes/{{ $shop_theme }}/img/wrapper2-image2.png">
+                        <div class="title-image-container">Lorem Ipsum.</div>
+                        <div class="info-container">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis cumque minima, molestias totamPorro perspici atis eveniet veniam voluptatum Illum officiis magnam eos, fuga architecto nobi culpa commodi, sunt deleniti assumenda voluptatibus dicta?</div>
+                    </div> 
+                </div>
+                <div class="col-md-4">
+                    <div class="image-container">
+                        <img src="/themes/{{ $shop_theme }}/img/wrapper2-image3.png">
+                        <div class="title-image-container">Lorem Ipsum.</div>
+                        <div class="info-container">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis cumque minima, molestias totamPorro perspici atis eveniet veniam voluptatum Illum officiis magnam eos, fuga architecto nobi culpa commodi, sunt deleniti assumenda voluptatibus dicta?</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="wrapper-3">
+        <div class="title-container">Membership Packages</div>
+        <div class="container">
+            <div class="row clearfix">
+                <div class="col-md-4">
+                    <div class="border-container">
+                        <div class="border-background-container">
+                            <div class="package-container">Package 1<br>$20</div>
+                            <div class="details-container">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat possimus pariatur perferendis, inventore ex error fuga labore cumque! Qui, facere hic eligendi architecto quisquam nobis ipsum tenetur. Sapiente, quis, possimus. Odit vitae provident ipsum facere laborum ullam modi, adipisci optio assumenda, eaque vero reprehenderit aspernatur!
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="border-container">
+                        <div class="border-background-container">
+                            <div class="package-container">Package 2<br>$20</div>
+                            <div class="details-container">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat possimus pariatur perferendis, inventore ex error fuga labore cumque! Qui, facere hic eligendi architecto quisquam nobis ipsum tenetur. Sapiente, quis, possimus. Odit vitae provident ipsum facere laborum ullam modi, adipisci optio assumenda, eaque vero reprehenderit aspernatur!
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="border-container">
+                        <div class="border-background-container">
+                            <div class="package-container">Package 3<br>$20</div>
+                            <div class="details-container">
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat possimus pariatur perferendis, inventore ex error fuga labore cumque! Qui, facere hic eligendi architecto quisquam nobis ipsum tenetur. Sapiente, quis, possimus. Odit vitae provident ipsum facere laborum ullam modi, adipisci optio assumenda, eaque vero reprehenderit aspernatur!
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper-4">
+        <div class="container">
+            <div class="wrapper-4-top">
+                <div class="title-container">Our Partners</div>
+                <div class="container">
+                    <div class="row clearfix">
+                        <div class="col-md-3">
+                            <div class="image-holder">
+                                    <img src="/themes/{{ $shop_theme }}/img/partner1.png">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="image-holder">
+                                    <img src="/themes/{{ $shop_theme }}/img/partner2.png">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="image-holder">
+                                    <img src="/themes/{{ $shop_theme }}/img/partner3.png">
+                            </div>
+                        </div>
+                        <div class="col-md-3">
+                            <div class="image-holder">
+                                    <img src="/themes/{{ $shop_theme }}/img/partner4.png">
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="wrapper-4-bottom">
+                <div class="title-container">Become a Business Partner</div>
+                <div class="details-container">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, recusandae. Id minus perferendis ullam quia, enim earum reprehenderit?</div>
+                <div class="button-container">
+                    <a href="#">Become a Member</a>
+                </div>
+            </div>
+        </div>
+        <div class="wrapper-5">
+            <div class="container">
+                <div class="row clearfix">
+                    <div class="col-md-6">
+                        <div class="register-container">
+                            <div class="title-container">Get In Touch With Us</div>
+                            <div class="row clearfix">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="First Name*">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Last Name*">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <input type="phone" class="form-control" placeholder="Phone Number*">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" placeholder="Email Address*">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <input type="text area" class="form-control" placeholder="Subject*">
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <textarea type="text" class="form-control text-message" placeholder="Message">
+                                        </textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="button-container">
+                                <a href="#">SEND</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="location-container">
+                            <div class="title-container">Location</div>
+                            <div class="title-container">PRINCIPAL OFFICE</div>
+                            <div class="desc-title">Vicar's Bldg. #31 Visayas Avenue Corner Road 1 Vasra, Quezon City</div>
+                            <div class="title-container">GENSAN BRANCH OFFICE</div>
+                            <div class="desc-title">Door #2 Perla Compania de Seguros Bldg. Jp. Laurel East, Corner Sampaguita Street, General Santos City</div>
+                            <div class="contact-container">
+                                <span class="icon"><i class="fa fa-mobile" aria-hidden="true"></i></span>
+                                <span>+63 2 518 8637</span>
+                            </div>
+                             <div class="contact-container">
+                                <span class="icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+                                <span>sales@3xcell.com</span>
+                            </div>
+                            <div class="title-container">BUSINESS HOURS</div>
+                            <span class="icon"><i class="fa fa-clock-o" aria-hidden="true"></i></span>
+                            <span class="business-hours">BUSINESS HOURS</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        
+        
+    </div>
+
     <!-- SCROLL TO TOP -->
     <div class="scroll-up"><img src="/themes/{{ $shop_theme }}/img/scroll-up.png"></div>
 </div>

11422df9b856a7894a60b0978c87ae2b592eba79
arcylen gutierrez <arcylen103095@gmail.com>
2017-11-23 17:27:45 +0800

    arc

--- app/Http/Controllers/Member/ReportLogisticController.php
+++ app/Http/Controllers/Member/ReportLogisticController.php
@@ -22,19 +22,21 @@ class ReportLogisticController extends Member
         $data['head_discription'] = '';
 
         $report_type    = Request::input('report_type');
+        $payment_type    = Request::input('payment_type');
         $load_view      = Request::input('load_view');
-        $period         = Request::input('report_period') ? Request::input('report_period') : 'all';
+        $period         = Request::input('report_period') ? Request::input('report_period') : 'this_week';
         $date['start']  = Request::input('from');
         $date['end']    = Request::input('to');
         $data['from']   = Report::checkDatePeriod($period, $date)['start_date'];
         $data['to']     = Report::checkDatePeriod($period, $date)['end_date'];
-        $data['_report'] = Transaction::get_all_transaction_item($this->user_info->shop_id, $data['from'], $data['to'], 'receipt');
+        $data['_report'] = Transaction::get_all_transaction_item($this->user_info->shop_id, $data['from'], $data['to'], 'receipt', $payment_type);
+        $data['payment_method'] = $payment_type;
 
         /* IF REPORT TYPE IS EXIST AND NOT RETURNING VIEW */
         if($report_type && !$load_view)
         {
             $view =  'member.reports.output.logistic_report'; 
-            return Report::check_report_type($report_type, $view, $data, 'Logistic Report-'.Carbon::now(), 'portrait');
+            return Report::check_report_type($report_type, $view, $data, 'Logistic Report-'.Carbon::now(), 'landscape');
         }
         else
         {

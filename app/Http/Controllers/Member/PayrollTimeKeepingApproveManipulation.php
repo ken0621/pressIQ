7314701c6b641faf99c43a9ca198c5c6b2ea5365
kimbriel <kimbriel21@gmail.com>
2017-11-24 11:50:56 +0800

    done

--- app/Http/Controllers/Member/PayrollTimeKeepingApproveManipulation.php
+++ app/Http/Controllers/Member/PayrollTimeKeepingApproveManipulation.php
@@ -21,22 +21,40 @@ class PayrollTimeKeepingApproveManipulation extends Member
 {
 	public function time_breakdown($period_company_id, $employee_id)
 	{
-		$_timekeeping_approve = Tbl_payroll_time_keeping_approved::where('payroll_period_company_id',$period_company_id)->get();
 		
-		foreach ($_timekeeping_approve as $key => $timekeeping_approve) 
-		{
-			
+		
+		if ($employee_id != 0) {
+			$timekeeping_approve = Tbl_payroll_time_keeping_approved::where('payroll_period_company_id',$period_company_id)->where("employee_id", $employee_id)->get();
 			$time_keeping_approve_id = $timekeeping_approve->time_keeping_approve_id;
 
 			$cutoff_breakdown = unserialize($timekeeping_approve->cutoff_breakdown);
-			$cutoff_breakdown->_time_breakdown["absent"]["float"] =0;
+			$cutoff_breakdown->_time_breakdown["absent"]["float"] = 0;
 			$cutoff_breakdown->_time_breakdown["absent"]["time"]  = "No Absent";
 			
 			$update["cutoff_breakdown"] = serialize($cutoff_breakdown);
 
 			Tbl_payroll_time_keeping_approved::where('time_keeping_approve_id', $time_keeping_approve_id)->update($update);
 		}
-		dd(unserialize($_timekeeping_approve[0]->cutoff_breakdown));
+		else
+		{
+			$_timekeeping_approve = Tbl_payroll_time_keeping_approved::where('payroll_period_company_id',$period_company_id)->get();
+			foreach ($_timekeeping_approve as $key => $timekeeping_approve) 
+			{
+				
+				$time_keeping_approve_id = $timekeeping_approve->time_keeping_approve_id;
+
+				$cutoff_breakdown = unserialize($timekeeping_approve->cutoff_breakdown);
+				$cutoff_breakdown->_time_breakdown["absent"]["float"] = 0;
+				$cutoff_breakdown->_time_breakdown["absent"]["time"]  = "No Absent";
+				
+				$update["cutoff_breakdown"] = serialize($cutoff_breakdown);
+
+				Tbl_payroll_time_keeping_approved::where('time_keeping_approve_id', $time_keeping_approve_id)->update($update);
+			}
+			dd(unserialize($_timekeeping_approve[0]->cutoff_breakdown));
+		}
+		
+		
 	}
 		
 }

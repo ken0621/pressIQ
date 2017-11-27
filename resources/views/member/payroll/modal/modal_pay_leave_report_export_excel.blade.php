fdfd72ffa66ff5fe9a52535c1e2eb38e1845f662
nethkenn <johnkenneth.delara@gmail.com>
2017-11-27 10:36:56 +0800

    kenneth_create_

---
+++ resources/views/member/payroll/modal/modal_pay_leave_report_export_excel.blade.php
@@ -0,0 +1,45 @@
+	    <h4 class="modal-title"><b>Remaining - Leave Records</b> <i><br> Remaining - Leave Records</i></h4>
+
+<div class="modal-body clearfix">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-condensed">
+                        <thead>
+                            <tr>
+                                <th class="text-center">Leave Name</th>
+                                <th class="text-center">Employee Code</th>
+                                <th class="text-center">Employee Name</th>
+                                <th class="text-center">Leave Credits</th>
+                                <th class="text-center">Used Leave with Pay</th>
+                                <th class="text-center">Remaining Leave</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+{{--                             @if(isset($leave_report))
+                            @foreach($leave_report as $leave_data)
+                                @foreach($leave_data as $leave)
+                            <tr>
+                                <td class="text-center">{{ $leave->payroll_leave_temp_name }}</td>
+                                <td class="text-center">{{ $leave->payroll_employee_id }}</td>
+                                <td class="text-center">{{ $leave->payroll_employee_display_name }}</td>
+                                <td class="text-center">{{ $leave->payroll_leave_temp_hours }}</td>
+                                <td class="text-center">{{ $leave->total_leave_consume }}</td>
+                                <td class="text-center">{{ $leave->remaining_leave }}</td>
+                            </tr>
+                                 @endforeach
+                            @endforeach
+                            @endif --}}
+                        </tbody>
+                    </table>
+                </div>
+    </div>
+<style>
+.wa{
+    background-color: #ffff99 !important;
+     border: 5px solid #2C2C2C;
+}
+.empname{
+    background-color: #ccffff !important;
+        border: 5px solid #2C2C2C;
+}
+
+</style>

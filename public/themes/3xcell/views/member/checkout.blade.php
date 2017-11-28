b36700b79b084e4df7fc21318c9086471ef6515c
Rommel Cuneta <cunetarommel0620@gmail.com>
2017-11-28 16:09:15 +0800

    prevent from proceeding when fields are empty 3xcell

--- public/themes/3xcell/views/member/checkout.blade.php
+++ public/themes/3xcell/views/member/checkout.blade.php
@@ -27,7 +27,7 @@
 								<div class="form-container">
 									<div class="form-label">Province</div>
 									<div class="form-input">
-										<select name="customer_state">
+										<select name="customer_state" class="province-is-empty">
 											<option value="" hidden>Select Province</option>
 											@foreach($_locale as $locale)
 											<option value="{{ $locale->locale_name }}">{{ $locale->locale_name }}</option>
@@ -38,7 +38,7 @@
 								<div class="form-container">
 									<div class="form-label">Complete Shipping Address</div>
 									<div class="form-input">
-										<textarea required name="customer_street" placeholder="Type your complete address here"></textarea>
+										<textarea class="textarea-is-empty" required name="customer_street" placeholder="Type your complete address here"></textarea>
 									</div>
 								</div>
 							</div>
@@ -131,7 +131,7 @@
 								<div class="top-title">How do you want to pay?</div>
 								<div class="option">
 									<div class="form-input">
-										<select name="method" required="required">
+										<select name="method" required="required" class="payment-method-is-empty">
 											<option value="" hidden>Select Payment Method</option>
 											@foreach($_payment as $payment)
 												<option value="{{ $payment->link_reference_name }}">{{ $payment->method_name }}</option>
--- public/themes/3xcell/views/member/return_policy.blade.php
+++ public/themes/3xcell/views/member/return_policy.blade.php
@@ -100,7 +100,38 @@
         </div>
         <div class="modal-footer">
              <button type="button" class="btn btn-cancel" data-dismiss="modal">Cancel</button>
-             <button type="button" onClick="$('.submitt-asd').submit();" id="proceed" class="btn btn-proceed" data-dismiss="modal">Proceed</button>
+             <button type="button" id="proceed" class="btn btn-proceed" data-dismiss="modal">Proceed</button>
         </div>
     </div>
 </div>
+
+<script>
+    $(".btn-proceed").unbind('click');
+    $(".btn-proceed").bind('click', function()
+    {
+        var province = $(".province-is-empty").val();
+        var payment_method = $(".payment-method-is-empty").val();
+        var shipping_address = $(".textarea-is-empty").val();
+
+        if (province == "") 
+        {
+            alert("Please select a province.");
+        }
+
+        else if(shipping_address == "")
+        {
+            alert("Please add your complete address");
+        }
+
+        else if(payment_method == "")
+        {
+            alert("Please select a payment method.");
+        }
+
+        else
+        {
+            $('.submitt-asd').submit();
+        }
+
+    });
+</script>

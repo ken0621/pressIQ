db1e51fb32722894feba4727ad1d42153dec6b8e
carlosegovia <carlosegovia5@gmail.com>
2017-11-29 09:00:40 -0800

    press

--- public/themes/pressrelease/views/press_member/member.blade.php
+++ public/themes/pressrelease/views/press_member/member.blade.php
@@ -49,6 +49,35 @@
     					   		</div>
     					   </div>
 
+    					   <div id="press-release-view" class="tab-pane fade">
+    					   		<div class="row-no-padding clearfix">
+    					   			<div class="col-md-9">
+    					   				<div class="press-view-holder">
+    					   					<div class="title-container"><a href="/pressmember">Press Release 1</a></div>
+    					   					<div class="date-container">November 25, 2017</div>
+    					   					<div class="details-container">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod corporis possimus laboriosam
+    					   					</div>
+    					   					<div class="details-container">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse ipsam mollitia velit, dicta optio, magni harum! Alias vero, doloremque, illo et nihil pariatur quidem, quia libero nisi reiciendis ut? Modi a corporis nesciunt, ullam commodi nostrum dolor dolores sint, molestias tenetur, delectus accusantium sunt sit, natus repellat. Ab tenetur commodi fugiat modi vero error, veritatis eligendi aut voluptas quae praesentium?
+    					   					</div>
+    					   					<div class="details-container">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse ipsam mollitia velit, dicta optio, magni harum! Alias vero, doloremque, illo et nihil pariatur quidem, quia libero nisi reiciendis ut? Modi a corporis nesciunt, ullam commodi nostrum dolor dolores sint, molestias tenetur, delectus accusantium sunt sit, natus repellat. Ab tenetur commodi fugiat modi vero error, veritatis eligendi aut voluptas quae praesentium?
+    					   					</div>
+    					   				</div>
+    					   			</div>
+    					   			<div class="col-md-3">
+    					   				<div class="press-others-holder">
+    					   					<div class="title-container"><a href="/pressmember/view">Press Release 1</a></div>
+    					   					<div class="date-container">November 25, 2017</div>
+    					   					<div class="border"></div>
+    					   					<div class="title-container"><a data-toggle="tab" href="#">Press Release 2</a></div>
+    					   					<div class="date-container">November 25, 2017</div>
+    					   					<div class="border"></div>
+    					   					<div class="title-container"><a data-toggle="tab" href="#">Press Release 3</a></div>
+    					   					<div class="date-container">November 25, 2017</div>
+    					   				</div>
+    					   			</div>
+    					   		</div>
+    					   </div>
+
     					   <!-- Press Release -->
     					   <div id="press_release" class="tab-pane fade in active">
     					   		<div class="press-release-container">
@@ -102,17 +131,15 @@
 													</tr>
 												</table>
 											</div>
-											
 										</div>
 									</div>
-    					   
     					   		</div>
     					   </div>
 
     					   <!-- My Press Release -->
     					   <div id="my_press_releases" class="tab-pane fade">
 								<div class="my-press-release-container">
-									<div class="title-container"><a href="/pressmember/view">Press Release 1</a></div>
+									<div class="title-container"><a data-toggle="tab" href="#press-release-view">Press Release 1</a></div>
 									<div class="date-container">November 25, 2017</div>
 									<div class="details-container">Lorem ipsum dolor sit amet, consectetur adipisicing elit. sed do eiusmod tempor incididunt ut labore</div>
 									<div class="border"></div>

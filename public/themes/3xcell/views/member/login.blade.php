5f19c5aab170901b05f2bc26500133e1f2bcca1f
Rommel Cuneta <cunetarommel0620@gmail.com>
2017-11-29 17:44:47 +0800

    image upon login

--- public/themes/3xcell/views/member/login.blade.php
+++ public/themes/3xcell/views/member/login.blade.php
@@ -6,9 +6,9 @@
 		<table>
 			<tbody>
 				<tr>
-					<td class="c1" style="background-color: #ddd;">
+					<td class="c1">
 						<div class="register-side">
-					
+							<img src="/themes/{{ $shop_theme }}/img/login-img.jpg">
 						</div>
 					</td>
 					<td class="c2">
@@ -16,7 +16,7 @@
 					</td>
 				</tr>
 			</tbody>
-		</table>		
+		</table>	
 	</div>
 </div>
 @endsection

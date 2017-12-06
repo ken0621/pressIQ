8348341762cb4338b7bfdfc8f2b6e430f76de6b8
Rommel Cuneta <cunetarommel0620@gmail.com>
2017-12-06 12:40:08 +0800

    include image in auto login

--- public/themes/shift/views/member/login.blade.php
+++ public/themes/shift/views/member/login.blade.php
@@ -14,9 +14,9 @@
 					<td class="c2">
 						<div class="register-form">
 							<div class="text-right social-button">
-								<a href="{{$fb_login_url or '#'}}" class="holder fb">
+								{{-- <a href="{{$fb_login_url or '#'}}" class="holder fb">
 									<div class="name"><i class="fa fa-facebook" aria-hidden="true"></i> Sign in with Facebook</div>
-								</a>
+								</a> --}}
 								<a href="javascript:" class="holder gp" id="customBtn">
 									<div class="name "><i class="fa fa-google-plus" aria-hidden="true"></i> Sign in with Google+</div>
 								</a>

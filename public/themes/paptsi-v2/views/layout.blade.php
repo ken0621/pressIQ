5f18fefb62678ab3ea1bbde6b1f41508c1283b24
Mark Figues <markcarlofigues.digima@gmail.com>
2017-12-06 10:37:21 +0800

    paptsu

--- public/themes/paptsi-v2/views/layout.blade.php
+++ public/themes/paptsi-v2/views/layout.blade.php
@@ -11,7 +11,7 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- <link rel="apple-touch-icon" href="apple-touch-icon.png"> -->
-        <link rel="icon" href="/themes/{{ $shop_theme }}/img/nice-icon.png" type="image/png" />
+        <link rel="icon" href="/themes/{{ $shop_theme }}/img/header-logo.jpg" type="image/jpg" />
     
         <!-- GOOGLE FONT -->
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet"> 

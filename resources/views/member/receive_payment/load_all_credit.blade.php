d9288132ad1d8b17e2b6967c0dfeda2e2537770a
arcylen gutierrez <arcylen103095@gmail.com>
2017-12-12 16:51:18 +0800

    Arc save 121217451

--- resources/views/member/receive_payment/load_all_credit.blade.php
+++ resources/views/member/receive_payment/load_all_credit.blade.php
@@ -6,30 +6,36 @@
 	<div class="row">
         <div class="clearfix modal-body">
             <div class="form-horizontal">
+                @if($customer_id)
                 <div class="form-group">
                     <h4>Customer Name : {{isset($customer_data) ? ($customer_data->company != '' ? $customer_data->company  : $customer_data->first_name. ' '.$customer_data->last_name) : 'No Customer'}}</h4>
                 </div>
-            </div>
-            <div class="form-horizontal">
-                @if($customer_id)
                 <div class="form-group">
                     @if(count($_credits) > 0)
-                    <table class="table table-condensed table-bordered">
+                    <table class="table table-condensed table-bordered {{$total_credit = 0}}">
                         <thead>
                             <tr>
-                                <th class="text-center"><input type="checkbox" name=""></th>
+                                <th class="text-center"><input type="checkbox" class="check-all-credit" name=""></th>
                                 <th>Credit Memo Number</th>
                                 <th>Amount</th>
                             </tr>
                         </thead>
                         <tbody>
                         @foreach($_credits as $credit)
-                        <tr>
-                            <td class="text-center"><input type="checkbox" name=""></td>
+                        <tr class="{{$total_credit+= $credit->cm_amount}} tr-credit">
+                            <td class="text-center"><input type="checkbox" class="td-credit" name="apply_credit[{{$credit->cm_id}}]" data-content="{{$credit->cm_amount}}"></td>
                             <td>{{$credit->cm_id}}</td>
-                            <td>{{currency('PHP',$credit->cm_amount)}}</td>
+                            <td class="text-center">{{currency('PHP',$credit->cm_amount)}}</td>
                         </tr>
                         @endforeach
+                        <tr>
+                            <td colspan="2" class="text-right"><b> Total Credit</b> </td>
+                            <td class="text-center"><b> {{currency('PHP',$total_credit)}}</b></td>
+                        </tr>
+                        <tr>
+                            <td colspan="2" class="text-right"><b style="color:green"> Total Amount to Apply Credit</b> </td>
+                            <td class="text-center"><b class="total-apply-credit"> {{currency('PHP',0.00)}}</b></td>
+                        </tr>
                         </tbody>
                     </table>
                     @else
@@ -47,3 +53,30 @@
 	<button type="button" class="btn btn-def-white btn-custom-white" data-dismiss="modal">Close</button>
 	<button class="btn btn-primary btn-custom-primary" type="button">Apply</button>
 </div>
+<script type="text/javascript">
+    $(document).ready(function()
+    {
+        $('body').on('click','.check-all-credit', function()
+        {
+            $('input:checkbox').not(this).prop('checked', this.checked); 
+            compute_credit();        
+        });
+        $('body').on('click','.td-credit', function()
+        {
+            $(this).prop('checked', this.checked); 
+            compute_credit();        
+        });
+        function compute_credit()
+        {
+            var total_apply_credit = 0;
+            $('.td-credit').each(function(a, b)
+            {
+                if($(b).is( ":checked" ))
+                {
+                    total_apply_credit += parseFloat($(b).attr('data-content'));
+                }
+            });
+            $('.total-apply-credit').html('PHP '+total_apply_credit.toFixed(2));
+        }       
+    })
+</script>

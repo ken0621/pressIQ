20e6abd7bc2d7b47e917d6e0189f2db79b926f83
Patrick Manarang <Patrick Manarang>
2017-12-12 18:06:41 +0800

    report spelling

--- app/helpers/page.php
+++ app/helpers/page.php
@@ -1412,7 +1412,7 @@ function page_list($pis = 0)
     $code = "merchant-commission-report";
     $nav[$page]['submenu'][$code]['label'] = "Commission Report";
     $nav[$page]['submenu'][$code]['code'] = $code;
-    $nav[$page]['submenu'][$code]['url'] = $path . "merchant" . "/commission-reoprt";
+    $nav[$page]['submenu'][$code]['url'] = $path . "merchant" . "/commission-report";
     $nav[$page]['submenu'][$code]['user_settings'] = ['access_page'];
     $nav[$page]['submenu'][$code]['status'] = "on progress";
     $nav[$page]['submenu'][$code]['developer'] = "<span style='color: blue'>Patrick Manarang</span>";

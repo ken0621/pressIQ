65f1a6ef119f6150491b6cbcb2c506f88c5a8cde
Oliver Desu <oliver@oliver.ver>
2017-12-13 17:11:40 +0800

    Done Recipient

--- public/themes/pressrelease/views/press_user/choose_recipient.blade.php
+++ public/themes/pressrelease/views/press_user/choose_recipient.blade.php
@@ -6,9 +6,7 @@
    height: 400px;
    overflow-y: scroll;
 }
-
 </style>
-
 <div class="popup-choose">
    <!-- Modal content-->
    <div class="modal-content" id="recipient-modal" name="recipient-modal">
@@ -17,7 +15,7 @@
          <div class="title-container">Found Result</div>
       </div>
       <div class="modal-body">
-            <div class="row pull-right" style="padding:10px 10px 10px 10px;">
+            <div class="row pull-right" style="padding:10px 10px 10px 10px; ">
                <button  type="button" id="select_all" name="select_all" class="btn btn-success" >Select All Found</button>
                <button  type="button" id="unselect_all" name="unselect_all" class="btn btn-success" >Unselect All Found</button>
             </div>
@@ -37,26 +35,27 @@
                            <th style="text-align: center;width: 25%;">COMPANY</th>
                            <th style="text-align: center;width: 25%;">RECIPIENT</th>
                            <th style="text-align: center;width: 25%;">POSITION</th>   
+                           <th style="display:none;">Email</th>   
                          
                         </tr>
-                     </thead>
+                     </thead> 
                      <tbody>
                         @foreach($_recipient as $recipients)
                         <tr>
-                           <input type="hidden" id="recipient_id" name="recipient_id" value="{{$recipients->recipient_id}}">
-                           <td style="text-align: center;"><input type="checkbox" name="checkbox" value="{{$recipients->recipient_id}}" ></td>
+                           <input type="hidden" id="recipient_id" name="recipient_id[]" value="{{$recipients->recipient_id}}">
+                           <td style="text-align: center;"><input type="checkbox" class="recipient_checkbox" name="checkbox" value="{{$recipients->recipient_id}}" ></td>
                            <td style="text-align: center;">{{$recipients->company_name}}</td>
-                           <td style="text-align: center;">{{$recipients->name}}</td>
+                           <td class="rec_name_{{$recipients->recipient_id}}" style="text-align: center;">{{$recipients->name}}</td>
                            <td style="text-align: center;">{{$recipients->position}}</td>
-                           
+
+                           <td class="rec_email_{{$recipients->recipient_id}}" style="display:none;">{{$recipients->research_email_address}}</td>
                         </tr>
                         @endforeach
                      </tbody>
-                     
                   </table>
                </div>
                <div class="row pull-right" style="padding:10px 10px 10px 10px;">
-                  <button type="button" id="recipient_submit" name="recipient_submit" class="btn btn-success">Done</button>
+                  <button type="button" id="recipient_button" name="recipient_button" class="btn btn-success">Done</button>
                </div> 
                </form>  
             </div>

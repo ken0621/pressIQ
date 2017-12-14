f15f94c637768c5174a7f21defe3066c16f9c5bb
arcylen gutierrez <arcylen103095@gmail.com>
2017-12-14 10:25:32 +0800

    Arc

---
+++ app/Http/Controllers/Member/TransactionEstimateQuotationController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Member;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+use App\Globals\Cart2;
+use App\Globals\WarehouseTransfer;
+use App\Globals\Warehouse2;
+use App\Globals\Item;
+use App\Globals\Transaction;
+use App\Models\Tbl_warehouse_issuance_report;
+
+use Session;
+use Carbon\Carbon;
+use App\Globals\Pdf_global;
+
+class TransactionEstimateQuotationController extends Member
+{
+  
+}
--- routes/routes_config/routes_transaction.php
+++ routes/routes_config/routes_transaction.php
@@ -4,4 +4,20 @@ Route::group(array('prefix' => '/member/transaction'), function()
 
 
 
+
+
+	/* <-- CUSTOMER TRANSACTION --> */
+
+	/* Estimate and Quotation */
+	AdvancedRoute::controller('/estimate_quotation', 'Member\TransactionEstimateQuotationController');
+	/* Sales Order */
+	AdvancedRoute::controller('/sales_order', 'Member\TransactionSalesOrderController');
+	/* Sales Invoice */
+	AdvancedRoute::controller('/sales_invoice', 'Member\TransactionSalesInvoiceController');
+	/* Sales Receipt */
+	AdvancedRoute::controller('/sales_receipt', 'Member\TransactionSalesReceiptController');
+	/* Receive Payment */
+	AdvancedRoute::controller('/receive_payment', 'Member\TransactionReceivePaymentController');
+	/* Credit Memo */
+	AdvancedRoute::controller('/credit_memo', 'Member\TransactionCreditMemoController');
 });

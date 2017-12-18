cf2c7fef35544bc201cc79b656fde260db3c2212
BerlyOmpoc <ompoc.kimberly@gmail.com>
2017-12-18 16:42:16 +0800

    done

--- public/themes/pressrelease/views/press_user/press_user_pressrelease.blade.php
+++ public/themes/pressrelease/views/press_user/press_user_pressrelease.blade.php
@@ -16,7 +16,6 @@
 
               <form class="recipient_form" onsubmit="add_event_global_submit()" action="/pressuser/choose_recipient" method="POST" style="">
                 {{csrf_field()}}
-                @if()
                 <div id="create_release" class="tabcontent create-release-container">
                   <div class="title-container">New Release</div>
                   <div class="title">Headline:</div>

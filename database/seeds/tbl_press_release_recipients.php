77650a737f8ec55c4c73895a5fcd5bd040706aec
Oliver Desu <oliver@oliver.ver>
2017-12-29 17:41:23 +0800

    done

--- database/seeds/tbl_press_release_recipients.php
+++ database/seeds/tbl_press_release_recipients.php
@@ -13,7 +13,7 @@ class tbl_press_release_recipients extends Seeder
     {
         DB::table('tbl_press_release_recipients')->truncate();
         $statement = "INSERT INTO `tbl_press_release_recipients` (`recipient_id`,`media_type`) VALUES 
-        	(1, 'BDO'),
+        	(1, 'Magazine'),
         	(2, 'Newspaper');
             (3, 'Telegrama');
             (4, 'News Agency');

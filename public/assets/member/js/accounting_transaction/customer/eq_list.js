fd94485edbf6239fbca0ceabc9ff8d103bdc7619
arcylen gutierrez <arcylen103095@gmail.com>
2018-01-06 17:54:57 +0800

    arcy save

---
+++ public/assets/member/js/accounting_transaction/customer/eq_list.js
@@ -0,0 +1,81 @@
+var eq_list = new eq_list();
+var load_table_data = {};
+
+function eq_list()
+{
+	init();
+
+	function init()
+	{
+		$(document).ready(function()
+		{
+			document_ready();
+		});
+	}
+
+	function document_ready()
+	{
+		action_load_table();
+		add_event_pagination();
+		event_load_search();
+	}
+	function event_load_search()
+	{
+		$('.search-keyword').unbind("change");
+		$('.search-keyword').bind("change", function(e)
+		{
+			action_filter_search(e.currentTarget);
+		});
+	}
+	function action_filter_search(self)
+	{
+		var search = $(self).val();
+
+		load_table_data.search_keyword = search;
+	    load_table_data.page = 1;
+	    action_load_table();
+	}
+	function add_event_pagination()
+	{
+		$("body").on("click", ".pagination a", function(e)
+		{
+			$url = $(e.currentTarget).attr("href");
+			var url = new URL($url);
+			$page = url.searchParams.get("page");
+			load_table_data.page = $page;
+			action_load_table();
+			return false;
+		});
+	}
+	function action_load_table()
+	{
+		if($(".load-item-table").text() == "")
+		{
+			$(".load-item-table").html(get_loader_html(100));
+		}
+		else
+		{
+			$(".load-item-table").css("opacity", 0.3);
+		}
+		$.ajax(
+		{
+			url:"/member/transaction/estimate_quotation/load-estimate-quotation",
+			data: load_table_data,
+			type: "get",
+			success: function(data)
+			{
+				$(".load-item-table").html(data);
+				$(".load-item-table").css("opacity", 1);
+			}
+		});
+	}
+	function get_loader_html($padding = 50)
+	{
+		return '<div style="padding: ' + $padding + 'px; font-size: 20px;" class="text-center"><i class="fa fa-spinner fa-pulse fa-fw"></i></div>';
+	}
+
+	this.action_load_table = function()
+	{
+		action_load_table();
+	}
+}

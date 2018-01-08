66b0bbccd223918b8ec15417fe51fcd1664c3215
Oliver Desu <oliver@oliver.ver>
2018-01-08 10:08:21 +0800

    Done

--- public/themes/pressrelease/views/search_news_room.blade.php
+++ public/themes/pressrelease/views/search_news_room.blade.php
@@ -15,10 +15,12 @@
             <div class="title"><a href="/newsroom/view/{{$prs->pr_id}}">{{$prs->pr_headline}}</a></div>
         </div>  
         <div class="details-container">
+            <a href="/newsroom/view/{{$prs->pr_id}}">
             <p>{!!$prs->pr_content!!}</p>
+            </a>
         </div>
         <div class="button-container">
-            <button onclick="window.location.href='/newsroom/view/{{$prs->pr_id}}'">Read More</button>
+          <button onclick="window.location.href='/newsroom/view/{{$prs->pr_id}}'">Read More</button>
         </div>
         @endforeach
     </div>

a97cc27d26aeee31290beba3c2bbb797bbfc6fb8
carlosegovia <carlosegovia5@gmail.com>
2018-01-09 16:24:06 -0800

    press new tab

--- public/themes/pressrelease/views/press_admin/admin.blade.php
+++ public/themes/pressrelease/views/press_admin/admin.blade.php
@@ -3,14 +3,14 @@
 <div class="content">
 <div class="background-container" style="background-image: url('/themes/{{ $shop_theme }}/img/home-banner.jpg')">
 	<div class="container">
-		<div class="row clearfix row-no-padding">
+		{{-- <div class="row clearfix row-no-padding">
 			<div class="col-md-12">
 				<div class="press-member">
 					<ul class="nav nav-tabs">
 					   <li class="{{ Request::segment(2) == "dashboard" ? "active" : "" }}"><a href="/pressadmin/dashboard">Dashboard</a></li>
-					   <li class="{{ Request::segment(2) == "mediacontacts" ? "active" : "" }}"><a href="/pressadmin/mediacontacts">Media Contacts</a></li><!-- Add link -->
-					  <!--  <li class="{{ Request::segment(2) == "pressreleases" ? "active" : "" }}"><a href="/pressadmin/pressreleases">Press Releases</a></li> --><!-- add link -->
-					   <li class="{{ Request::segment(2) == "email" ? "active" : "" }}"><a href="/pressadmin/email">Email</a></li><!-- add link -->
+					   <li class="{{ Request::segment(2) == "mediacontacts" ? "active" : "" }}"><a href="/pressadmin/mediacontacts">Media Contacts</a></li>Add link
+					   <li class="{{ Request::segment(2) == "pressreleases" ? "active" : "" }}"><a href="/pressadmin/pressreleases">Press Releases</a></li>
+					   <li class="{{ Request::segment(2) == "email" ? "active" : "" }}"><a href="/pressadmin/email">Email</a></li>
 					</ul>
 
 					<div class="tab-content">
@@ -18,6 +18,16 @@
 					</div>
 				</div>
 			</div>
+		</div> --}}
+		<div id='cssmenu'>
+    		<ul>
+    		   <li class="{{ Request::segment(2) == "dashboard" ? "active" : "" }}"><a href="/pressadmin/dashboard">Dashboard</a></li>
+			   <li class="{{ Request::segment(2) == "mediacontacts" ? "active" : "" }}"><a href="/pressadmin/mediacontacts">Media Contacts</a></li>
+			   <li class="{{ Request::segment(2) == "email" ? "active" : "" }}"><a href="/pressadmin/email">Email</a></li>
+			</ul>
+		</div>
+		<div class="tab-content">
+			@yield("pressview")
 		</div>
 	</div>
 </div>

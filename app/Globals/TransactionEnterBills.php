b1fefb7bdee48f82c71569bf6a57f0da821859c4
arcylen gutierrez <arcylen103095@gmail.com>
2018-01-31 15:02:15 +0800

    Arc

--- app/Globals/TransactionEnterBills.php
+++ app/Globals/TransactionEnterBills.php
@@ -76,7 +76,7 @@ class TransactionEnterBills
 
         return $data;
     }
-	public static function postInsert($ri_id, $shop_id, $insert, $insert_item, $insert_acct)
+	public static function postInsert($ri_id, $shop_id, $insert, $insert_item, $insert_acct = '')
 	{
     	$val = AccountingTransaction::vendorValidation($insert, $insert_item, 'enter_bills');
     
@@ -200,12 +200,15 @@ class TransactionEnterBills
     public static function insertLine($enter_bills_id, $insert_item, $entry, $insert_acct)
     {
         $acct_line = null;
-        foreach ($insert_acct as $key_acct => $value_acct)
+        if(count($insert_acct) > 0)
         {
-            $acct_line[$key_acct]['accline_bill_id']     = $enter_bills_id;
-            $acct_line[$key_acct]['accline_coa_id']      = $value_acct['account_id'];
-            $acct_line[$key_acct]['accline_description'] = $value_acct['account_desc'];
-            $acct_line[$key_acct]['accline_amount']      = $value_acct['account_amount'];
+            foreach ($insert_acct as $key_acct => $value_acct)
+            {
+                $acct_line[$key_acct]['accline_bill_id']     = $enter_bills_id;
+                $acct_line[$key_acct]['accline_coa_id']      = $value_acct['account_id'];
+                $acct_line[$key_acct]['accline_description'] = $value_acct['account_desc'];
+                $acct_line[$key_acct]['accline_amount']      = $value_acct['account_amount'];
+            }
         }
         
         $itemline = null;

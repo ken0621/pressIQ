e62573ca16e85c709bf2967eab1e31417789492e
carlosegovia <carlosegovia5@gmail.com>
2018-02-01 15:07:34 -0800

    press manage user

--- public/themes/pressrelease/views/press_user/press_user_manage_user.blade.php
+++ public/themes/pressrelease/views/press_user/press_user_manage_user.blade.php
@@ -2,54 +2,40 @@
 @section("pressview")
 <div class="background-container">
     <div class="pressview">
-       <div class="drafts-holder-container">
+       <div class="manage-user-container">
         @if (Session::has('success_update_user'))
               <div class="alert alert-success">
                  <center>{{ Session::get('success_update_user') }}</center>
               </div>
             @endif  
          <div class="title-container">User Details</div>
-            <div class="title-container">
-
 
             <form method="post" enctype="multipart/form-data">
               {{csrf_field()}}
-              <div class="col-md-2">First Name:</div>
-              <div class="col-md-4">
+              <div class="title">First Name:</div>
               <input type="text" id="user_first_name" name="user_first_name" class="form-control" value="{{$manage_user->user_first_name}}" >
-              </div>
               
-              <div class="col-md-2">Last Name:</div>
-              <div class="col-md-4">
+              <div class="title">Last Name:</div>
               <input type="text" id="user_last_name" name="user_last_name" class="form-control"  value="{{$manage_user->user_last_name}}" >
-              </div>
 
-              <div class="col-md-2">Email:</div>
-              <div class="col-md-4">
+              <div class="title">Email:</div>  
               <input type="text" id="user_email" name="user_email" class="form-control"  value="{{$manage_user->user_email}}" readonly>
-              </div>
 
-              <div class="col-md-2">Company Name:</div>
-              <div class="col-md-4">
+              <div class="title">Company Name:</div>
               <input type="text" id="user_company_name" name="user_company_name" class="form-control"  value="{{$manage_user->user_company_name}}" >
-              </div>
 
-              <div class="col-md-2">New Password:</div>
-              <div class="col-md-4">
-                 <input type="Password" id="user_password" name="user_password" class="form-control"  >
-              </div>
+              <div class="title">New Password:</div>
+              <input type="Password" id="user_password" name="user_password" class="form-control"  >
 
-              <div class="col-md-2">Confirm Password:</div>
-              <div class="col-md-4">
-                 <input type="Password" id="user_password_confirmation" name="user_password_confirmation" class="form-control"  v>
-              </div>
-              <div class="col-md-4">
-                  <input type="file" name="user_company_image" id="user_company_image" accept=".png, .jpg, .jpeg">
-              </div>
+              <div class="title">Confirm Password:</div>
+              <input type="Password" id="user_password_confirmation" name="user_password_confirmation" class="form-control"  v>
+              
+              <input type="file" name="user_company_image" id="user_company_image" accept=".png, .jpg, .jpeg" style="margin-top: 10px; background-color: inherit;">
 
-              <div class="col-md-8">
-                    <button type="Submit" formaction="/pressuser/manage_user/update">Update</button>
+              <div class="button-container">
+                <button type="Submit" formaction="/pressuser/manage_user/update">Update</button>
               </div>
+            
             </form>
 
               @if(session()->has('message'))
@@ -59,9 +45,6 @@
               </span>
               </div>
               @endif
-
-
-            </div>
         </div>
     </div>
 </div>
@@ -75,7 +58,7 @@
 @endsection
 
 @section("css")
-<link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/press_user_drafts.css">
+<link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/pressuser_manage_user.css">
 @endsection
 
 @section("script")

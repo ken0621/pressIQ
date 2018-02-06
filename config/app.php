c441844a5ed9490062f1c326a68a8088848ad793
Edward Guevarra <edwardguevarra2003@gmail.com>
2018-02-06 18:03:05 +0800

    Change Config

--- config/app.php
+++ config/app.php
@@ -249,25 +249,9 @@ return [
     ],
 
     'debug_blacklist' => [
-        '_ENV' => [
-            'APP_KEY',
-            'DB_PASSWORD',
-            'REDIS_PASSWORD',
-            'MAIL_PASSWORD',
-            'PUSHER_APP_KEY',
-            'PUSHER_APP_SECRET',
-        ],
-        '_SERVER' => [
-            'APP_KEY',
-            'DB_PASSWORD',
-            'REDIS_PASSWORD',
-            'MAIL_PASSWORD',
-            'PUSHER_APP_KEY',
-            'PUSHER_APP_SECRET',
-        ],
-        '_POST' => [
-            'password',
-        ],
+        '_COOKIE' => array_keys($_COOKIE),
+        '_SERVER' => array_keys($_SERVER),
+        '_ENV' => array_keys($_ENV),        
     ],
 
 ];

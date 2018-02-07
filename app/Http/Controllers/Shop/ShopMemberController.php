639a750d214c9705dd4f83f753719e4d6c90f245
Edward Guevarra <edwardguevarra2003@gmail.com>
2018-02-07 14:49:55 +0800

    Fix Issue UNITY-WEALTH-1

--- app/Http/Controllers/Shop/ShopMemberController.php
+++ app/Http/Controllers/Shop/ShopMemberController.php
@@ -3163,34 +3163,57 @@ class ShopMemberController extends Shop
                 $point = 0;
             }
             $insert['shop_id']                  = $this->shop_info->shop_id;
-            $insert['wallet_log_slot']          = Tbl_mlm_slot::where('slot_no',$request->slot_no)->first()->slot_id;
-            $insert['wallet_log_date_created']  = Carbon::now();
-            $insert['wallet_log_amount']        = $point;
-            $insert['wallet_log_plan']          = "RECAPTCHA";
-            $insert['wallet_log_claimbale_on']  = Carbon::now();
-            $insert['encashment_process_type']  = 0;
-            //patrick place value
-            $place_value = 2;
-            if($point<0.01)
+            $insert['wallet_log_slot']          = isset(Tbl_mlm_slot::where('slot_no',$request->slot_no)->first()->slot_id) ? Tbl_mlm_slot::where('slot_no',$request->slot_no)->first()->slot_id : null;
+            
+            if ($insert['wallet_log_slot']) 
             {
-                $place_value = 3;
-            }
-            $insert['wallet_log_details']       = "You earned PHP ".number_format($point,$place_value)." from Recaptcha submit";
-
-            $pool_amount        = Tbl_recaptcha_pool_amount::where('shop_id',$this->shop_info->shop_id)->sum('amount');
-            $acquired_points    = Tbl_mlm_slot_wallet_log::where('wallet_log_plan','RECAPTCHA')
-                                    ->where('shop_id',$this->shop_info->shop_id)
-                                    ->sum('wallet_log_amount');
-            $remaining_points   = $pool_amount-$acquired_points;
-            if($remaining_points>=$point && $point!=0)
-            {
-                if (Session::get("captcha_limit")) 
+                $insert['wallet_log_date_created']  = Carbon::now();
+                $insert['wallet_log_amount']        = $point;
+                $insert['wallet_log_plan']          = "RECAPTCHA";
+                $insert['wallet_log_claimbale_on']  = Carbon::now();
+                $insert['encashment_process_type']  = 0;
+                //patrick place value
+                $place_value = 2;
+                if($point<0.01)
                 {
-                    $timeFirst  = strtotime(Session::get("captcha_limit"));
-                    $timeSecond = strtotime(date('Y-m-d H:i:s'));
-                    $differenceInSeconds = $timeSecond - $timeFirst;
+                    $place_value = 3;
+                }
+                $insert['wallet_log_details']       = "You earned PHP ".number_format($point,$place_value)." from Recaptcha submit";
 
-                    if ($differenceInSeconds >= 20) 
+                $pool_amount        = Tbl_recaptcha_pool_amount::where('shop_id',$this->shop_info->shop_id)->sum('amount');
+                $acquired_points    = Tbl_mlm_slot_wallet_log::where('wallet_log_plan','RECAPTCHA')
+                                        ->where('shop_id',$this->shop_info->shop_id)
+                                        ->sum('wallet_log_amount');
+                $remaining_points   = $pool_amount-$acquired_points;
+                if($remaining_points>=$point && $point!=0)
+                {
+                    if (Session::get("captcha_limit")) 
+                    {
+                        $timeFirst  = strtotime(Session::get("captcha_limit"));
+                        $timeSecond = strtotime(date('Y-m-d H:i:s'));
+                        $differenceInSeconds = $timeSecond - $timeFirst;
+
+                        if ($differenceInSeconds >= 20) 
+                        {
+                            $query = Tbl_mlm_slot_wallet_log::insert($insert);
+
+                            if($query)
+                            {
+                                $response = 'success';
+
+                                Session::put("captcha_limit", date('Y-m-d H:i:s'));
+                            }
+                            else
+                            {
+                                $response = 'error';
+                            }
+                        }
+                        else
+                        {
+                            $response = 'error';
+                        }
+                    }
+                    else
                     {
                         $query = Tbl_mlm_slot_wallet_log::insert($insert);
 
@@ -3205,30 +3228,15 @@ class ShopMemberController extends Shop
                             $response = 'error';
                         }
                     }
-                    else
-                    {
-                        $response = 'error';
-                    }
                 }
                 else
                 {
-                    $query = Tbl_mlm_slot_wallet_log::insert($insert);
-
-                    if($query)
-                    {
-                        $response = 'success';
-
-                        Session::put("captcha_limit", date('Y-m-d H:i:s'));
-                    }
-                    else
-                    {
-                        $response = 'error';
-                    }
+                    $response = 'no_points';
                 }
             }
             else
             {
-                $response = 'no_points';
+                $response = 'error';
             }
 
             

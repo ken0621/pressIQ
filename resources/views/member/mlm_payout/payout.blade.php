c220bb79c006313e597883eed91e40fde4297da9
arcylen gutierrez <arcylen103095@gmail.com>
2018-02-08 13:13:01 +0800

    fix reject

--- resources/views/member/mlm_payout/payout.blade.php
+++ resources/views/member/mlm_payout/payout.blade.php
@@ -27,8 +27,8 @@
     <ul class="nav nav-tabs">
         <li class="active change-tab pending-tab cursor-pointer" mode="pending"><a class="cursor-pointer"><i class="fa fa-money"></i> Pending</a></li>
         <li class="cursor-pointer change-tab approve-tab" mode="done"><a class="cursor-pointer"><i class="fa fa-check"></i> Processed</a></li>
-        {{-- @if($shop_id == 55 && $count_reject == 0) --}}
-        @if($shop_id == 55)
+        {{-- @if($shop_id == 90 && $count_reject == 0) --}}
+        @if($shop_id == 90)
         <li class="pull-right">
             <button class="btn btn-primary popup" link="/member/mlm/payout/reject-encashment" size="md" style="margin: 5px;">REJECT ENCASHMENT</button>
         </li>

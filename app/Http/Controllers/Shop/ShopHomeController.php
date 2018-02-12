a54541cbb8f8b54d7471a0f8b2f1da0995ca47b8
Rommel Cuneta <cunetarommel0620@gmail.com>
2018-02-12 11:09:00 +0800

    kolorete

--- app/Http/Controllers/Shop/ShopHomeController.php
+++ app/Http/Controllers/Shop/ShopHomeController.php
@@ -25,8 +25,11 @@ class ShopHomeController extends Shop
         $data["page"]      = "Home";
         $data["_product"] = Ecom_Product::getAllProduct($this->shop_info->shop_id);
 
-        //home
-        $data["_category"] = Ecom_Product::getAllCategory($this->shop_info->shop_id);
+        //display categories in home
+        if ($this->shop_info->shop_theme == "kolorete") 
+        {
+            $data["_category"] = Ecom_Product::getAllCategory($this->shop_info->shop_id);
+        }
 
         /* Intogadgets Exclusive */
         if ($this->shop_info->shop_theme == "intogadgets") 

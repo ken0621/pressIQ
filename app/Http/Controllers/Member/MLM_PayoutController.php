7bf5129e354a2c78af8e33946d3e1a774a237c43
arcylen gutierrez <arcylen103095@gmail.com>
2018-02-15 11:50:49 +0800

    arc reject payment

--- app/Http/Controllers/Member/MLM_PayoutController.php
+++ app/Http/Controllers/Member/MLM_PayoutController.php
@@ -409,7 +409,7 @@ class MLM_PayoutController extends Member
 	{
 		return view("member.mlm_payout.payout_reject");
 	}
-	public function postRejectEncashmentsubmit()
+	public function anyRejectEncashmentsubmit()
 	{
 		$amount = Request::input('less_than_amount');
 		$encashment_type = Request::input('encashment_type');

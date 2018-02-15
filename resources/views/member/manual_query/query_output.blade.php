5a7cfa5132a75bda47404c98970dad3dd4208bde
arcylen gutierrez <arcylen103095@gmail.com>
2018-02-15 13:39:53 +0800

    Arc

--- resources/views/member/manual_query/query_output.blade.php
+++ resources/views/member/manual_query/query_output.blade.php
@@ -28,12 +28,12 @@
 			    				<td class="text-center">{{$payout->slot_no}}</td>
 					            <td class="text-center">{{$payout->first_name}} {{$payout->last_name}}</td>
 				                <td class="text-center">{{$payout->slot_eon}}</td>
-				                <td class="text-center">{{$payout->slot_eon_account_no}}</td>
-				                <td class="text-center">{{$payout->slot_eon_card_no}}</td>
+				                <td class="text-center">*{{$payout->slot_eon_account_no}}*</td>
+				                <td class="text-center">*{{$payout->slot_eon_card_no}}*</td>
 				                <td class="text-center">{{$payout->payout_bank_name}}</td>
 				                <td class="text-center">{{$payout->bank_account_name}}</td>
 				                <td class="text-center">{{$payout->bank_account_type}}</td>
-				                <td class="text-center">{{$payout->bank_account_number}}</td>
+				                <td class="text-center">*{{$payout->bank_account_number}}*</td>
 			    				<td class="text-center">{{currency('',$payout->ez_bonus)}}</td>
 			    				<td class="text-center">{{currency('',$payout->total_payout)}}</td>
 			    				<td class="text-center">{{currency('',$payout->payout)}}</td>

34cc2723c30ef0cdcc2039e2fe3ae23a6907de06
Rommel Cuneta <cunetarommel0620@gmail.com>
2018-02-20 15:14:09 +0800

    bootstrap 4 for new clients

---
+++ resources/views/frontend/gfootv2.blade.php
@@ -0,0 +1,58 @@
+<!--MODAL-->
+<div id="global_modal" class="modal fade" role="dialog" >
+    <div class="modal-dialog">
+        <!-- Modal content-->
+        <div class="modal-content modal-content-global clearfix">
+        </div>
+    </div>
+</div>
+<div class="multiple_global_modal_container"></div>
+
+<!--JQUERY-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+
+<!-- BOOTSTRAP 4-->
+<script src="/assets/bootstrap-4/dist/js/bootstrap.min.js"></script>
+
+<!--EXTERNAL-->
+<script type="text/javascript" src="/assets/front/external_js/match-height.js"></script>
+<script type="text/javascript" src="/assets/front/external_js/fit-text.js"></script>
+<script type="text/javascript" src="/assets/front/external_js/figuesslider.js"></script>
+<script type="text/javascript" src="/assets/front/external_js/jquery.keep-ratio.min.js"></script>
+<script type="text/javascript" src="/assets/front/external_js/footable.js"></script>
+<script type="text/javascript" src="/assets/member/plugin/toaster/toastr.min.js"></script>
+
+<!--SLICK-->
+<script type="text/javascript" src="/assets/slick/slick.min.js"></script>
+
+<!--LIGHTBOX-->
+<script type="text/javascript" src="/assets/lightbox/js/lightbox.js"></script>
+
+<!-- LIGHTSPEEDBOX -->
+<script type="text/javascript" src="/assets/lightspeedbox/lsb.min.js"></script>
+
+<!--EASING-->
+<script type="text/javascript" src="/assets/easing/jquery.easing.min.js"></script>
+
+<!--WOW-->
+<script type="text/javascript" src="/assets/wow/js/wow.min.js"></script>
+<script>
+      new WOW().init();
+</script>
+
+<!--GLOBALS-->
+<script type="text/javascript" src="/assets/front/js/global_function.js"></script>
+<script type="text/javascript" src="/assets/front/js/globalv2.js"></script>
+<script type="text/javascript" src="/assets/front/js/global_cart.js"></script>
+
+<script type="text/javascript">
+$(document).on('show.bs.modal', '.modal', function () 
+{
+    var zIndex = 1040 + (10 * $('.modal:visible').length);
+    $(this).css('z-index', zIndex);
+    setTimeout(function() {
+        $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+    }, 0);
+});
+</script>
---
+++ resources/views/frontend/gheadv2.blade.php
@@ -0,0 +1,41 @@
+<!-- BOOTSTRAP -->
+<link rel="stylesheet" href="/assets/bootstrap-4/dist/css/bootstrap.min.css">
+
+<!--MODERNIZR-->
+<script src="/themes/{{ $shop_theme }}/assets/initializr/js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+
+<!-- FONT AWESOME 5-->
+<link rel="stylesheet" type="text/css" href="/assets/font-awesome/css/font-awesome.min.css">
+<script defer src="/assets/font-awesome-5/svg-with-js/css/fa-svg-with-js.css"></script>
+<script defer src="/assets/font-awesome-5/svg-with-js/js/fontawesome-all.js"></script>
+
+<!-- SLICK CSS -->
+<link rel="stylesheet" type="text/css" href="/assets/slick/slick.css">
+<link rel="stylesheet" type="text/css" href="/assets/slick/slick-theme.css">
+
+<!-- LOADER CSS -->
+<link rel="stylesheet" type="text/css" href="/assets/front/css/loader.css">
+
+<!--JQUERY UI-->
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+<!-- PARALLAX -->
+<link rel="stylesheet" type="text/css" href="/assets/front/external_css/parallax.css">
+
+<!-- LIGHTBOX -->
+<link rel="stylesheet" type="text/css" href="/assets/lightbox/css/lightbox.css">
+
+<!-- LIGHTSPEEDBOX -->
+<link rel="stylesheet" type="text/css" href="/assets/lightspeedbox/lsb.css">
+
+<!-- WOW JS -->
+<link rel="stylesheet" type="text/css" href="/assets/wow/css/animate.css">
+
+<!--TOASTER-->
+<link rel="stylesheet" type="text/css" href="/assets/member/plugin/toaster/toastr.css">
+
+<!--FOOTABLE-->
+<link rel="stylesheet" type="text/css" href="/assets/front/external_css/footable.bootstrap.css">
+
+<!-- THEME COLOR -->
+<link href="/themes/{{ $shop_theme }}/css/{{ $shop_theme_color }}.css" rel="stylesheet" type="text/css">

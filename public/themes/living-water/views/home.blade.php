325d610df918b2714d4db60e0e3d16273fc444f7
carlosegovia <carlosegovia5@gmail.com>
2018-03-15 09:53:42 -0800

    living search

--- public/themes/living-water/views/home.blade.php
+++ public/themes/living-water/views/home.blade.php
@@ -150,24 +150,6 @@
                         <div class="per-album-container">
                             <div class="img-container">
                                 <a href="/product/view2/{{ $product['eprod_id'] }}">
-                                    <img src="/themes/{{ $shop_theme }}/img/alkaline.jpg">
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="per-album-container">
-                            <div class="img-container">
-                                <a href="/product/view2/{{ $product['eprod_id'] }}">
-                                    <img src="/themes/{{ $shop_theme }}/img/APC.jpg">
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="per-album-container">
-                            <div class="img-container">
-                                <a href="/product/view2/{{ $product['eprod_id'] }}">
                                     <img src="/themes/{{ $shop_theme }}/img/bottled-water.jpg">
                                 </a>
                             </div>
--- public/themes/living-water/views/layout.blade.php
+++ public/themes/living-water/views/layout.blade.php
@@ -55,11 +55,12 @@
                 <span>{!! get_content($shop_theme_info, "contact_details", "contact_company_email_address") !!}</span>
             </div>
             <div class="right-container">
-                <span>
-                    <input class="form-control" placeholder="Search">
-                </span>
-                <i class="fa fa-search" aria-hidden="true"></i>
-                
+                <form action="/product" method="get" id="form-search">
+                    <span>
+                        <input onkeydown="javascript: if(event.keyCode === 13) onSearch();" type="text" name="search" id="keyword1" class="form-control" placeholder="Search">
+                    </span>
+                    <a href="" onclick="onSearch();" id="submit_link" style="color: #fff;"><i class="fa fa-search" aria-hidden="true"></i></a>
+                </form>
             </div>
         </div>
     </div>

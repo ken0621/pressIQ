ad721f7a42626cb5dd4bca5b6499c46ffce5b712
Mark Figues <markcarlofigues.digima@gmail.com>
2018-04-02 17:01:21 +0800

    living water fixed

--- public/themes/living-water/views/layout.blade.php
+++ public/themes/living-water/views/layout.blade.php
@@ -18,7 +18,7 @@
         <link href="https://fonts.googleapis.com/css?family=PT+Sans:400,700" rel="stylesheet">   
 
         <!-- GLOBAL CSS -->
-        <link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/global.css">
+        <link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/global.css?version=1.2">
 
         @include("frontend.ghead")
 

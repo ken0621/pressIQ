e544f2ca2612ed50d9c8ae14b4371744812dcc10
Rommel Cuneta <cunetarommel0620@gmail.com>
2018-04-02 17:14:22 +0800

    mel

--- public/themes/tf-wellness/views/product_content.blade.php
+++ public/themes/tf-wellness/views/product_content.blade.php
@@ -1,5 +1,21 @@
 @extends("layout")
 @section("content")
+<style type="text/css">
+.prod-image-thumb-container
+{
+	width: 100%;
+}
+
+.prod-image-thumb-container .holder
+{
+	
+}
+
+.slick-slide img
+{
+	object-fit: cover !important;
+}
+</style>
 <div class="content">
 	<div class="main-container">
 		<div class="container">
@@ -37,89 +53,97 @@
 	</div>
 	<div class="top-1-container">
 		<div class="container">
-			<div class="prod-content-container row clearfix">
-				<div class="col-md-6">
-					<!-- PRODUCT IMAGE -->
-					<div class="prod-image-container">
-						<img class="single-product-img" src="{{ get_product_first_image($product) }}">
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div class="purchase-details-container">
-						<div class="product-name-container">{{ get_product_first_name($product) }}{{-- <div class="line-bot"></div> --}}</div>
-						<!-- PRODUCT DESCRIPTION -->
-						<div class="prod-description-container">
-							<div class="description-title">
-								Description
-							</div>
-							<div class="prod-description">
-								<p>
-									{{-- {!! get_product_first_description($product) !!} --}}
-									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo aliquid itaque minima eveniet alias recusandae quos placeat quasi harum qui dolore pariatur ipsum excepturi ipsam inventore, soluta ab accusamus earum?
-								</p>
-							</div>
-							<div class="cat-title">
-								Category
-							</div>
-							<div class="cat-description">
-								<p>
-									{{-- {!! get_product_first_description($product) !!} --}}
-									Home Appliances
-								</p>
-							</div>
+			@foreach($product['variant'] as $key => $product_variant)
+				<div class="product-content prod-content-container row clearfix {{ $loop->first ? '' : 'hide' }}" variant-id="{{ $product_variant['evariant_id'] }}">
+					<div class="col-md-6">
+						<!-- PRODUCT IMAGE -->
+						<div class="prod-image-container">
+							@foreach($product_variant['image'] as $key => $image)
+							{{-- <img class="single-product-img" src="{{ get_product_first_image($product) }}"> --}}
+							<img key="{{ $key }}" variant-id="{{ $product_variant['evariant_id'] }}" class="single-product-img 4-3-ratio {{ $key == 0 ? '' : 'hide' }}" key="{{ $key }}" style="width: 100%;" src="{{ $image['image_path'] }}" data-zoom-image="{{ $image['image_path'] }}">
+							@endforeach
 						</div>
-						<div class="prod-price">
+						{{-- JUST NOW --}}
+						<div class="prod-image-thumb-container" style="margin-left: -7.5px; margin-right: -7.5px;">
+							@foreach($product_variant['image'] as $key => $image)
+                            <div class="holder" style="cursor: pointer; padding: 0 7.5px;" key="{{ $key }}" variant-id="{{ $product_variant['evariant_id'] }}"><img style="width: 100%; object-fit: cover;" class="item-image-small small-yellow-bag 4-3-ratio" src="{{ $image['image_path'] }}"></div>
+                            @endforeach
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="purchase-details-container">
+							<div class="product-name-container">{{ $product["eprod_name"] }}{{-- <div class="line-bot"></div> --}}</div>
+							<!-- PRODUCT DESCRIPTION -->
+							<div class="prod-description-container">
+								<div class="description-title">
+									Description
+								</div>
+								<div class="prod-description">
+									<p>
+										{{-- {!! get_product_first_description($product) !!} --}}
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo aliquid itaque minima eveniet alias recusandae quos placeat quasi harum qui dolore pariatur ipsum excepturi ipsam inventore, soluta ab accusamus earum?
+									</p>
+								</div>
+								<div class="cat-title">
+									Category
+								</div>
+								<div class="cat-description">
+									<p>
+										{{-- {!! get_product_first_description($product) !!} --}}
+										{{-- Home Appliances --}}
+										{!! $product_variant["evariant_description"] !!}
+									</p>
+								</div>
+							</div>
+							<div class="prod-price">
 								Price
 							</div>
-						<div class="product-price">
-							@if(isset($product["variant"][0]["price_level"]) && $product["variant"][0]["price_level"])
-								₱ {{ number_format($product["variant"][0]["price_level"], 2) }}
-							@else
-								{{ get_product_first_price($product) }}
-							@endif
-							{{-- <div style="margin-top: 15px; font-size: 18px;">
-								@if($mlm_member)
-									@foreach($product["variant"][0]["pv"] as $key => $value)
-										@if($key == "STAIRSTEP" ) --}}
-										{{-- || $key == "STAIRSTEP_GROUP" || $key == "RANK" --}}
-											{{-- <div>
-												<span class="pv-label" style="padding-left: 0;">
-													<i class="fa fa-star" aria-hidden="true"></i>
-												</span> --}}
-												{{-- @if($key == "RANK")
-													<span class="pv-label">RANK PV:</span>
-												@elseif($key == "STAIRSTEP_GROUP")
-													<span class="pv-label">GROUP VOLUME:</span> --}}
-												{{-- @if($key == "STAIRSTEP")
-													<span class="pv-label"></span>
-												@else
-													<span class="pv-label">{{ str_replace("_", " ", $key) }}</span>
-												@endif
-												<span class="pv-details">{{ number_format($value, 2) }} PV</span>
-											</div>
-										@endif
-									@endforeach
-								@endif
-							</div> --}}
-						</div>
-						<div class="product-quantity">
-							<div class="info-title">
-								Quantity
+							<div class="product-price">
+								@if($product_variant['discounted'] == "true")
+                                    <div class="price">&#8369; {{ number_format($product_variant['discounted_price'], 2) }}</div>
+                                    <div class="price" style="text-decoration: line-through; font-size: 14px;">&#8369; {{ number_format($product_variant['evariant_price'], 2) }}</div>
+                                @else
+                                	<div class="price">&#8369; {{ number_format($product_variant['evariant_price'], 2) }}</div>
+                                @endif
 							</div>
-							<input class="input-quantity" onChange="$(this).siblings('.product-add-cart').attr('quantity', $(this).val())" type="number" name="quantity" min="1" step="1" value="1">
-							<div class="add-to-cart-button product-add-cart" item-id="{{ $product['variant'][0]['evariant_item_id'] }}" quantity="1"><span><i class="fa fa-cart-plus" aria-hidden="true"></i></span>&nbsp;&nbsp;<span>ADD TO CART</span></div>
-						</div>
-						
-						<!-- <div class="share-product">
-							<div class="info-title">Share This Product</div>
-							<div class="share-button-container">
-								<div class="share-button"><span><i class="fa fa-facebook" aria-hidden="true"></i></span><span>&nbsp;Facebook</span></div>
-								<div class="share-button"><span><i class="fa fa-twitter" aria-hidden="true"></i></span><span>&nbsp;Twitter</span></div>
+
+							@if(isset($_variant) && $_variant && count($_variant) > 0)
+								<div class="variant-holder" style="margin-top: 15px;">
+	                                @foreach($_variant as $variant)
+	                                <div class="holder">
+	                                    <div class="s-label" style="color: #585858; font-size: 13px;font-weight: 500; margin-bottom: 5px;">Select {{ $variant['option_name'] }}</div>
+	                                    <div class="select">
+	                                        <select name="attr[{{ $variant['option_name_id'] }}]" style="text-transform: capitalize;" class="form-control select-variation" variant-label="{{ $variant['option_name'] }}" product-id="{{ $product['eprod_id'] }}" variant-id="{{ $product_variant['evariant_id'] }}">
+	                                            <option value="0" style="text-transform: capitalize;">{{ $variant['option_name'] }}</option>
+	                                            @foreach(explode(",", $variant['variant_value']) as $option)
+	                                            <option value="{{ $option }}" style="text-transform: capitalize;">{{ $option }}</option>
+	                                            @endforeach
+	                                        </select>
+	                                    </div>
+	                                </div>
+	                                @endforeach
+	                            </div>
+                            @endif
+
+							<div class="product-quantity">
+								<div class="info-title">
+									Quantity
+								</div>
+								<input class="input-quantity" onChange="$(this).siblings('.product-add-cart').attr('quantity', $(this).val())" type="number" name="quantity" min="1" step="1" value="1">
+								<div class="add-to-cart-button product-add-cart" item-id="{{ $product['variant'][0]['evariant_item_id'] }}" quantity="1"><span><i class="fa fa-cart-plus" aria-hidden="true"></i></span>&nbsp;&nbsp;<span>ADD TO CART</span></div>
 							</div>
-						</div> -->
+							
+							<!-- <div class="share-product">
+								<div class="info-title">Share This Product</div>
+								<div class="share-button-container">
+									<div class="share-button"><span><i class="fa fa-facebook" aria-hidden="true"></i></span><span>&nbsp;Facebook</span></div>
+									<div class="share-button"><span><i class="fa fa-twitter" aria-hidden="true"></i></span><span>&nbsp;Twitter</span></div>
+								</div>
+							</div> -->
+						</div>
 					</div>
 				</div>
-			</div>
+			@endforeach
 		</div>
 	</div>
 	<div class="top-2-container">
@@ -238,4 +262,12 @@ var product_quantity = ".input-quantity";
 var cart_holder = '.cart-dropdown';
 </script>
 <script type="text/javascript" src="/assets/front/js/global_addcart.js"></script>
+<script type="text/javascript" src="/themes/{{ $shop_theme }}/js/product_content.js?version=1"></script>
+<script type="text/javascript">
+$('.prod-image-thumb-container').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1
+});	
+</script>
 @endsection

714501842481c4ae839a593378744f41016a5078
Arcy <arcylen103095@gmail.com>
2018-06-04 14:43:11 +0800

    Aarc

--- app/Http/Controllers/Shop/ShopMemberController.php
+++ app/Http/Controllers/Shop/ShopMemberController.php
@@ -3154,12 +3154,12 @@ class ShopMemberController extends Shop
             Mlm_tree::insert_tree_placement($slot_info_e, $slot_info_e, 1);
 
             MLM2::entry($shop_id,$slot_id);
-            /** ARCY CONSUME PRODUCT CODES
-            $consume['name'] = '';
-            $consume['id'] = '';
-            Warehouse2::consume_product_codes($shop_id, $codes->mlm_pin, $codes->mlm_activation, $consume ,"","used"); **/
-            $used['item_in_use'] = 'used';
-            Tbl_warehouse_inventory_record_log::where('record_log_id',$codes->record_log_id)->update($used);
+         
+            $consume['name'] = 'created_slot';
+            $consume['id'] = $slot_id;
+            Warehouse2::consume_product_codes($shop_id, $codes->mlm_pin, $codes->mlm_activation, $consume ,"Created Slot #".$slot_id,"used");
+            // $used['item_in_use'] = 'used';
+            // Tbl_warehouse_inventory_record_log::where('record_log_id',$codes->record_log_id)->update($used);
             $response = 'success';
         }
 

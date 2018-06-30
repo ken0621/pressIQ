2548d253cde0dbf507cb6508b95b3cdac07a2b5a
Edward <your@example.com>
2018-06-30 13:27:43 +0800

    privilege_card

--- public/themes/philtech/views/member/privilage_card_holder_dashboard.blade.php
+++ public/themes/philtech/views/member/privilage_card_holder_dashboard.blade.php
@@ -44,10 +44,13 @@
 					@if(count($_point_plan) > 0)
 					<div class="chart-legend" style="min-height: 117px; max-height: auto;">
 						@foreach($_point_plan as $plan)
-						<div class="holder">
-							
-							<div class="name" style="width: 100%;"><div class="color pull-left middle-pos"></div><span>{{ $plan->label }}</span> <span class="p-text">{{ $points->{ "display_" . $plan->string_plan } }}</span></div>
-						</div>
+						@if($plan->label == "Repurchase Cashback")
+							<div class="holder">
+								
+								<div class="name" style="width: 100%;"><div class="color pull-left middle-pos"></div><span>{{ $plan->label }}</span> <span class="p-text">{{ $points->{ "display_" . $plan->string_plan } }}</span></div>
+							</div>
+						@endif
+
 						@endforeach
 					</div>
 					@else

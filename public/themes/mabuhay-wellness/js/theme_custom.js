5446b8818ddbd24595783b25f9ef15abf9acfcbf
carlosegovia <carlosegovia5@gmail.com>
2018-08-13 17:13:25 -0800

    mabuhay wellness

---
+++ public/themes/mabuhay-wellness/js/theme_custom.js
@@ -0,0 +1,274 @@
+var theme_custom = new theme_custom();
+
+function theme_custom()
+{
+	init();
+
+	function init()
+	{
+        window_load();
+		$(document).ready(function()
+		{
+			document_ready();
+		});   
+	}
+    
+    function window_load()
+    {
+        $(window).load(function()
+        {
+            $('.loader-container').fadeOut();
+        });
+    }
+
+	function document_ready()
+	{
+        product_mobile_dropdown();
+        company_mobile_dropdown();
+        close_side_nav();
+		action_facebook_widget();
+		event_header_fix();
+		event_action_click();
+        event_mobile_sidenav();
+        scroll_up();
+        match_height();
+	}
+
+    function match_height()
+    {
+        $(".match-height").matchHeight();
+    }
+
+    function scroll_up()
+    {
+        /*scroll up*/
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 700) {
+                $('.scroll-up').fadeIn();
+            } else {
+                $('.scroll-up').fadeOut();
+            }
+        });
+
+        $('.scroll-up').click(function () {
+            $("html, body").animate({
+                scrollTop: 0
+            }, 700);
+            return false;
+        });
+    }
+
+	function action_facebook_widget()
+	{
+		(function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10";
+        fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+	}
+
+	function event_header_fix()
+	{
+        $window = $(window);
+        $window.scroll(function()
+        {
+            $scroll_position = $window.scrollTop();
+            if ($scroll_position > 32.2167)
+            {
+                $('.header-container').addClass('header-fixed');
+                $('.subheader-container').addClass('header-fixed');
+
+                header_height = $('.your-header').innerHeight();
+                $('body').css('padding-top', header_height);
+            }
+            else
+            {
+                $('body').css('padding-top', '0');
+                $('.header-container').removeClass('header-fixed');
+                $('.subheader-container').removeClass('header-fixed');
+            }
+        });
+	}
+
+	function event_action_click()
+	{
+        $('.slider3').diyslider(
+        {
+            width: "580px", // width of the slider
+            height: "120px", // height of the slider
+            display: 5, // number of slides you want it to display at once
+            loop: false // disable looping on slides
+        }); // this is all you need!
+
+        // use buttons to change slide
+        $('#gotoleft').bind("click", function()
+        {
+            // Go to the previous slide
+            $('.slider3').diyslider("move", "back");
+        });
+        $('#gotoright').unbind("click")
+        $('#gotoright').bind("click", function()
+        {
+            // Go to the previous slide
+            $('.slider3').diyslider("move", "forth");
+        });
+
+        /*PRODUCT HOVER TOGGLE*/
+
+        $('.product-hover').hover(function()
+            {
+                $('.product-dropdown').stop();
+                $('.product-dropdown').fadeIn(400);
+            },
+            function()
+            {
+                $('.product-dropdown').stop();
+                $('.product-dropdown').fadeOut(400);
+            });
+
+        $('.company-hover').hover(function()
+            {
+                $('.company-dropdown').stop();
+                $('.company-dropdown').fadeIn(400);
+            },
+            function()
+            {
+                $('.company-dropdown').stop();
+                $('.company-dropdown').fadeOut(400);
+            });
+
+        $('.cart-hover').hover(function()
+            {
+                $('.cart-dropdown').stop();
+                $('.cart-dropdown').fadeIn(400);
+            },
+            function()
+            {
+                $('.cart-dropdown').stop();
+                $('.cart-dropdown').fadeOut(400);
+            });
+
+
+        // NAVIRINO CLICK TOGGLE
+        $(".menu-nav").click(function()
+        {
+            $(".navirino").toggle("slow");
+        });
+
+
+        // COMPANY CLICK TOGGLE
+        $(".company-hover").click(function()
+        {
+            $(".minimize-cat-holder").toggle("slow");
+        });
+
+        // PRODUCT CLICK TOGGLE
+        $(".product-hover").click(function()
+        {
+            $(".minimize-product-holder").toggle("slow");
+        });
+	}
+
+    function event_mobile_sidenav()
+    {
+        $menuLeft = $('.pushmenu-left');
+        $nav_list = $('#nav_list');
+        
+        $nav_list.on("click", function() 
+        {
+            $(this).toggleClass('active');
+           /* $('.pushmenu-push').toggleClass('pushmenu-push-toright');*/
+            $menuLeft.toggleClass('pushmenu-open');
+        });
+    }
+
+    function close_side_nav()
+    {
+        /*$("body").click(function(e)
+        {
+            if(e.target.className == "pushmenu-open")
+            { 
+                alert("do't hide");  
+            }
+
+            else 
+            {
+                $(".pushmenu-open").hide();
+            }
+        });*/
+
+/*        $(document).mouseup(function(e) 
+        {
+            var container = $("pushmenu-push");
+
+            // if the target of the click isn't the container nor a descendant of the container
+            if (!container.is(e.target) && container.has(e.target).length === 0) 
+            {
+                container.hide();
+            }
+        });*/
+    }
+
+    function product_mobile_dropdown()
+    {
+        $(".product-mobile-dropdown").on("click", function (e) 
+        {
+            $(e.currentTarget).siblings(".product-mobile-dropdown-list").slideToggle();
+
+            // $(this).find($(".fa-change")).removeClass('fa-angle-down').addClass('fa-angle-up');
+
+            $(e.currentTarget).find(".fa-angle-down").toggleClass('fa-rotate-180');
+
+        });
+    }
+
+    function company_mobile_dropdown()
+    {
+        $(".company-mobile-dropdown").on("click", function (e) 
+        {
+            $(e.currentTarget).siblings(".company-mobile-dropdown-list").slideToggle();
+            
+            $(e.currentTarget).find(".fa-angle-down").toggleClass('fa-rotate-180');
+
+        });
+    }
+}
+
+/*JAVASCRIPT*/
+
+/*function on() 
+{
+    var $body = $(document.body);
+
+    document.getElementById("overlay").style.display = "block";
+    $body.css("overflow", "hidden");
+}
+
+function off()
+{
+    var $body = $(document.body);
+    document.getElementById("overlay").style.display = "none";
+    $('.pushmenu').removeClass("pushmenu-open");
+    $body.css("overflow", "auto");
+}*/
+
+function on() 
+{
+    document.getElementById("overlay").style.display = "block";
+    $("body").css({"overflow": "hidden","position": "fixed","margin": "0","padding": "0","right": "0","left": "0"});
+        $(".blur-me").css({
+        filter: 'blur(20px)'
+    });
+}
+
+function off()
+{
+    document.getElementById("overlay").style.display = "none";
+    $('.pushmenu').removeClass("pushmenu-open");
+    $("body").css({"overflow": "auto","position": "static"});
+        $(".blur-me").css({
+        filter: 'blur(0)'
+    });
+}
---
+++ public/themes/mabuhay-wellness/views/product.blade.php
@@ -0,0 +1,147 @@
+@extends("layout")
+@section("content")
+<div class="content">
+	<div class="main-container">
+		<div class="container">
+			<div class="hamburger-container">
+				<div class="mini-submenu">
+				    <span class="icon-bar"></span>
+				    <span class="icon-bar"></span>
+				    <span class="icon-bar"></span>
+				</div>
+				<ul id="menu">
+				    <div class="categories-header">
+				        <span class="title">CATEGORIES</span><i id="close-menu" class="fa fa-times" aria-hidden="true"></i>
+				    </div>
+				    <li><a href="#">Health Supplements</a></li>
+					<li><a href="#">Beverages</a></li>
+					<li><a href="#">Beauty Products</a></li>
+					<li><a href="#">Home Care Products</a></li>
+					<li><a href="#">Breakfast Foods</a></li>
+					<li><a href="#">RTWs</a></li>
+					<li><a href="#">Groceries</a></li>
+					<li><a href="#">Gadgets</a></li>
+					<li><a href="#">Home Appliances</a></li>
+					<li><a href="#">Beauty Salon</a></li>
+					<li><a href="#">Spa</a></li>
+					<li><a href="#">More</a></li>
+				</ul>
+				<div id="cat-title">Shop By Categories</div>
+			</div>
+		</div>
+	</div>
+	<div class="top-1-container">
+		<div class="container">
+			<div class="prod-container">
+				<div class="row clearfix">
+					<div class="product-list-holder col-md-12 col-sm-12 col-xs-12">
+						<div class="prod-list-container">
+							<div class="title-container">Featured Products<div class="line-bot"></div></div>
+							<div class="prod-list">
+								<div class="row-no-padding clearfix">
+									<!-- PER ITEM -->
+									{{-- @if(count($_product) > 0)
+										@foreach($_product as $product)
+										<div class="col-md-2">
+											<div class="product-holder">
+												<a class="item-hover" href="/product/view2/{{ $product['eprod_id'] }}" style="text-decoration: none;">
+													<div class="product-image">
+														<img src="{{ get_product_first_image($product) }}">
+													</div>
+												</a>
+											</div>
+										</div>
+										@endforeach
+									@else
+									   
+									@endif --}}
+									@if(count($_product) > 0)
+										@foreach($_product as $product)
+										<div class="col-md-2 prod-border">
+											@if($mlm_member)
+												<div class="prod-holder prod-holder-member">
+													<a href="/product/view2/{{ $product['eprod_id'] }}" style="text-decoration: none;">
+														<div class="prod-image">
+															<div class="prod-overlay">
+																<div class="border">
+																	<div class="top">
+																		<div class="label"><i class="fa fa-tag"></i> Member’s Price</div>
+																		<div class="price">PHP 1,800.00</div>
+																	</div>
+																	<div class="bottom">
+																		<div class="label"><i class="fa fa-star"></i> Point Value</div>
+																		<div class="point">2.0</div>
+																	</div>
+																</div>
+															</div>
+															<img class="img-hover" src="{{ get_product_first_image($product) }}">
+														</div>
+														<div class="details-container">
+															<div class="prod-type">Appliances</div>
+															<div class="prod-name">{{ get_product_first_name($product) }}</div>
+															<div class="prod-price">{{ get_product_first_price($product) }}</div>
+														</div>
+													</a>
+												</div>
+											@else
+												<div class="prod-holder prod-holder-nonmember">
+													<a href="/product/view2/{{ $product['eprod_id'] }}" style="text-decoration: none;">
+														<div class="prod-image">
+															<button class="shop-now-btn">Shop Now</button>
+															<img class="img-hover" src="{{ get_product_first_image($product) }}">
+														</div>
+														<div class="details-container">
+															<div class="prod-type">Appliances</div>
+															<div class="prod-name">{{ get_product_first_name($product) }}</div>
+															<div class="prod-price">{{ get_product_first_price($product) }}</div>
+														</div>
+													</a>
+												</div>
+											@endif
+										</div>
+										@endforeach
+									@else
+									   <div class="col-md-3" >
+                                            <strong>"NO ITEMS FOUND"</strong>
+                                       </div>
+									@endif
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- SCROLL TO TOP -->
+	<div class="scroll-up"><img src="/themes/{{ $shop_theme }}/img/scroll-up.png"></div>
+</div>
+@endsection
+
+@section("css")
+<link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/product.css">
+@endsection
+
+@section("js")
+<script type="text/javascript">
+    $(function()
+    {
+        $('#close-menu').on('click',function()
+        {
+            $(this).closest('#menu').toggle(500,function(){
+            $('.mini-submenu').fadeIn();
+            $('#cat-title').fadeIn();
+        });
+    });
+        $('.mini-submenu').on('click',function()
+        {
+            $(this).next('#menu').toggle(500);
+            $('.mini-submenu').hide();
+            $('#cat-title').hide();
+        })
+    })
+</script>
+@endsection
+
+
+

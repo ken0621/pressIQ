f160b37cc18d13ffae7e38442430dee6a69b4aaa
Edward <edwardguevarra2003@gmail.com>
2018-08-23 18:05:49 +0800

    Status Password

--- app/Globals/Customer.php
+++ app/Globals/Customer.php
@@ -112,7 +112,7 @@ class Customer
 	}
 	public static function check_account($shop_id, $email, $password = '')
 	{
-		$check_account =  Tbl_customer::where("shop_id", $shop_id)->where("email", $email)->first();
+		$check_account =  Tbl_customer::where("shop_id", $shop_id)->where("email", $email)->where("tbl_customer.archived", 0)->first();
 		//dd(Crypt::decrypt($check_account->password));
 		if($check_account)
 		{

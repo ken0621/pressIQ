bd71ca3036262f65f06c0bf32d6985b262497ef5
jammmh05 <sebastianjhonpaul@gmail.com>
2018-09-06 18:01:13 +0800

    shellcanvas

---
+++ public/themes/shell-canvas/js/gallery-swiper.js
@@ -0,0 +1,47 @@
+var swiper3 = new swiper3();
+
+function swiper3()
+{
+	init();
+
+	function init()
+	{
+		document_ready();
+	}
+
+	function document_ready()
+	{
+		$(document).ready(function()
+		{
+			swiper_multiple_row();
+		});
+	}
+
+	function swiper_multiple_row()
+	{
+		var swiper = new Swiper('.swiper3', {
+			autoplay: true,
+			slidesPerView: 4,
+			slidesPerColumn: 2,
+			spaceBetween: 30,
+			pagination: {
+			el: '.swiper-pagination3',
+			clickable: true,
+			},
+			breakpoints: {
+				768: {
+					slidesPerView: 3,
+					slidesPerColumn: 2
+				},
+				425: {
+					slidesPerView: 2,
+					slidesPerColumn: 2
+				},
+				375: {
+					slidesPerView: 1,
+					slidesPerColumn: 2
+				}
+			}
+		});
+	}
+}
---
+++ public/themes/shell-canvas/views/member/register.blade.php
@@ -0,0 +1,211 @@
+@extends("layout")
+@section("content")
+
+<div id="home" class="register-wrapper" style="background-image: url('/themes/heartzone/img/login-banner.jpg');">
+    <div style="padding: 50px 0;">
+
+        @include("member2.include_register")
+
+        <!-- Modal -->
+        <div class="modal fade modal-agreement" id="modal_agreement" role="dialog">
+            <div class="modal-dialog">
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">Accept HEARTZONE ENTERPRISES INC. Contract</div>
+                    <div class="modal-body">
+                        <div class="contract">
+                            <p class="cnt-txt-b">
+                                This INDEPENDENT DISTRIBUTORSHIP Agreement is made and entered into by and between:  
+                                HEARTZONE ENTERPRISES INC., hereinafter referred to as to as the FIRST PARTY, and<br>
+                            </p>
+
+                            <p class="cnt-txt-b">
+                                The INDEPENDENT DISTRIBUTOR who voluntarily sign-up to the program of HEARTZONE ENTETPRISES INC., committing to deligently promote and distribute the  products and services of the FIRST PARTY, hereinafter referred to as THE SECOND PARTY.<br>
+                            </p>
+
+                            <p class="cnt-txt-b">
+                                BY COMPLETING THIS REGISTRATION PROCESS AND AGREEING TO THE TERMS AND CONDITIONS OF THIS CONTRACT, THE SECOND PARTY HEREBY UNDERSTANDS FULLY THAT HE/SHE IS  BOUND TO ADHERE TO THESE TERMS AND CONDITIONS AND TO ALL   OTHER POLICIES OF THE FIRST PARTY.  IF THE SECOND PARTY DOES NOT WISH TO BE BOUND BY THE TERMS OF THIS AGREEMENT, HE/SHE MAY EXIT THE REGISTRATION PROCESS NOW.<br>
+                            </p>
+                            
+                            <ol>
+                              <li style="font-weight: 600;">1. Compliance with Agreement<br></li>
+                            </ol> 
+                            
+                            <p class="cnt-txt-b">
+                                It is within FIRST PARTY’S   sole discretion as to whether the distributor   has   violated the terms of this Agreement. The FIRST PARTY reserves the right to refuse anyone to become a distributor. The FIRST PARTY may also, in its sole discretion, prohibit any distributor or client from using or promoting the FIRST PARTY’S Products and Services when a violation is committed.
+                            </p>
+                            
+                            <ol>
+                              <li style="font-weight: 600;">2. Payments of Commissions /Bonuses<br></li>
+                            </ol> 
+
+                            <p class="cnt-txt-b">
+                                WHEREAS, the program intends to offer FINANCIAL SECURITY AND FREEDOM to aspiring distributors, hence a very lucrative COMPENSATION PLAN was designed.  NOW, THEREFORE, The FIRST PARTY further reserves the right to alter or change the marketing plan when needed. In cases of over payout due to increases in manufacturing costs and other expenses directly or indirectly related to product development and distribution, the FIRST PARTY through its auto-safety checkpoints, will automatically adjust or modify the commissions/bonuses rates  into variable rates  up to a fairly safe level,, to achieve a benchmark gross profit.  Thus, the welfare of the majority of the distributors and the stability of the enterprise is hereby protected.<br>
+                            </p>
+
+                            <p class="cnt-txt-b">
+                                Commissions shall be paid on a weekly basis for incomes generated from initial purchase of promo packages, hereby referred to as DIRECT REFERRAL INCOME. All commissions generated from repeat purchases, also referred to as UNILEVEL INCOME and  MLM INCOME, will be paid on a monthly basis. The FIRST PARTY has the right to assign cut-off dates for encasement transactions on the system, moreover to schedule   pay-out dates for the said commissions<br>
+                            </p>
+
+                            <ol>
+                              <li style="font-weight: 600;">3. Distributor Responsibilities:<br></li>
+                            </ol> 
+
+                            <p class="cnt-txt-b">
+                                The SECOND PARTY fully understands and agrees that violation of any of the following conditions shall result in the forfeiture of any accrued commissions, and immediate termination of his/her account: <br>
+                            </p>
+
+                            <div class="sub-info" style="padding-left: 30px;">
+
+                                <p style="font-weight: 600; font-size: 15px;">a) Unsolicited e-mail<br></p>
+
+                                <p class="cnt-txt-b">
+                                    The SECOND PARTY  shall not use Unsolicited Commercial E-mail, referred to as “Spam”, or any form of unsolicited social media promotions of  any of the products and Services associated with HEARTZONE ENTERPRISES INC.<br>
+                                </p>
+
+                                <p style="font-weight: 600; font-size: 15px;">b) Inappropriate Content:<br></p>
+
+                                <p class="cnt-txt-b">
+                                    The SECOND PARTY  may not link to HEARTZONE ENTERPRISES INC.from any website which contains content that is slanderous, libelous, threatening, pornographic, obscene and or infringing that could give rise to any civil or criminal liability. <br>
+                                </p>
+
+                                <p style="font-weight: 600; font-size: 15px;">c) Inappropriate Representation<br></p>
+
+                                <p class="cnt-txt-b">
+                                    The SECOND PARTY shall not misrepresent the FIRST PARTY by means of false or exaggerated claims about the products, services or compensation plan of A’S DIAMOND PLUS that could destroy or demoralize the reputation of the FIRST PARTY. <br>
+                                </p>
+
+                                <p style="font-weight: 600; font-size: 15px;">d) HEARTZONE ENTERPRISES INC.PLUS Competitors:<br></p>
+
+                                <p class="cnt-txt-b">
+                                    TTHE SECOND PARTY may not refer or place links to competitors of the HEARTZONE ENTERPRISES INC. (such as other MLM Programs) on the SECOND PARTY’S HEARTZONE ENTERPRISES INC. account, tracking center or site.<br>
+                                </p>
+
+                            </div>
+
+                            <ol>
+                              <li style="font-weight: 600;">4. Use of HEARTZONE ENTERPRISES INC. Intellectual Property and Content:<br></li>
+                            </ol> 
+
+                            <p class="cnt-txt-b">
+                                The SECOND PARTY  IS  granted a limited, non-exclusive authorization to use selected images, logos, trade names, trademarks, promo materials, copyrighted material and similar identifying material relating to the FIRST PARTY as provided by HEARTZONE ENTERPRISES INC. solely to advertise or market its Products and Services. The SECOND PARTY may not sell, reproduce, distribute, copy, duplicate, resell, modify, display, prepare derivative works for any commercial purpose, or otherwise use any of the Content in any way, for any public or commercial purpose without prior written consent of the FIRST PARTY.<br>
+                            </p>
+
+                            <ol>
+                              <li style="font-weight: 600;">5. Publicity:<br></li>
+                            </ol> 
+
+                            <p class="cnt-txt-b">
+                                The SECOND PARTY shall not create, publish, distribute, or permit any written material that makes reference to HEARTZONE ENTERPRISES INC. without first submitting such material to the FIRST PARTY and receiving its prior written consent. <br>
+                            </p>
+
+                            <ol>
+                              <li style="font-weight: 600;">6. Term of the Agreement:<br></li>
+                            </ol> 
+
+                            <p class="cnt-txt-b">
+                                The Term of this Agreement shall become effective upon acceptance of the Distributor Application submitted by the SECOND PARTY through the Registration Process. Either the FIRST PARTY or the SECOND PARTY may terminate this Agreement at any time, for any reason or no reason, by giving the other party written notice of termination. The SECOND PARTY is only eligible to earn commissions/bonuses on sales occurring during the term of this Agreement, and commissions earned through the date of termination will remain payable excluding amounts incurred due to payment fraud, charge backs, refunds and bad debts and credits for canceled sales. The FIRST PARTY may withhold the SECOND PARTY’S final payment, or a portion thereof, for a reasonable time to ensure that the correct amount is paid.<br>
+                            </p>
+
+                            <ol>
+                              <li style="font-weight: 600;">7. Modifications of Agreement:<br></li>
+                            </ol> 
+
+                            <p class="cnt-txt-b">
+                                The FIRST PARTY may modify any of the terms and conditions contained in this Agreement, at any time and in its sole discretion, by posting a change notice or a new Agreement on the Site of HEARTZONE ENTERPRISES INC.. The SECOND PARTY will be notified of any such change through an e-mail or other similar method before a change is made. Modifications may include, but are not limited to, changes in the scope of available commissions, payment procedures and terms and conditions of the Distributor Program. If any modification is unacceptable to the SECOND PARTY, his/her only recourse is to terminate this agreement as provided in paragraph 8. In the event the SECOND PARTY object to a change, the changes that were announced will nevertheless become effective unless both parties agree, in writing, to the contrary.  The SECOND PARTY’S continued participation in the distributor program by failure to terminate this agreement following the posting of a change notice or new agreement on the HEARTZONE ENTERPRISES INC. site will constitute binding acceptance of such change or new agreement.<br>
+                            </p>
+
+                            <ol>
+                              <li style="font-weight: 600;">8. Relationship of Parties:<br></li>
+                            </ol> 
+
+                            <p class="cnt-txt-b">
+                                The FIRST PARTY and the SECOND PARTY are independent contractors, and nothing in this Agreement will create any partnership, joint venture, agency, franchise, sales representative or employment relationship between the parties. The SECOND PARTY has no authority to make or accept any offers or representations on FIRST PARTY’S behalf unless otherwise authorized by the FIRST PARTY. <br>
+                            </p>
+
+                            <ol>
+                              <li style="font-weight: 600;">9. Disclaimers:<br></li>
+                            </ol> 
+
+                            <p class="cnt-txt-b">
+                                BOTH PARTIES MAKE NO EXPRESS OR IMPLIED WARRANTIES OR REPRESENTATIONS WITH RESPECT TO THE PROGRAM OR WITH RESPECT TO THE PRODUCTS OR SERVICES SOLD THROUGH THE PROGRAM (INCLUDING, WITHOUT LIMITATION, WARRANTIES OF THERAPEUTIC CLAIMS, FITNESS AND BEAUTY, GUARANTEED INCOME AND FINANCIAL SUCCESS, OR ANY IMPLIED WARRANTIES ARISING OUT OF COURSE OF PERFORMANCE). IN ADDITION, BOTH PARTIES MAKE NO REPRESENTATION THAT THE OPERATION OF HEARTZONE ENTERPRISES INC.’ SITE AND SYSTEM OR THE PROVISION OF THE PRODUCTS AND SERVICES WILL BE UNINTERRUPTED OR ERROR FREE, AND WILL NOT BE LIABLE IN ANY WAY FOR THE CONSEQUENCES OF ANY INTERRUPTIONS OR ERRORS. <br>
+                            </p>
+
+                            <ol>
+                              <li style="font-weight: 600;">10. Limitation of Liability:<br></li>
+                            </ol> 
+
+                            <p class="cnt-txt-b">
+                                BOTH PARTIES, after thorough investigation, will not be liable for indirect, special or consequential damages, or any loss of revenue, profits or data, arising out of or relating to this Agreement or the Program, damages caused by “FORCE-MAJEURE” or “ACT OF GOD”, even if both parties have been advised of the possibility of such damages. <br>
+                            </p>
+
+                            <ol>
+                              <li style="font-weight: 600;">11. Indemnification:<br></li>
+                            </ol> 
+
+                            <p class="cnt-txt-b">
+                               The SECOND PARTY  hereby agree to indemnify and hold harmless HEARTZONE ENTERPRISES INC., its subsidiaries and affiliates, and their directors, officers, employees, agents, shareholders, partners, members and other owners, against any and all claims, actions, demands, liabilities, losses, damages, judgments, settlements, expenses (including reasonable attorneys' fees, which will include an allocable portion of the expense of such party's corporate legal department), and costs (any or all of the foregoing hereinafter referred to as "Losses") insofar as such Losses (or actions in respect thereof) arise out of or are related to: (i) any claim that the SECOND PARTY’S use of the materials infringes on any trademark, trade name, service mark, copyright, license, intellectual property, or other proprietary right of any third party.<br>
+                            </p>
+
+                            <ol>
+                              <li style="font-weight: 600;">12. No Income Guarantee:<br></li>
+                            </ol> 
+
+                            <p class="cnt-txt-b">
+                               THE SECOND PARTY ACKNOWLEDGES BY PRESSING THE "I AGREE" BUTTON BELOW THAT (A) HE OR SHE HAS READ THIS AGREEMENT AND ACCEPT ALL ITS TERMS AND CONDITIONS, (B) HE OR SHE UNDERSTANDS THAT BOTH PARTIES MAY AT ANY TIME (DIRECTLY OR INDIRECTLY) SOLICIT CUSTOMER REFERRALS ON TERMS THAT MAY DIFFER FROM THOSE CONTAINED IN THIS AGREEMENT, OR DEVELOP OTHER LINES OR NETWORK ORGANAZATIONS DIRECTLY CONNECTED TO THE HEARTZONE ENTERPRISES INC.’S NETWORK STRUCTURE, (C) HE HAS INDEPENDENTLY EVALUATED THE DESIRABILITY OF PARTICIPATING IN THE PROGRAM AND IS NOT RELYING ON ANY REPRESENTATION, GUARANTEE, OR STATEMENT BY HIS “UPLINES”,  OTHER THAN AS SET FORTH IN THIS AGREEMENT AND (D) HE OR SHE ACKNOWLEDGES THAT THE FIRST PARTY HAS MADE NO REPRESENTATION OR GUARANTEE REGARDING THE POSSIBILITY OR AMOUNT OF INCOME THAT MAY BE EARNED PURSUANT TO THE PROGRAM.<br>
+                            </p>
+
+                            <ol>
+                              <li style="font-weight: 600;">13. Governing Law:<br></li>
+                            </ol> 
+
+                            <p class="cnt-txt-b">
+                               THThis Agreement will be governed by the laws of the Republic of the Philippines, without regard to the choice of law provisions thereof. Any dispute between the parties arising under this Agreement will be resolved by binding arbitration by an arbitrator to be conducted in Quezon City, Philippines, in accordance with the then current Commercial Arbitration Rules of the Philippines. The arbitrator shall have the discretion to order that the cost of arbitration, including the arbitrator's fees, or other costs, and reasonable attorneys' fees, shall be borne by the losing party. The award of the arbitrator shall be binding and may be entered as a judgment in any court of competent jurisdiction.<br>
+                            </p>
+
+                            <ol>
+                              <li style="font-weight: 600;">14. Attorneys' Fees:<br></li>
+                            </ol> 
+
+                            <p class="cnt-txt-b">
+                               If either of the parties hereto (or any successor thereto) resorts to legal action in order to enforce, defend or interpret any of the terms or the provisions of this Agreement, the prevailing party will be entitled to receive, in addition to such other remedies as will be awarded to it in such legal action, reimbursement from the non-prevailing party for all reasonable attorneys' fees and all other costs incurred in commencing, maintaining or defending such action (which will include an allocable portion of the expense of such party's corporate legal department). In addition, the prevailing party will be entitled to recover from the non-prevailing party post-judgment attorneys' fees incurred in enforcing a judgment against the non-prevailing party.<br>
+                            </p>
+
+                            <ol>
+                              <li style="font-weight: 600;">15. Account deactivation:<br></li>
+                            </ol> 
+
+                            <p class="cnt-txt-b">
+                               The SECOND PARTY’S account will be deactivated and all bonuses/commissions forfeited if not used or accessed within a two year period.<br>
+                            </p>
+
+                            <ol>
+                              <li style="font-weight: 600;">16. Miscellaneous:<br></li>
+                            </ol> 
+
+                            <p class="cnt-txt-b">
+                               All notices under this Agreement shall be in writing and shall be deemed to have been duly given when receipt is electronically confirmed, if sent by cargo shipments or any other similar methods of delivery, return receipt may be requested. The SECOND PARTY may not assign this Agreement, by operation of law or otherwise, without the FIRST PARTY’S prior written consent. Subject to that restriction, this Agreement will be binding on, inure to the benefit of, and be enforceable against the parties and their respective successors and assigns. The FIRST PARTY’S failure to enforce the SECOND PARTY’S strict performance of any provision of this Agreement will not constitute a waiver of FIRST PARTY’S right to subsequently enforce such provision or any other provision of this Agreement.<br>
+                            </p>
+
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-pure pull-right" data-dismiss="modal">Accept</button>
+                        <button type="submit" class="btn btn-semi pull-right" data-dismiss="modal">Cancel</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+@section("script")
+<script type="text/javascript" src="/themes/{{ $shop_theme }}/js/member_register.js"></script>
+{{-- <script type="text/javascript" src="/assets/member/js/google_script/google_script_auth.js"></script> --}}
+
+<script>startApp();</script>
+@endsection
+@section("css")
+<link rel="stylesheet" type="text/css" href="/themes/{{ $shop_theme }}/css/member_register.css">
+@endsection

453cb26d8caa8f1e4f3f52af454f5596e2b2719d
jammmh05 <sebastianjhonpaul@gmail.com>
2018-10-17 13:14:14 +0800

    cms

--- public/themes/philtech/views/about.blade.php
+++ public/themes/philtech/views/about.blade.php
@@ -6,28 +6,29 @@
 	<div class="greetings">
 		<div class="container">
 			<div class="title-holder">
-				Welcome to the <span>BUSINESS OPPORTUNITY</span>
+				{{-- Welcome to the --}}{!! get_content($shop_theme_info, "about", "about_title_1") !!} <span>{{-- BUSINESS OPPORTUNITY --}}{!! get_content($shop_theme_info, "about", "about_title_2") !!}</span>
 			</div>
 			<div class="paragraph">
-				This is a very unique business model that would make all of us realize and achieve our DREAMS. My greatest vision and passion have been to help more Filipinos to own an affordable and profitable BUSINESS and be truly successful holistically in their lives, thus the PhilTECH company was founded.
+				{{-- This is a very unique business model that would make all of us realize and achieve our DREAMS. My greatest vision and passion have been to help more Filipinos to own an affordable and profitable BUSINESS and be truly successful holistically in their lives, thus the PhilTECH company was founded.
 				<br><br>
 				For me, SUCCESS is not just about FINANCIAL FREEDOM, it is also about the TOTAL LIFE TRANSFORMATION: FINANCIALLY, EMOTIONALLY AND SPIRITUALLY. To MENTOR, MOTIVATE and INSPIRE other people regardless of age, gender, socio-economic status and educational background. 
 				<br><br>
 				PhilTECH, INC. has been established to provide the right vehicle to ride on to earn limitless passive income and to become MILLIONAIRE in the future. PhilTECH, INC. is soaring at its highest potential to offer the BUSINESS PACKAGES to all FILIPINOS anywhere in the world. It is one of my dreams that many people will be successful for the rest of their lives. 
 				<br><br>
-				Again, I would like to welcome you all to this Life-Changing- Opportunity!
+				Again, I would like to welcome you all to this Life-Changing- Opportunity! --}}
+				{!! get_content($shop_theme_info, "about", "about_content_1") !!}
 			</div>
 			<div class="owner">
 				<div class="row">
 					<div class="col-md-6 col-sm-6 col-xs-12">
 						<div class="image-holder">
-							<img src="/themes/{{ $shop_theme }}/img/greeting-owner.jpg">
+							<img src="{{-- /themes/{{ $shop_theme }}/img/greeting-owner.jpg --}}{!! get_content($shop_theme_info, "about", "about_image_1") !!}">
 						</div>
 					</div>
 					<div class="col-md-6 col-sm-6 col-xs-12">
 						<div class="name-holder">
-							<div class="name">ARNOLD A. ARBILLERA</div>
-							<div class="position">President & CEO</div>
+							<div class="name">{{-- ARNOLD A. ARBILLERA --}}{!! get_content($shop_theme_info, "about", "about_author") !!}</div>
+							<div class="position">{{-- President & CEO --}}{!! get_content($shop_theme_info, "about", "about_position") !!}</div>
 						</div>
 					</div>
 				</div>
@@ -41,19 +42,19 @@
 				<div class="row">
 					<div class="col-md-6 col-sm-6 col-xs-12">
 						<div class="title-holder">
-							Welcome to <span>Philtech</span>
+							{{-- Welcome to --}}{!! get_content($shop_theme_info, "about", "about_title_3") !!} <span>{{-- Philtech --}}{!! get_content($shop_theme_info, "about", "about_title_4") !!}</span>
 						</div>
 					</div>
 					<div class="col-md-6 col-sm-6 col-xs-12">
 						<div class="image-holder">
-							<img src="/themes/{{ $shop_theme }}/img/gold-logo.png">
+							<img src="{{-- /themes/{{ $shop_theme }}/img/gold-logo.png --}}{!! get_content($shop_theme_info, "about", "about_image_2") !!}">
 						</div>
 					</div>
 				</div>
 			</div>
 			
 			<div class="paragraph">
-				<strong>COMPANY PROFILE</strong>
+				{{-- <strong>COMPANY PROFILE</strong>
 				<br><br>
 				PHILTECH, INC. is a SALES AND MARKETING  Company offering a wide range of PRODUCTS and SERVICES. The first and pioneer in the Philippines to provide MULTI-BUSINESS-SYSTEMS with UNIQUE CONSUMERS LOYALTY, REWARDS and CASHBACK SYSTEM. 
 				<br><br>
@@ -62,7 +63,7 @@
 				PHILTECH, INC. is offering COMPLETE PAYMENT ECO-SYSTEM, ALL-IN-ONE BUSINESS SOLUTIONS for SME’s and Large Scale Enterprises. 
 				Our primary objective is to provide RISK-FREE, HASSLE FREE and CONVENIENCE to both MERCHANTS and CUSTOMERS by providing PAYMENTS SOLUTIONS like QR CODE, CREDIT CARDS POS and DEBIT CARD CASH-OUT Device. PHILTECH MERCHANTS eliminate the risk of CASH SHORTAGE, FAKE MONEY and any RISK involving HUMAN manipulations in doing business.
 				<br><br>
-				The company is partnered by Philippine-Multi-National Company servicing  globally such as Airline Ticketing, Hotels Booking platform and Travel and Tours for both Domestic and International. 
+				The company is partnered by Philippine-Multi-National Company servicing  globally such as Airline Ticketing, Hotels Booking platform and Travel and Tours for both Domestic and International. --}}{!! get_content($shop_theme_info, "about", "about_content_2") !!}
 			</div>
 		</div>
 	</div>
@@ -71,18 +72,18 @@
 		<div class="container">
 			<div class="mission">
 				<div class="title-holder">
-					Our <span>Mission</span>
+					{{-- Our --}}{!! get_content($shop_theme_info, "about", "about_mission_title_1") !!} <span>{{-- Mission --}}{!! get_content($shop_theme_info, "about", "about_mission_title_2") !!}</span>
 				</div>
 				<div class="paragraph">
-					To empower all consumers to experience convenience in shopping with unique benefits and privileges. To become successful entrepreneurs for them to achieve their goals and dreams by providing an excellent business model towards financial freedom and developing a SENSE of OWNERSHIP.
+					{{-- To empower all consumers to experience convenience in shopping with unique benefits and privileges. To become successful entrepreneurs for them to achieve their goals and dreams by providing an excellent business model towards financial freedom and developing a SENSE of OWNERSHIP. --}}{!! get_content($shop_theme_info, "about", "about_mission_content") !!}
 				</div>
 			</div>
 			<div class="vision">
 				<div class="title-holder">
-					Our <span>Vision</span>
+					{{-- Our --}}{!! get_content($shop_theme_info, "about", "about_vision_title_1") !!} <span>{{-- Vision --}}{!! get_content($shop_theme_info, "about", "about_vision_title_2") !!}</span>
 				</div>
 				<div class="paragraph">
-					To continually provide affordable ONE-STOP-SHOP Multi Business Systems to all Filipinos, to provide convenience and Payment Solutions to both MERCHANTS and CUSTOMERS, to carry and engage in SALES and  MARKETING with LATEST TECHNOLOGY PRODUCTS  and  LATEST CONSUMERS PRODUCTS for all Filipinos.  To constantly develop and innovate PhilTECH’s unique business model and marketing system strategy by providing sustainable and long term business for all.
+					{{-- To continually provide affordable ONE-STOP-SHOP Multi Business Systems to all Filipinos, to provide convenience and Payment Solutions to both MERCHANTS and CUSTOMERS, to carry and engage in SALES and  MARKETING with LATEST TECHNOLOGY PRODUCTS  and  LATEST CONSUMERS PRODUCTS for all Filipinos.  To constantly develop and innovate PhilTECH’s unique business model and marketing system strategy by providing sustainable and long term business for all. --}}{!! get_content($shop_theme_info, "about", "about_vision_content") !!}
 				</div>
 			</div>
 		</div>
@@ -91,10 +92,10 @@
 	<div class="core-values">
 		<div class="container">
 			<div class="title-holder">
-				Our <span>Values</span>
+				{{-- Our --}}{!! get_content($shop_theme_info, "about", "about_corevalues_title_1") !!} <span>{{-- Values --}}{!! get_content($shop_theme_info, "about", "about_corevalues_title_2") !!}</span>
 			</div>
 			<div class="image-holder">
-				<img src="/themes/{{ $shop_theme }}/img/CoreValue.png">
+				<img src="{{-- /themes/{{ $shop_theme }}/img/CoreValue.png --}}{!! get_content($shop_theme_info, "about", "about_corevalues_image") !!}">
 			</div>
 		</div>
 	</div>

6b8348038acf558411267337d0f1e42a7f375de4
jammmh05 <sebastianjhonpaul@gmail.com>
2018-11-24 18:02:10 +0800

    dmsph

--- public/themes/dmsph/views/home.blade.php
+++ public/themes/dmsph/views/home.blade.php
@@ -127,6 +127,253 @@
 				<div class="product-title">
 					QUICKBOOKS PRODUCTS
 				</div>
+				 <div class="product-tabs" id="myTabs">
+					<a data-toggle="tab" href="#product1" class="product-btn active">QuickBooks Enterprise</a>
+					<a data-toggle="tab" href="#product2" class="product-btn">QuickBooks Premier</a>
+					<a data-toggle="tab" href="#product3" class="product-btn">QuickBooks Pro</a>
+				 </div>
+				 <div class="tab-content">
+				    <div id="product1" class="tab-pane fade in active">
+					    <div class="product-desc-container">
+					    	<div class="product-desc-container-1">
+					    		<div class="product-header">Product Description</div>
+					    		QuickBooks Enterprise Subscription Plan:
+					    		<br>
+					    		Be updated by subscribing to QuickBooks Enterprise Yearly Upgrade and Update Policy
+					    		<br><br>
+					    		QuickBooks Enterprise One Time Payment Policy:
+					    		<br>
+					    		Don’t ant yearly payment? Of course! Inquire now to get our QuickBooks One Time Payment Policy Promo! *Promo until supplies last*
+					    		<br><br>
+					    		QuickBooks Enterprise Old Version:
+					    		<br>
+					    		Nostalgic for past versions? Or maybe you just can’t move from this specific version? Is it the price? Come on! You all have reasons but we got you covered!
+						    </div>
+						    <div class="product-desc-container-2">
+						    	<table class="product-table">
+						    		<tr>
+						    			<th>QuickBooks Enterprise 2019</th>
+						    			<th>Silver</th>
+						    			<th>Platinum</th>
+						    		</tr>
+						    		<tr>
+						    			<td>QuickBooks Desktop Enterprise Software</td>
+						    			<td class="text-center">x</td>
+						    			<td class="text-center">x</td>
+						    		</tr>
+						    		<tr>
+						    			<td>DMSPH QuickBooks Enterprise Priority Response Program</td>
+						    			<td class="text-center">x</td>
+						    			<td class="text-center">x</td>
+						    		</tr>
+						    		<tr>
+						    			<td>Automatic QuickBooks Product Upgrades</td>
+						    			<td class="text-center">x</td>
+						    			<td class="text-center">x</td>
+						    		</tr>
+						    		<tr>
+						    			<td>Anytime, Anywhere, Any Device</td>
+						    			<td class="text-center"></td>
+						    			<td class="text-center"></td>
+						    		</tr>
+						    		<tr>
+						    			<td>Advanced Reporting</td>
+						    			<td class="text-center">x</td>
+						    			<td class="text-center">x</td>
+						    		</tr>
+						    		<tr>
+						    			<td>Advanced Inventory</td>
+						    			<td class="text-center"></td>
+						    			<td class="text-center">x</td>
+						    		</tr>
+						    		<tr>
+						    			<td>Advanced Pricing</td>
+						    			<td class="text-center"></td>
+						    			<td class="text-center">x</td>
+						    		</tr>
+						    	</table>
+						    </div>
+					    </div>
+					    <div class="button-container">
+					    	<button class="btn-feature">SEE FULL FEATURES</button>
+					    	<button class="btn-inquire">INQUIRE NOW</button>
+					    </div>
+				    </div>
+				    <div id="product2" class="tab-pane fade">
+					    <div class="product-desc-container">
+					    	<div class="product-desc-container-1">
+					    		<div class="product-header">Product Description</div>
+					    		• All the features of Pro
+					    		<br>
+								• Industry-specific reports
+								<br>
+								• Create sales orders
+								<br>
+								• Track costs for products and inventory
+								<br>
+								• Set product and service prices by customer type
+								<br>
+								• Up to 5 users8
+						    </div>
+					    </div>
+					    <div class="button-container">
+					    	<button class="btn-feature">SEE FULL FEATURES</button>
+					    	<button class="btn-inquire">INQUIRE NOW</button>
+					    </div>
+				    </div>
+				    <div id="product3" class="tab-pane fade">
+					    <div class="product-desc-container">
+					    	<div class="product-desc-container-1">
+					    		<div class="product-header">Product Description</div>
+					    		• Helps you maximize tax deductions with organized expenses
+					    		<br>
+								• Track business performance to make better decisions
+								<br>
+								• One-click financial, sales and tax reports
+								<br>
+								• Easy-to-use tools and simple setup
+								<br>
+								• No accounting knowledge necessary
+						    </div>
+					    </div>
+					    <div class="button-container">
+					    	<button class="btn-feature">SEE FULL FEATURES</button>
+					    	<button class="btn-inquire">INQUIRE NOW</button>
+					    </div>
+				    </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="wrapper-4">
+		<div class="container">
+			<div class="service-container">
+				<div class="service-title">
+					QUICKBOOKS SERVICES
+				</div>
+				<div class="service-holder">
+					<div class="service-content" style="background-image: url('/themes/{{ $shop_theme }}/img/service-1.jpg');">
+						<div class="title-holder">
+							<div class="title-1">
+							QuickBooks
+							</div>
+							<div class="title-2">
+								Training & Implementation
+							</div>
+						</div>
+					</div>
+					<div class="service-content" style="background-image: url('/themes/{{ $shop_theme }}/img/service-2.jpg');">
+						<div class="title-holder">
+							<div class="title-1">
+							QuickBooks
+							</div>
+							<div class="title-2">
+								Migration
+							</div>
+						</div>
+					</div>
+					<div class="service-content" style="background-image: url('/themes/{{ $shop_theme }}/img/service-3.jpg');">
+						<div class="title-holder">
+							<div class="title-1">
+							QuickBooks
+							</div>
+							<div class="title-2">
+								Auditing
+							</div>
+						</div>
+					</div>
+					<div class="service-content" style="background-image: url('/themes/{{ $shop_theme }}/img/service-4.jpg');">
+						<div class="title-holder">
+							<div class="title-1">
+							QuickBooks
+							</div>
+							<div class="title-2">
+								Support and Onsite Visit
+							</div>
+						</div>
+					</div>
+					<div class="service-content" style="background-image: url('/themes/{{ $shop_theme }}/img/service-5.jpg');">
+						<div class="title-holder">
+							<div class="title-1">
+							QuickBooks
+							</div>
+							<div class="title-2">
+								Forms Customization
+							</div>
+						</div>
+					</div>
+					<div class="service-content" style="background-image: url('/themes/{{ $shop_theme }}/img/service-6.jpg');">
+						<div class="title-holder">
+							<div class="title-1">
+							QuickBooks
+							</div>
+							<div class="title-2">
+								Reports Customization
+							</div>
+						</div>
+					</div>
+					<div class="service-content" style="background-image: url('/themes/{{ $shop_theme }}/img/service-7.jpg');">
+						<div class="title-holder">
+							<div class="title-1">
+							QuickBooks
+							</div>
+							<div class="title-2">
+								Special Reports and Special Forms Customization thru Crystal Report
+							</div>
+						</div>
+					</div>
+					<div class="service-content" style="background-image: url('/themes/{{ $shop_theme }}/img/service-8.jpg');">
+						<div class="title-holder">
+							<div class="title-1">
+							QuickBooks
+							</div>
+							<div class="title-2">
+								Anywhere Access Setup
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="wrapper-5">
+		<div class="container">
+			<div class="expertise-container">
+				<div class="expertise-title">
+					OUR EXPERTISE
+				</div>
+				<div class="expertise-holder">
+					<div class="expertise-header">
+						List of our Expertise
+					</div>
+					<div class="expertise-list-container">
+						<div class="expertise-list">
+							• Accounting or Bookkeeping<br>
+							• Advertising or Public Relations<br>
+							• Agriculture, Ranching, or Farming<br>
+							• Art, Writing or Photography<br>
+							• Automotive Sales or Repair<br>
+							• Church or Religious Organization<br>
+							• Construction General Contractor<br>
+							• Construction Trades (Plumber, Electrician, HVAC)<br>
+						</div>
+						<div class="expertise-list">
+							• Design, Architecture, or Engineering<br>
+							• Financial Services other than Accounting or Book<br>
+							• Hair Salon, Beauty Salon, or Barber Shop<br>
+							• Information Technology (Computers, Software)<br>
+							• Insurance Agency or Broker<br>
+							• Lawn Care or Landscaping<br>
+							• Legal Services<br>
+							• Lodging (Hotel, Motel)<br>
+						</div>
+					</div>
+					<div class="button-container">
+						<button class="btn-more">SEE MORE</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	</section>
@@ -177,17 +424,15 @@
 	});
 </script>
 <script>
-	$('.btn-more, .btn-show').click(function() {
-	    $('#morecontact, #hiddenabout').slideDown(1000);
-	    $('.btn-more, .btn-show').hide(0);
-	    $('.btn-less, .btn-hide').show(0);
-	});
-
-$('.btn-less, .btn-hide').click(function() {
-	    $('#morecontact, #hiddenabout').slideUp(1000);
-	    $('.btn-more, .btn-show').show(0);
-	    $('.btn-less, .btn-hide').hide(0);
-	});
+	var btnContainer = document.getElementById("myTabs");
+	var btns = btnContainer.getElementsByClassName("product-btn");
+	for (var i = 0; i < btns.length; i++) {
+  		btns[i].addEventListener("click", function() {
+	    var current = document.getElementsByClassName("active");
+	    current[0].className = current[0].className.replace(" active", "");
+	    this.className += " active";
+	  });
+	}
 </script>
 
 @endsection

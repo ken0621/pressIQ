4e68cda2684b297085695a81c49f8d80a24ae160
carlosegovia <carlosegovia5@gmail.com>
2019-07-19 09:55:39 +0800

    announcement

--- public/themes/philtech/views/bulletin.blade.php
+++ public/themes/philtech/views/bulletin.blade.php
@@ -1,9 +1,12 @@
 @extends("layout")
 @section("content")
 <div class="container">
-    <div class="bulletin-container" style="text-align: center; height: 100vh; display: flex; align-items:center; justify-content: center; padding: 25px;">
+    <div class="announcement-container" style="text-align: center; height: 100vh; display: flex; align-items:center; justify-content: center; padding: 25px;">
+        <div style="max-width: 700px; margin-right: 10px;">
+            <img src="/themes/{{ $shop_theme }}/img/announcement-1.jpg" alt="" style="width: 100%;">
+        </div>
         <div style="max-width: 700px;">
-            <img src="/themes/{{ $shop_theme }}/img/fcking-bulletin.jpg" alt="" style="width: 100%;">
+            <img src="/themes/{{ $shop_theme }}/img/announcement-2.jpg" alt="" style="width: 100%;">
         </div>
     </div>
 </div>
